d9c2bd5fdffa3a5c56c78982f7efbb8ed5053faf
Johannes Schindelin <johannes.schindelin@gmx.de>
2009-03-03 16:37:04 +0100

    tests: when a test failed, say which one
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- tests/run.sh
+++ tests/run.sh
@@ -2,6 +2,14 @@
 
 # Tests for fiji
 
+cd "$(dirname "$0")"
+
 # Run all jython tests contained in this current folder
-#
-../fiji --jython *.py
+
+for script in *.py
+do
+	../fiji --jython $script || {
+		echo "Failed test: $script"
+		exit 1
+	}
+done
