1e16d1996e5ed5cdf76d6e2565ee1dfa9d7d5efd
Johannes Schindelin <johannes.schindelin@gmx.de>
2009-03-03 18:11:01 +0100

    tests/plugin_jars.py: check all classes with ImageJ's class loader
    While at it, reindent and rearrange the code.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- tests/plugin_jars.py
+++ tests/plugin_jars.py
@@ -1,49 +1,52 @@
-# Test whether any menu items contain pointers to non-existent classes
-# which likely indicate a missconfiguration of a plugins.config file in a .jar plugin.
+#!/usr/bin/python
 
-import ij
-from ij import ImageJ, Menus
-import java
-from java.lang import Class, ClassNotFoundException, System, NoClassDefFoundError
+# Test whether any menu items contain pointers to non-existent classes which
+# likely indicate a missconfiguration of a plugins.config file in a .jar plugin.
+
+from ij import IJ, ImageJ, Menus
+
+from java.lang import Class, ClassNotFoundException, NoClassDefFoundError
+
 import sys
 
 # Launch ImageJ
 ImageJ()
 
 if len(sys.argv) > 1 and sys.argv[1] == '-v':
-  for key in ij.Menus.getCommands():
-    command = ij.Menus.getCommands().get(key)
-    print key, '->', command
-  sys.exit()
+	for key in Menus.getCommands():
+		command = Menus.getCommands().get(key)
+	print key, '->', command
+	sys.exit()
 
 ok = 1
 
+def doesClassExist(name):
+	try:
+		IJ.getClassLoader().loadClass(name)
+		return True
+	except ClassNotFoundException:
+		return False
+	except NoClassDefFoundError:
+		return False
+
 # Inspect each menu command
-for it in ij.Menus.getCommands().entrySet().iterator():
-  try:
-    cl = it.value
-    k = cl.find('(')
-    if -1 != k:
-      cl = cl[:k]
-    #print "testing: ", cl
-    cl = Class.forName(cl)
-  except ClassNotFoundException:
-    error = 0
-    # Try without the first package name, since it may be fake
-    # for plugins in subfolders of the plugins directory:
-    k = cl.find('.')
-    if -1 != k:
-      try:
-	# print 'Searching ij.IJ.getClassLoader for', cl
-	cl = ij.IJ.getClassLoader().loadClass(cl[k+1:])
-      except NoClassDefFoundError:
-        error = 1
-    else: error = 1
-    if error:
-      print 'ERROR: Class not found for menu command: ', it.key, '=>', it.value
-      ok = 0
+for it in Menus.getCommands().entrySet().iterator():
+	name = it.value
+	paren = name.find('(')
+	if -1 != paren:
+		name = name[:paren]
+
+	if not doesClassExist(name):
+		# Try without the first package name, since it may be fake
+		# for plugins in subfolders of the plugins directory:
+		dot = name.find('.')
+		if -1 == dot or not doesClassExist(name[dot+1:]):
+			print 'ERROR: Class not found for menu command:', \
+				it.key, '=>', it.value
+			ok = 0
 
 if ok:
-    print "ok - Menu commands all correct."
+	print "ok - Menu commands all correct."
+	sys.exit(0)
 
-System.exit(0)
+sys.exit(1)
