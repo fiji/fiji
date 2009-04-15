2d9351398c6572e850d47ebc2cc84ed3a26bb0f7
Daniel James White <dan@chalkie.org.uk>
2009-04-15 16:54:22 +0200

    Added an anhanced time stamper plugin
    This merges some functions of the imageJ bundled time stamper
     with the time stamper plugin from Tony Collin's collection at macbiophotonics

---
+++ src-plugins/Time_Stamper/Time_Stamper_Enhanced.java
@@ -0,0 +1,137 @@
+import ij.*;
+import ij.process.*;
+import ij.gui.*;
+import java.awt.*;
+import ij.plugin.filter.*;
+
+public class Time_Stamper_Enhanced implements PlugInFilter {
+	ImagePlus imp;
+	double time;
+	static int x = 2;
+	static int y = 15;
+	static int size = 12;
+	int maxWidth;
+	Font font;
+	static double start = 0;
+	static double interval = 1;
+	static String suffix = "s";
+	static int decimalPlaces = 0;
+	boolean canceled;
+	static boolean digital = false;
+	boolean AAtext=true;
+	int frame, first, last;
+
+	public int setup(String arg, ImagePlus imp) {
+		this.imp = imp;
+		IJ.register(Time_Stamper_Enhanced.class);
+		if (imp!=null) {
+			first = 1;
+			last = imp.getStackSize();
+		}
+		return DOES_ALL+DOES_STACKS+STACK_REQUIRED;
+	}
+
+	public void run(ImageProcessor ip) {
+		frame++;
+		if (frame==1) showDialog(ip);
+		if (canceled || frame<first || frame>last) return;
+		ip.setFont(font);
+		ip.setColor(Toolbar.getForegroundColor());
+		ip.setAntialiasedText(AAtext);
+		String s = "";
+		
+		time += interval;  // increments the time by the time interval
+		if (frame==last) imp.updateAndDraw();
+	
+	
+		if (!digital) s = getString(time);
+		if (digital) s = getString2(time);
+		ip.moveTo(x+maxWidth-ip.getStringWidth(s), y);
+		ip.drawString(s);
+		time += interval;
+	}
+
+		// makes the string containing the number for the time stamp, with specified decimal places
+		String getString(double time) {
+			if (interval==0.0)
+				return suffix;
+			else
+				return (decimalPlaces==0?""+(int)time:IJ.d2s(time, decimalPlaces))+" "+suffix;
+		}
+
+		String twoDigits(int value) {
+			return (value < 10 ? "0" : "") + value;
+		}
+
+		// makes the string containing the number for the time stamp, with hh:mm format
+			// which is nice, but also really need hh:mm:ss and mm:ss
+		String getString2(double time) {
+			int hour = (int)(time / 3600);
+			time -= hour * 3600;
+			int minute = (int)(time / 60);
+			time -= minute * 60;
+			return twoDigits(hour) + ":" + twoDigits(minute) + ":"
+				+ (time < 10 ? "0" : "") 
+				+ IJ.d2s(time, decimalPlaces);
+		}
+
+//	String getString(double time) {
+//		if (interval==0.0)
+//			return suffix;
+//		else
+//			return (decimalPlaces==0?""+(int)time:IJ.d2s(time, decimalPlaces))+" "+suffix;
+//	}
+
+
+	void showDialog(ImageProcessor ip) {
+		// Here we work out the size of the font to use from the size of the ROI box drawn, if one was drawn		
+		Rectangle roi = ip.getRoi();
+		if (roi.width<ip.getWidth() || roi.height<ip.getHeight()) {
+			x = roi.x;
+			y = roi.y+roi.height;
+			size = (int) ((roi.height - 1.10526)/0.934211);	
+			if (size<7) size = 7;
+			if (size>80) size = 80;
+		}
+		
+//		String[] timeunitsoptions =  { "h", "m", "s"};
+		
+		// This makes the GUI and reads user input parameters from it
+		GenericDialog gd = new GenericDialog("Time Stamper");
+		gd.addNumericField("Starting Time:", start, 2);
+		gd.addNumericField("Time Interval Between Frames:", interval, 2);
+		gd.addNumericField("X Location:", x, 0);
+		gd.addNumericField("Y Location:", y, 0);
+		gd.addNumericField("Font Size:", size, 0);
+		gd.addNumericField("Decimal Places:", decimalPlaces, 0);
+		gd.addNumericField("First Frame:", first, 0);
+		gd.addNumericField("Last Frame:", last, 0);
+		// should change this to a choice between digital or decimal
+		gd.addCheckbox("'hh:mm' format:", digital);  // but what about mm:ss 
+		gd.addStringField("Or with a suffix      Suffix:", suffix);
+//	    gd.addChoice("Time units:", timeunitsoptions, timeunitsoptions[1]); // should be able to choose h m or s from a list?
+		gd.addCheckbox("Anti-Aliased text?", true);
+		gd.showDialog();
+		if (gd.wasCanceled())
+			{canceled = true; return;}
+		start = gd.getNextNumber();
+ 		interval = gd.getNextNumber();
+		x = (int)gd.getNextNumber();
+		y = (int)gd.getNextNumber();
+		size = (int)gd.getNextNumber();
+		decimalPlaces = (int)gd.getNextNumber();
+		first = (int)gd.getNextNumber();
+		last = (int)gd.getNextNumber();
+		digital = gd.getNextBoolean();
+		suffix = gd.getNextString();
+		AAtext = gd.getNextBoolean(); 
+		font = new Font("SansSerif", Font.PLAIN, size);
+		ip.setFont(font);
+		time = start;
+		if (y<size)
+			y = size;
+		maxWidth = ip.getStringWidth(getString(start+interval*imp.getStackSize()));
+		imp.startTiming();
+	}
+
+}
