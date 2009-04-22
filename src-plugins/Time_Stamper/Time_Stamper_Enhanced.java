699a233c060fc318ef6b0fa28c8d5e4f8d80c5b6
Daniel James White <dan@chalkie.org.uk>
2009-04-22 15:24:06 +0200

    logic changes in GUI for selection of time stamp string format digital or decimal and fixed typos and added comments
    changed boolean selection of digital or decimal  to a drop down list,
    then use the output of that to select what format to write the timestamp string in.
    This entailed changing the if (!digital) to a double if statement matching the
    string output ofthe drop down list for time format selection,
    and sending that result to the Time Stamp string formatting  code.
    Various comments added and fixed so that i can understand the code better.
    Some typos fixed
    Seems to work as expected. Possible bug in AAtext... sometimes doesnt work as expected,
    perhaps on first try. Might need to put back the gadget maxWidth to prevent the
    timestamp text from running off the rioght side of hte image. I turned this off,
    but maybe it is a good thing after all.

--- src-plugins/Time_Stamper/Time_Stamper_Enhanced.java
+++ src-plugins/Time_Stamper/Time_Stamper_Enhanced.java
@@ -21,12 +21,13 @@ public class Time_Stamper_Enhanced implements PlugInFilter {
 	static double start = 0;
 	static double interval = 1;
 	static String customSuffix = "";
-	static String suffix = "s";
+	static String chosenSuffix = "s";
+	static String suffix = chosenSuffix;
 	static int decimalPlaces = 3;
 	boolean canceled;
-	static boolean digital = false;
+	static String digitalOrDecimal = "decimal";
 	boolean AAtext = true;
-	int frame, first, last;
+	int frame, first, last;  //these default to 0 as no values are given
 
 	// setup the plugin and tell imagej it needs to work on a stack
 	public int setup(String arg, ImagePlus imp) {
@@ -44,35 +45,42 @@ public class Time_Stamper_Enhanced implements PlugInFilter {
 		// Here we work out the size of the font to use from the size of the ROI box drawn, if one was drawn
 		// and set x and y at the ROI if there is one, so time stamp is drawn there, not at default x and y. 
 		Rectangle roi = ip.getRoi();
+		
 		if (roi.width<ip.getWidth() || roi.height<ip.getHeight()) {
 			x = roi.x;
 			y = roi.y+roi.height;
 			size = (int) ((roi.height - 1.10526)/0.934211);	// whats up with these numbers? Seems to make font too big?
-			// make sure the font is not too big or small.... but why?
+			// make sure the font is not too big or small.... but why? Too -  small cabt read it. Too Big - ?
+			// should this use private and public and get / set methods?
 			if (size<7) size = 7;
 			if (size>80) size = 80;
 		}
 	
 		// here is a list of SI? approved time units for a drop down list to choose from 
-		String[] timeunitsoptions =  { "y", "d", "h", "min", "s", "ms", "µs", "ns", "ps", "fs", "as" };
+		String[] timeUnitsOptions =  { "y", "d", "h", "min", "s", "ms", "µs", "ns", "ps", "fs", "as", "custom"};
+		String[] timeFormats = {"Decimal", "hh:mm:ss.ms"};
 		
 		// This makes the actual GUI 
 		GenericDialog gd = new GenericDialog("Time Stamper Enhanced");
-		gd.addNumericField("Starting Time:", start, 2);
-		gd.addNumericField("Time Interval Between Frames:", interval, 2);
+		gd.addNumericField("Starting Time (in s if digital):", start, 2);
+		gd.addNumericField("Time Interval Between Frames (in s if digital):", interval, 2);
 		gd.addNumericField("X Location:", x, 0);
 		gd.addNumericField("Y Location:", y, 0);
 		gd.addNumericField("Font Size:", size, 0);
 		gd.addNumericField("Decimal Places:", decimalPlaces, 0);
 		gd.addNumericField("First Frame:", first, 0);
 		gd.addNumericField("Last Frame:", last, 0);
+		
 		// should change this to a choice between digital or decimal
-		gd.addCheckbox("use digital 'hh:mm:ss.ms' format:", digital);  // but what about mm:ss 
-		gd.addStringField("Or a customised suffix:", customSuffix);
+		//gd.addCheckbox("use digital 'hh:mm:ss.ms' format:", digital);  // but what about mm:ss 
+		// options are in the string array timeFormats, default is Decimal:  something.somethingelse 
+		gd.addChoice("Time format:", timeFormats, timeFormats[0]); 
+		
+		gd.addStringField("Customised suffix:", customSuffix);
 		gd.addCheckbox("Anti-Aliased text?", true);
 		
 		// we can choose time units from a drop down list, list defined in timeunitsoptions
-		gd.addChoice("Time units:", timeunitsoptions, timeunitsoptions[4]); 
+		gd.addChoice("Time units:", timeUnitsOptions, timeUnitsOptions[4]); 
 		
 		gd.showDialog();  // shows the dialog GUI!
 		
@@ -89,35 +97,54 @@ public class Time_Stamper_Enhanced implements PlugInFilter {
 		decimalPlaces = (int)gd.getNextNumber();
 		first = (int)gd.getNextNumber();
 		last = (int)gd.getNextNumber();
-		digital = gd.getNextBoolean();
+		digitalOrDecimal = gd.getNextChoice();
 		customSuffix = gd.getNextString();
 		AAtext = gd.getNextBoolean(); 
-		suffix = gd.getNextChoice();
+		chosenSuffix = gd.getNextChoice();
+		
+		// set the font
 		font = new Font("SansSerif", Font.PLAIN, size);
 		ip.setFont(font);
-		time = start;
+		
+		// initialise time with the value of the starting time
+		time = start; 
+		
+		// make sure the y position for is not less than the font height: size, 
+		// so the time stamp is not off the bottom of the image?
 		if (y<size)
 			y = size;
     	
-		// maxWidth is an integer the length of the timeString for the last slice of the stack to be stamped
-		// it is used to work out where to start writing the timestamp in the image,
+		// maxWidth is an integer the length of the timeString (in pixels?) 
+		// for the last slice of the stack to be stamped.
+		// It is used to work out where to start writing the timestamp in the image,
+		// so it does not run off the right side of the image
 		// was:
 		// maxWidth = ip.getStringWidth(getString(start+interval*imp.getStackSize()));
-		// actually why bother, just start writing the time stamp at specificed xy accoriofng to default or ROI
-		// maxWidth = ip.getStringWidth(timeString(start+interval*imp.getStackSize()));
+		// actually why bother, just start writing the time stamp at specificed xy according to default or ROI
+		
 		imp.startTiming(); //What is this for?
 	}	
 	
+	
+	// Here we make the strings to print into the images. 
+	
+		// is there a non empty string in the custom suffix box in the dialog GUI?
+		// if so use it as suffix
+	String suffix() {
+		if (chosenSuffix == "custom")
+			return customSuffix;
+		else return chosenSuffix;
+	}
 	
 		// makes the string containing the number for the time stamp, with specified
 		// decimal places format is decimal number with specificed no of digits after
 		// the point if specificed no. of decimal places is 0 then just return the
 		// speficied customSuffix
 	String decimalString(double time) { 
-	  if (interval==0.0) 
-	    return suffix; 
-	  else
-	    return (decimalPlaces == 0 ? ""+(int)time : IJ.d2s(time, decimalPlaces)) + " " + suffix; 
+		if (interval==0.0) 
+			return suffix(); 
+		else
+			return (decimalPlaces == 0 ? ""+(int)time : IJ.d2s(time, decimalPlaces)) + " " + suffix(); 
 	}	
 	
 		// makes the string containing the number for the time stamp,
@@ -143,10 +170,8 @@ public class Time_Stamper_Enhanced implements PlugInFilter {
 
 	
 	public void run(ImageProcessor ip) {
-		// is there a non empty string in the custom suffix box in the dialog GUI?
-		// if so use it as suffix
-		if (customSuffix != "")
-				suffix = customSuffix;
+
+		// this increments frame integer by 1. If a int is declared with no value, it defaults to 0
 		frame++;
 		if (frame==1) showDialog(ip);
 		if (canceled || frame<first || frame>last) return;
@@ -161,15 +186,20 @@ public class Time_Stamper_Enhanced implements PlugInFilter {
 		// if it is decimal (not digital) then need to set suffix from drop down list (or custom suffix if one is entered)
 		// if it is digital, then there is no suffix as format is set yy:ddd:hh:mm:ss.ms? 
 		String timeString = "";
-		if (digital) 
+		if (digitalOrDecimal == "hh:mm:ss.ms") 
 			timeString = digitalString(time);
-		else
+		if (digitalOrDecimal == "Decimal") 
 			timeString = decimalString(time);
-		// the next line tries to moive the time stamp right a bit to account for the max length th etime stamp will be.
-		// possible superfulous, since you really want the time stamp to be written at the bottom left of the ROI you drew 
-		// or from the default of x and y. So just move to x y instead
+		//else except  // should catch this exception? it can only be one of what is in the String array  timeFormats
+		
+		// this commebnted out line tries to move the time stamp right a bit to account for the max length the time stamp will be.
+		// possible superfluous, since you really want the time stamp to be written at the bottom left of the ROI you drew 
+		// or from the default of x and y. So just move to x y instead. it you put it too close to the right edge, then thats
+		// pretty silly, and you need to make the font smaller to fit it there anyway. OK people are silly, so we need to still
+		// handle that....
 		//ip.moveTo(x+maxWidth-ip.getStringWidth(timeString), y);
-		ip.moveTo(x, y);
+		
+		ip.moveTo(x, y);  // move to x y position for Timestamp writing
 		ip.drawString(timeString);
 		time += interval;  // increments the time by the time interval
 
@@ -177,6 +207,6 @@ public class Time_Stamper_Enhanced implements PlugInFilter {
 
 	
 
-}	// thats the end of Time_Stamper_Enhanced 
+}	// thats the end of Time_Stamper_Enhanced class
 
 
