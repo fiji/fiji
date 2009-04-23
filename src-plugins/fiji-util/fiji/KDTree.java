328ba8b0ba8a5885e2c2e6cd245603e5313201b6
Johannes Schindelin <johannes.schindelin@gmx.de>
2009-04-23 11:41:22 +0200

    Move KDTree and NearestNeighbor into the 'fiji' package
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/fiji-util/fiji/KDTree.java
+++ src-plugins/fiji-util/fiji/KDTree.java
@@ -1,3 +1,5 @@
+package fiji;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
--- src-plugins/fiji-util/fiji/NearestNeighbor.java
+++ src-plugins/fiji-util/fiji/NearestNeighbor.java
@@ -1,3 +1,5 @@
+package fiji;
+
 import java.util.ArrayList;
 import java.util.List;
 
