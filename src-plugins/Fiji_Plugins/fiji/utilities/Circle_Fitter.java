5758f1a9c89984438eafca0c03192c4c3d64d04b
Johannes Schindelin <johannes.schindelin@gmx.de>
2009-05-19 18:32:44 +0200

    Add the "Fit Circle to Image" plugin to the Fiji_Plugins package
    The circle fitter tries to fit a circle selection to your image; it
    needs a pretty black background to do so.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ src-plugins/Fiji_Plugins/fiji/utilities/Circle_Fitter.java
@@ -0,0 +1,66 @@
+package fiji.utilities;
+
+import ij.ImagePlus;
+
+import ij.gui.OvalRoi;
+
+import ij.plugin.filter.PlugInFilter;
+
+import ij.process.ImageProcessor;
+
+public class Circle_Fitter implements PlugInFilter {
+	ImagePlus image;
+
+	public int setup(String arg, ImagePlus image) {
+		this.image = image;
+		return DOES_ALL | NO_CHANGES;
+	}
+
+	public void run(ImageProcessor ip) {
+		int w = ip.getWidth(), h = ip.getHeight();
+
+		// calculate mean centroid
+		float x, y, total;
+		x = y = total = 0;
+		for (int j = 0; j < h; j++)
+			for (int i = 0; i < w; i++) {
+				float value = ip.getf(i, j);
+				x += i * value;
+				y += j * value;
+				total += value;
+			}
+
+		x /= total;
+		y /= total;
+
+		// calculate the rest
+		float uu, uv, vv, uuu, uuv, uvv, vvv;
+		uu = uv = vv = uuu = uuv = uvv = vvv = 0;
+		for (int j = 0; j < h; j++)
+			for (int i = 0; i < w; i++) {
+				float value = ip.getf(i, j);
+				float u = i - x;
+				float v = j - y;
+				uu += u * u * value;
+				uv += u * v * value;
+				vv += v * v * value;
+				uuu += u * u * u * value;
+				uuv += u * u * v * value;
+				uvv += u * v * v * value;
+				vvv += v * v * v * value;
+			}
+
+		// calculate center & radius
+		float f = 0.5f / (uu * vv - uv * uv);
+		float centerU = (vv * (uuu + uvv) - uv * (uuv + vvv)) * f;
+		float centerV = (-uv * (uuu + uvv) + uu * (uuv + vvv)) * f;
+		float radius = (float)Math.sqrt(centerU * centerU
+				+ centerV * centerV + (uu + vv) / total);
+
+		int x0 = (int)Math.max(0, x + centerU - radius);
+		int y0 = (int)Math.max(0, y + centerV - radius);
+		int x1 = (int)Math.min(w, x + centerU + radius);
+		int y1 = (int)Math.min(h, y + centerV + radius);
+		image.setRoi(new OvalRoi(x0, y0, x1 - x0, y1 - y0));
+	}
+}
