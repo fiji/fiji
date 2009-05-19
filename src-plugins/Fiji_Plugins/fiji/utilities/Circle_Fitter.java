96e84e43d3dd0625cc41badd9f90e411dc8dd33c
Johannes Schindelin <johannes.schindelin@gmx.de>
2009-05-19 19:02:44 +0200

    Fit Circle: ask for a threshold, defaulting to a sensible one
    We actually expect at most sqrt(w * h) pixels to define the circle, so
    let's calculate a threshold based on that number, but let the user adjust
    the threshold, too.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/Fiji_Plugins/fiji/utilities/Circle_Fitter.java
+++ src-plugins/Fiji_Plugins/fiji/utilities/Circle_Fitter.java
@@ -2,6 +2,7 @@ package fiji.utilities;
 
 import ij.ImagePlus;
 
+import ij.gui.GenericDialog;
 import ij.gui.OvalRoi;
 
 import ij.plugin.filter.PlugInFilter;
@@ -10,6 +11,9 @@ import ij.process.ImageProcessor;
 
 public class Circle_Fitter implements PlugInFilter {
 	ImagePlus image;
+	ImageProcessor ip;
+	float threshold;
+	int w, h;
 
 	public int setup(String arg, ImagePlus image) {
 		this.image = image;
@@ -17,14 +21,24 @@ public class Circle_Fitter implements PlugInFilter {
 	}
 
 	public void run(ImageProcessor ip) {
-		int w = ip.getWidth(), h = ip.getHeight();
+		this.ip = ip;
+		w = ip.getWidth();
+		h = ip.getHeight();
+		threshold = getDefaultThreshold();
+
+		GenericDialog gd = new GenericDialog("Fit Circle");
+		gd.addNumericField("threshold", threshold, 2);
+		gd.showDialog();
+		if (gd.wasCanceled())
+			return;
+		threshold = (float)gd.getNextNumber();
 
 		// calculate mean centroid
 		float x, y, total;
 		x = y = total = 0;
 		for (int j = 0; j < h; j++)
 			for (int i = 0; i < w; i++) {
-				float value = ip.getf(i, j);
+				float value = getValue(i, j);
 				x += i * value;
 				y += j * value;
 				total += value;
@@ -38,7 +52,7 @@ public class Circle_Fitter implements PlugInFilter {
 		uu = uv = vv = uuu = uuv = uvv = vvv = 0;
 		for (int j = 0; j < h; j++)
 			for (int i = 0; i < w; i++) {
-				float value = ip.getf(i, j);
+				float value = getValue(i, j);
 				float u = i - x;
 				float v = j - y;
 				uu += u * u * value;
@@ -62,5 +76,24 @@ public class Circle_Fitter implements PlugInFilter {
 		int x1 = (int)Math.min(w, x + centerU + radius);
 		int y1 = (int)Math.min(h, y + centerV + radius);
 		image.setRoi(new OvalRoi(x0, y0, x1 - x0, y1 - y0));
+	}
+
+	float getValue(int i, int j) {
+		return Math.max(0, ip.getf(i, j) - threshold);
+	}
+
+	float getDefaultThreshold() {
+		int w = ip.getWidth(), h = ip.getHeight();
+		float min = (float)ip.getMin(), max = (float)ip.getMax();
+		float[] histogram = new float[256];
+
+		for (int j = 0; j < h; j++)
+			for (int i = 0; i < w; i++)
+				histogram[(int)((ip.getf(i, j) - min)
+					* 255.99f / (max - min))]++;
+		int total = 0, i, want = (int)Math.sqrt(w * h);
+		for (i = 255; i > 0 && total < want; i--)
+			total += histogram[i];
+		return min + i * (max - min) / 255.99f;
 	}
 }
