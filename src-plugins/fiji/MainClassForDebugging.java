a49e708843f495753d3dce2274ff48e7bd7c9938
Johannes Schindelin <johannes.schindelin@gmx.de>
2009-08-20 01:20:03 +0200

    MainClassForDebugging: a lot of style fixes
    This class did not adher to the suggested coding style, with a few
    exceptions.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/fiji/MainClassForDebugging.java
+++ src-plugins/fiji/MainClassForDebugging.java
@@ -1,62 +1,55 @@
 package fiji;
 
 import ij.IJ;
-import ij.Menus;
 import ij.ImageJ;
+import ij.Menus;
+
 import java.io.File;
 
 public class MainClassForDebugging {
-
 	static int foo;
 	static String className;
 
 	public static void main(String args[]) {
-		if(IJ.getInstance()==null) {
+		if (IJ.getInstance() == null)
 			new ImageJ();
-		}
-		String path="";
-		int i=0;
-		while(i<args.length-1) {
-
-			path+=args[i];
-			i++;
-			path+=" ";
-		}
-		path+=args[i];
-		className=findClassName(path);
+		String path = "";
+		int i;
+		for (i = 0; i < args.length - 1; i++)
+			path += args[i] + " ";
+		path += args[i];
+		className = findClassName(path);
 		try {
-			IJ.runPlugIn(className,"");
+			IJ.runPlugIn(className, "");
 		} catch(Exception e) { e.printStackTrace(); }
 		IJ.getInstance().dispose();
 
 	}
 
 	public static String findClassName(String path) {
-		String c = path;
-		String c1= path;
-        if (c.endsWith(".java")) {
-			c = c.substring(0, c.length() - 5);
-		}
+		String c1 = path;
+		if (path.endsWith(".java"))
+			path = path.substring(0, path.length() - 5);
 		String pluginsPath = Menus.getPlugInsPath();
-		File f1=new File(pluginsPath);
+		File f1 = new File(pluginsPath);
 		if (!pluginsPath.endsWith(File.separator))
 			pluginsPath += File.separator;
-		boolean check=false;
-		while(c1.lastIndexOf(File.separator)>0) {
-			File f2=new File(c1);
-			if(f2.compareTo(f1)==0) {
-				check=true;
+		boolean check = false;
+		for (;;) {
+			int lastSlash = c1.lastIndexOf(File.separator);
+			if (lastSlash < 0)
 				break;
-			}else
-				c1=c1.substring(0,c1.lastIndexOf(File.separator));
+			if (new File(c1).equals(f1)) {
+				check = true;
+				break;
+			}
+			c1 = c1.substring(0, lastSlash);
 		}
-		if(check) {
-			c=c.substring(c1.length());
-			while(c.startsWith(File.separator))
-				c=c.substring(1);
+		if (check) {
+			path = path.substring(c1.length());
+			while (path.startsWith(File.separator))
+				path = path.substring(1);
 		}
-		return(c.replace('/','.'));
+		return path.replace('/', '.') ;
 	}
-
 }
-
