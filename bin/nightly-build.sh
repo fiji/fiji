508c59672fd02ca2b5438a774a96652271e4614f
Johannes Schindelin <johannes.schindelin@gmx.de>
2009-09-13 23:35:22 +0200

    nightly-build.sh: make useful for test-compiles, too
    After changing a lot of things, it is often nice to be able to test-compile
    before the nightly build can complain.  If the nightly build is already set
    up in the subdirectory nightly-build/, you can perform a test compile now:
    
    	./bin/nightly-build.sh HEAD
    
    (You can use any other local branch name, too; no need to switch to that
    branch first.)
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- bin/nightly-build.sh
+++ bin/nightly-build.sh
@@ -1,24 +1,37 @@
 #!/bin/sh
 
-cd "$(dirname "$0")"/..
-
-EMAIL=fiji-devel@googlegroups.com
-TMPFILE=.git/build.$$.out
-
-(git fetch origin &&
- git reset --hard origin/master &&
- git clean -q -x -f &&
- find * -type d |
- while read dir
- do
-	test ! -z "$(ls "$dir")" ||
-	rm -r "$dir" ||
-	break
- done &&
- ./Build.sh) > $TMPFILE 2>&1  &&
-rm $TMPFILE || {
-	mail -s "Fiji nightly build failed" \
-		-a "Content-Type: text/plain; charset=UTF-8" \
-		$EMAIL < $TMPFILE
-	echo Failed: see $TMPFILE
+compile () {
+	git reset --hard $1 &&
+	git clean -q -x -f &&
+	find * -type d |
+	while read dir
+	do
+		test ! -z "$(ls "$dir")" ||
+		rm -r "$dir" ||
+		break
+	done &&
+	./Build.sh
 }
+
+case "$1" in
+'')
+	cd "$(dirname "$0")"/..
+
+	EMAIL=fiji-devel@googlegroups.com
+	TMPFILE=.git/build.$$.out
+
+	(git fetch origin &&
+	 compile origin/master) > $TMPFILE 2>&1  &&
+	rm $TMPFILE || {
+		mail -s "Fiji nightly build failed" \
+			-a "Content-Type: text/plain; charset=UTF-8" \
+			$EMAIL < $TMPFILE
+		echo Failed: see $TMPFILE
+	}
+	;;
+*)
+	cd nightly-build &&
+	git fetch .. "$1" &&
+	compile FETCH_HEAD
+	;;
+esac
