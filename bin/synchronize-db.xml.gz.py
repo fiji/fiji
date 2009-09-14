fab4f98e32caebdec0f2e1572b0ea1eadbf12ea3
Johannes Schindelin <johannes.schindelin@gmx.de>
2009-09-14 19:36:39 +0200

    Fiji Updater: use synchronize-db.xml.gz.py for a different purpose
    Now that db.xml.gz has been synchronized, and the update-fiji.py that
    uses the new Fiji Updater hit 'master', we can use the same script
    for a different purpose: update db.xml.gz to contain the checksums
    of the files of previous releases.
    
    You have to call the script with a directory that contains the
    previous release for now.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- bin/synchronize-db.xml.gz.py
+++ bin/synchronize-db.xml.gz.py
@@ -1,106 +1,25 @@
 #!/bin/sh
 ''''exec "$(dirname "$0")"/../fiji --jython "$0" "$@" # (call again with fiji)'''
 
-from fiji.updater.util import Util
-import os
-import re
-import sys
+from os import system
+from sys import argv, exit
 
-prefix = '/var/www/update/'
-currentTXTPath = prefix + 'current.txt'
-xmlPath = prefix + 'db.xml.gz'
+from fiji.updater.logic import Checksummer, PluginCollection, \
+	XMLFileReader, XMLFileWriter
+from fiji.updater.util import StderrProgress
+from java.io import FileInputStream
+from java.lang.System import getProperty
+from java.util.zip import GZIPInputStream
 
-pattern = re.compile('^(.*)-([0-9]{14})$')
+if len(argv) != 2:
+	print 'Need the location of a previous installation'
+	exit(1)
 
-def addRecord(result, path):
-	match = pattern.match(path)
-	if match == None:
-		return
-	sys.stderr.write("\rAdding record for " + path + '... ')
-	sha1 = Util.getDigest(match.group(1), prefix + path)
-	size = os.stat(prefix + path).st_size
-	if match.group(1) in result:
-		result[match.group(1)][match.group(2)] = [sha1, size]
-	else:
-		result[match.group(1)] = dict({match.group(2): [sha1, size]})
+dbPath = getProperty('fiji.dir') + '/db.xml.gz'
+reader = XMLFileReader(GZIPInputStream(FileInputStream(dbPath)))
 
-def getDigests(result, dir):
-	names = os.listdir(prefix + dir)
-	names.sort()
-	for name in names:
-		if dir == '':
-			path = name
-		else:
-			path = dir + '/' + name
-		prefixed = prefix + path
-		if os.path.isdir(prefixed):
-			getDigests(result, path)
-			continue
-		addRecord(result, path)
-
-def readCurrentTXT(path):
-	f = open(path)
-	result = [line.split(' ')[0] for line in f.readlines()]
-	f.close()
-	return result
-
-def readXML(path):
-	return ''.join(os.popen('gunzip < ' + path).readlines())
-
-def writeXML(path, xml):
-	f = os.popen('gzip -9 > ' + path, 'w')
-	f.write(xml)
-	f.close()
-
-def insertRecord(xml, plugin, record):
-	pluginTag = '<plugin filename="' + plugin + "\">\n"
-	off = xml.find(pluginTag)
-	if off < 0:
-		off = xml.rfind('</plugin>')
-		insert = "</plugin>\n    " + pluginTag
-		xml = xml[:off] + insert + '    ' + xml[off:]
-		off = off + len(insert)
-		endOff = off
-		versionBody = None
-	else:
-		off = off + len(pluginTag)
-		versionOff = xml.find('<version ', off)
-		bodyOff = xml.find(">\n", versionOff) + 2
-		if xml[bodyOff - 3] == '/':
-			versionBody = None
-			bodyEndOff = bodyOff
-		else:
-			bodyEndOff = xml.find("        </version>\n", bodyOff)
-			versionBody = xml[bodyOff:bodyEndOff]
-		endOff = xml.find('    </plugin>', bodyEndOff)
-	dates = record.keys()
-	dates.sort()
-	dates.reverse()
-	insert = None
-	if not plugin in currentTXT:
-		insert = ''
-	for date in dates:
-		metadata = record[date]
-		if insert == None:
-			insert = '        <version timestamp="' + date \
-				+ '" checksum="' + metadata[0] \
-				+ '" filesize="' + str(metadata[1]) + '\"'
-			if versionBody == None:
-				insert = insert + "/>\n"
-			else:
-				insert = insert + ">\n" + versionBody \
-					 + "        </version>\n"
-		else:
-			insert = insert \
-				+ '        <previous-version timestamp="' \
-				+ date + '" checksum="' + metadata[0] + "\"/>\n"
-	xml = xml[:off] + insert + xml[endOff:]
-	return xml
-
-currentTXT = readCurrentTXT(currentTXTPath)
-xml = readXML(xmlPath)
-digests = dict()
-getDigests(digests, '')
-for plugin in digests.keys():
-	xml = insertRecord(xml, plugin, digests[plugin])
-writeXML(xmlPath, xml)
+progress = StderrProgress()
+checksummer = Checksummer(progress)
+checksummer.updateFromPreviousInstallation(argv[1])
+XMLFileWriter.writeAndValidate(dbPath[:-3])
+system('gzip -9f ' + dbPath[:-3])
