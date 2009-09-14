5b23b70fad7b5bcf1a91f46e7d16b87416b5fa94
Johannes Schindelin <johannes.schindelin@gmx.de>
2009-09-14 20:00:31 +0200

    WIP: synchronize-db.xml.gz: allow specifying .tar.bz2 and .dmg files
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- bin/synchronize-db.xml.gz.py
+++ bin/synchronize-db.xml.gz.py
@@ -1,8 +1,10 @@
 #!/bin/sh
 ''''exec "$(dirname "$0")"/../fiji --jython "$0" "$@" # (call again with fiji)'''
 
-from os import system
+from os import listdir, mkdir, remove, rmdir, system
+from os.path import isdir
 from sys import argv, exit
+from tempfile import mktemp
 
 from fiji.updater.logic import Checksummer, PluginCollection, \
 	XMLFileReader, XMLFileWriter
@@ -15,6 +17,14 @@ if len(argv) != 2:
 	print 'Need the location of a previous installation'
 	exit(1)
 
+tmpDirectory = None
+if argv[1].endswith('.tar.bz2'):
+	tmpDirectory = mktemp()
+	mkdir(tmpDirectory)
+	system('tar -C ' + tmpDirectory + ' --strip-components 1 -xvf ' \
+		+ argv[1])
+	argv[1] = tmpDirectory
+
 dbPath = getProperty('fiji.dir') + '/db.xml.gz'
 reader = XMLFileReader(GZIPInputStream(FileInputStream(dbPath)))
 
@@ -23,3 +33,14 @@ checksummer = Checksummer(progress)
 checksummer.updateFromPreviousInstallation(argv[1])
 XMLFileWriter.writeAndValidate(dbPath[:-3])
 system('gzip -9f ' + dbPath[:-3])
+
+if tmpDirectory != None:
+	def rmRecursively(file):
+		if isdir(file):
+			for f in listdir(file):
+				rmRecursively(file + '/' + f)
+			rmdir(file)
+		else:
+			remove(file)
+	rmRecursively(tmpDirectory)
+	print tmpDirectory
