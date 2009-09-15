c5bd78d767442a4e7e1643fb3acb645b8f1b70c4
Johannes Schindelin <johannes.schindelin@gmx.de>
2009-09-15 18:40:29 +0200

    update-fiji.py: automatically strip 'precompiled/' prefix
    When a developer wants to upload a launcher, chances are that she
    uses the precompiled/ prefix, because that is where the files with
    the platform-specific suffixes live.
    
    But the updater actually wants the name as the user sees it, so
    the prefix needs to be stripped.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- bin/update-fiji.py
+++ bin/update-fiji.py
@@ -19,6 +19,13 @@ if argv[1] == '--upload-to':
 else:
 	updateDirectory = '/var/www/update/'
 
+def stripPrecompiled(string):
+	if string.startswith('precompiled/'):
+		return string[12:]
+	return string
+
+files = [stripPrecompiled(file) for file in argv[1:]]
+
 Updater.MAIN_URL = 'file:' + updateDirectory
 downloader = XMLFileDownloader()
 downloader.start()
@@ -26,18 +33,12 @@ reader = XMLFileReader(downloader.getInputStream())
 
 progress = StderrProgress()
 checksummer = Checksummer(progress)
-if len(argv) == 1:
+plugins = PluginCollection.getInstance()
+if len(files) == 0:
 	checksummer.updateFromLocal()
 else:
-	checksummer.updateFromLocal(argv[1:])
-
-# mark as update
-collection = PluginCollection.getInstance()
-now = Util.timestamp(Calendar.getInstance())
-if len(argv) == 1:
-	plugins = collection
-else:
-	plugins = [collection.getPlugin(file) for file in argv[1:]]
+	checksummer.updateFromLocal(files)
+	plugins = [plugins.getPlugin(file) for file in files]
 	# TODO: add dependencies
 for plugin in plugins:
 	for action in [Action.UPLOAD, Action.REMOVE]:
