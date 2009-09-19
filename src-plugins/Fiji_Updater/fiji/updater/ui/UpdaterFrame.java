749c8f3badde43be3a86d5570d4cfe0f94c9c412
Johannes Schindelin <johannes.schindelin@gmx.de>
2009-09-19 20:45:03 +0200

    Fiji Updater: handle the Escape key to close the window
    The Escape key now hits the exact same code path as clicking the
    Close/Cancel button.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/Fiji_Updater/fiji/updater/ui/UpdaterFrame.java
+++ src-plugins/Fiji_Updater/fiji/updater/ui/UpdaterFrame.java
@@ -30,6 +30,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -44,6 +45,7 @@ import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -240,6 +242,10 @@ public class UpdaterFrame extends JFrame
 		table.getModel().addTableModelListener(this);
 
 		pack();
+
+		SwingTools.addAccelerator(cancel, (JComponent)getContentPane(),
+				cancel.getActionListeners()[0],
+				KeyEvent.VK_ESCAPE, 0);
 	}
 
 	public void dispose() {
