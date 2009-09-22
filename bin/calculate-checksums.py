91bd6512d6e51e93e60749205a24eb72f5218ea9
Johannes Schindelin <johannes.schindelin@gmx.de>
2009-09-22 05:11:59 +0200

    calculate-checksums: optionally take filenames as parameters
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- bin/calculate-checksums.py
+++ bin/calculate-checksums.py
@@ -1,6 +1,8 @@
 #!/bin/sh
 ''''exec "$(dirname "$0")"/../fiji --jython "$0" "$@" # (call again with fiji)'''
 
+from sys import argv
+
 from fiji.updater.logic import Checksummer, PluginCollection
 from fiji.updater.util import StderrProgress
 from java.lang.System import getProperty
@@ -9,7 +11,10 @@ dbPath = getProperty('fiji.dir') + '/db.xml.gz'
 
 progress = StderrProgress()
 checksummer = Checksummer(progress)
-checksummer.updateFromLocal()
+if len(argv) > 1:
+	checksummer.updateFromLocal(argv[1:])
+else:
+	checksummer.updateFromLocal()
 plugins = PluginCollection.getInstance()
 plugins.sort()
 for plugin in plugins:
