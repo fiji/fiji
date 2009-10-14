409ba8681a3549e42a3cc1836fb4057ed9321b14
Johannes Schindelin <johannes.schindelin@gmx.de>
2009-10-15 01:06:18 +0200

    upload-mirrors: we have a SourceForge mirror now, too
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- bin/upload-mirrors.sh
+++ bin/upload-mirrors.sh
@@ -17,3 +17,8 @@ curl $URL/wiki/index.php/Downloads |
 sed -e "s|$URL/downloads|$MIRROR/downloads|g" \
 	-e 's~\("\|(\)/\(wiki\|fiji-\)~\1'$URL'/\2~g' |
 ssh $HOST "cat > $DIR/index.html"
+
+# Sourceforge
+DIRS=$(echo "$DOWNLOADS" | sed "s/\(downloads\/[^\/]*\)\/.*/\1/" | sort | uniq |
+	grep -v /apt$)
+rsync -e ssh -avP $DIRS frs.sf.net:/home/frs/project/f/fi/fiji-bi/fiji/
