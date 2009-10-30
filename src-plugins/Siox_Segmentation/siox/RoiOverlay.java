399af2f198a70730e72d3466b13cef519eb82449
Ignacio Arganda-Carreras <iarganda@tomancak-pc-4.(none)>
2009-10-30 18:30:23 +0100

    Finished paint method.

--- src-plugins/Siox_Segmentation/siox/RoiOverlay.java
+++ src-plugins/Siox_Segmentation/siox/RoiOverlay.java
@@ -1,6 +1,5 @@
 package siox;
 
-import ij.IJ;
 import ij.gui.Roi;
 import ij.gui.ShapeRoi;
 import ij.gui.ShapeRoiHelper;
@@ -10,14 +9,16 @@ import java.awt.Color;
 import java.awt.Composite;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.Shape;
+import java.awt.geom.AffineTransform;
 
 import fiji.util.gui.OverlayedImageCanvas.Overlay;
 
 public class RoiOverlay implements Overlay {
 	Roi roi = null;
-	Composite composite = AlphaComposite.getInstance(AlphaComposite.DST_OVER);
 	Color color = Roi.getColor();
+	Composite composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER);
 	
 	public RoiOverlay(){}
 	
@@ -32,14 +33,21 @@ public class RoiOverlay implements Overlay {
 	public void paint(Graphics g, int x, int y, double magnification) {
 		if ( null == this.roi )
 			return;
-		
-		// TODO Auto-generated method stub
-		Shape shape = ShapeRoiHelper.getShape(new ShapeRoi(roi), g, x, y, magnification);
+		Shape shape = ShapeRoiHelper.getShape(new ShapeRoi(roi));
+		final Rectangle roiBox = roi.getBounds();
 		final Graphics2D g2d = (Graphics2D)g;
+		final AffineTransform orig = g2d.getTransform();
+		final AffineTransform at = new AffineTransform();
+		at.scale( magnification, magnification );
+		at.translate( roiBox.x - x, roiBox.y - y );
+		at.concatenate( orig );
+		
+		g2d.setTransform( at );
 		g2d.setComposite( this.composite );
 		g2d.setColor( this.color );
-		g2d.draw(shape);
+		g2d.fill(shape);
 		
+		g2d.setTransform( orig );
 	}
 	
 	public void setRoi(Roi roi)
