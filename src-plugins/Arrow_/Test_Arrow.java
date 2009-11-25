3244dc5a204682cf3884c1a388862644f4fbcd3b
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2009-11-25 11:47:27 +0100

    AbstractTool: reimplemented an arrow example
    Following Johannes advice, made an arrow from a GeneralPath.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src-plugins/Arrow_/Test_Arrow.java
+++ src-plugins/Arrow_/Test_Arrow.java
@@ -1,26 +1,41 @@
-import fiji.roi.Arrow;
 import ij.IJ;
 import ij.ImagePlus;
 import ij.WindowManager;
+import ij.gui.ImageCanvas;
+import ij.gui.ShapeRoi;
 
 import java.awt.BasicStroke;
-import java.awt.Color;
 import java.awt.event.MouseEvent;
+import java.awt.geom.GeneralPath;
 
 public class Test_Arrow extends AbstractTool {
 	
-	Arrow arr;
+	GeneralPath arrow;
+	/**
+	 * Double array that contains the coordinates in pixels of the arrow points.
+	 */
+	double[] points = new double[2*5];
+	/**
+	 * Length of the arrow head, in pixels. Could be set by user/
+	 */
+	double length = 10.0;
+	/**
+	 * Tip angle (in degrees) of the arrow head.
+	 */
+	double tip = 60.0;
+	/**
+	 * Base angle (in degrees) of the arrow head.
+	 */
+	double base = 20.0;
 	ImagePlus imp;
 	BasicStroke stroke = new BasicStroke();
 	
 	public void run(String arg) {
 		super.run(arg);
 		imp = WindowManager.getCurrentImage();
-		arr = new Arrow(0, 0, 0, 0, 10, 30, 60, false);
-		imp.setDisplayList(arr, Color.GREEN, stroke);
+		arrow = new GeneralPath();
 		if (toolID >= 0)
-			IJ.showStatus("selected " + getToolName()
-					+ " Tool(" + toolID + ")");
+			IJ.showStatus("selected " + getToolName() + " Tool(" + toolID + ")");
 	}
 
 	public String getToolName() {
@@ -32,14 +47,47 @@ public class Test_Arrow extends AbstractTool {
 	}
 
 	public void handleMousePress(MouseEvent e) {
-		IJ.showStatus("press noticed by " + getToolName() + " Tool");
-		arr.setStartPoint(e.getPoint());
+		ImageCanvas canvas = (ImageCanvas) e.getSource();
+		points[0] = canvas.offScreenXD(e.getX());        
+		points[1] = canvas.offScreenYD(e.getY());
 	}
 
 	public void handleMouseDrag(MouseEvent e) {
-		IJ.showStatus("moved to " + e.getX() + ", " + e.getY());	
-		arr.setEndPoint(e.getPoint());
-		imp.setDisplayList(arr, Color.GREEN, stroke);
-		
+		ImageCanvas canvas = (ImageCanvas) e.getSource();
+		points[2*3] = canvas.offScreenXD(e.getX());        
+		points[2*3+1] = canvas.offScreenYD(e.getY()); 
+		calculatePoints();
+		makePath();	
+		imp.setRoi(new ShapeRoi(arrow));
 	}
+	
+	private void calculatePoints() {
+		// P1 = P3 - length*alpha
+		double alpha = Math.atan2(points[2*3+1] - points[1], points[2*3] - points[0]);
+		points[1*2]   = points[2*3]   - length*Math.cos(alpha);
+		points[1*2+1] = points[2*3+1] - length*Math.sin(alpha);
+
+		// SL (sideLength) = length*cos(tip) + length * sin(tip) * cos(base-tip) / sin(base-tip) 
+		double SL = length*Math.cos(tip) + length*Math.sin(tip) * Math.cos(base-tip) / Math.sin(base-tip);
+
+		// P2 = P3 - SL*alpha+tip
+		points[2*2]   = points[2*3]   - SL*Math.cos(alpha+tip);
+		points[2*2+1] = points[2*3+1] - SL*Math.sin(alpha+tip);
+
+		// P4 = P3 - SL*alpha-tip
+		points[2*4]   = points[2*3]   - SL*Math.cos(alpha-tip);
+		points[2*4+1] = points[2*3+1] - SL*Math.sin(alpha-tip);		
+	}
+	
+	private void makePath() {
+		arrow.reset();
+		// Line
+		arrow.moveTo(points[0], points[1]);
+		arrow.lineTo(points[2*1], points[2*1+1]);
+		arrow.lineTo(points[2*2], points[2*2+1]);
+		arrow.lineTo(points[2*3], points[2*3+1]);
+		arrow.lineTo(points[2*4], points[2*4+1]);
+		arrow.lineTo(points[2*1], points[2*1+1]);
+	}
+
 }
