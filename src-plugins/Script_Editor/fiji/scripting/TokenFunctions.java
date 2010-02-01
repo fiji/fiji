c29231c3770a7058bbb5912f1c0b85909c2c9957
Johannes Schindelin <johannes.schindelin@gmx.de>
2010-02-01 19:20:02 +0100

    Script Editor: refactor token parsing code
    In upcoming patches, functions will be added that also need to sift through
    all tokens; let's unify that code.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/Script_Editor/fiji/scripting/TokenFunctions.java
+++ src-plugins/Script_Editor/fiji/scripting/TokenFunctions.java
@@ -1,28 +1,19 @@
 package fiji.scripting;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
 import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
 import org.fife.ui.rsyntaxtextarea.Token;
 
-public class TokenFunctions {
+public class TokenFunctions implements Iterable<Token> {
 	RSyntaxTextArea textArea;
 
 	public TokenFunctions(RSyntaxTextArea textArea) {
 		this.textArea = textArea;
-	}
-
-	public String getClassName() {
-		boolean classSeen = false;
-		for (int i = 0; i < textArea.getLineCount(); i++) {
-			Token token = textArea.getTokenListForLine(i);
-			while (token != null) {
-				if (isClass(token))
-					classSeen = true;
-				else if (classSeen && isIdentifier(token))
-					return getText(token);
-				token = token.getNextToken();
-			}
-		}
-		return null;
 	}
 
 	public static boolean tokenEquals(Token token, char[] text) {
@@ -35,17 +26,83 @@ public class TokenFunctions {
 		return true;
 	}
 
+	public static boolean isIdentifier(Token token) {
+		if (token.type != token.IDENTIFIER)
+			return false;
+		if (!Character.isJavaIdentifierStart(token.text[token.textOffset]))
+			return false;
+		for (int i = 1; i < token.textCount; i++)
+			if (!Character.isJavaIdentifierStart(token.text[token.textOffset + i]))
+				return false;
+		return true;
+	}
+
+	public static String getText(Token token) {
+		if (token.text == null)
+			return "";
+		return new String(token.text,
+				token.textOffset, token.textCount);
+	}
+
+	class TokenIterator implements Iterator<Token> {
+		int line = -1;
+		Token current, next;
+
+		public boolean hasNext() {
+			if (next == null)
+				getNextToken();
+			return next != null;
+		}
+
+		public Token next() {
+			current = next;
+			next = null;
+			return current;
+		}
+
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+
+		void getNextToken() {
+			if (current != null) {
+				next = current.getNextToken();
+				if (next != null)
+					return;
+			}
+
+			while (next == null) {
+				if (++line >= textArea.getLineCount())
+					return;
+
+				next = textArea.getTokenListForLine(line);
+			}
+		}
+	}
+
+	public Iterator<Token> iterator() {
+		return new TokenIterator();
+	}
+
+	public static boolean isDot(Token token) {
+		return token.type == token.IDENTIFIER && token.textCount == 1
+			&& token.text[token.textOffset] == '.';
+	}
+
+	/* The following methods are Java-specific */
+
 	public final static char[] classCharacters = {'c', 'l', 'a', 's', 's'};
 	public static boolean isClass(Token token) {
 		return tokenEquals(token, classCharacters);
 	}
 
-	public static boolean isIdentifier(Token token) {
-		return token.type == token.IDENTIFIER;
-	}
-
-	public static String getText(Token token) {
-		return new String(token.text,
-				token.textOffset, token.textCount);
+	public String getClassName() {
+		boolean classSeen = false;
+		for (Token token : this)
+			if (isClass(token))
+				classSeen = true;
+			else if (classSeen && isIdentifier(token))
+				return getText(token);
+		return null;
 	}
 }
