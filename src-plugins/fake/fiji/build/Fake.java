44fe8ec810cda9ffb4b73be6ca8a49ed9ebae747
Johannes Schindelin <johannes.schindelin@gmx.de>
2010-03-09 19:02:51 +0100

    Fiji Build: exclude headless.jar from the classpath
    The headless.jar style classes are not good enough for most of Fiji, so
    let's not use them.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/fake/fiji/build/Fake.java
+++ src-plugins/fake/fiji/build/Fake.java
@@ -157,7 +157,8 @@ public class Fake {
 		 * include all plugin's jars...
 		 */
 		// expandGlob(fijiHome + "plugins/**/*.jar", jars, cwd);
-		expandGlob(fijiHome + "misc/**/*.jar", jars, cwd);
+		if (new File(fijiHome + "misc/Fiji.jar").exists())
+			jars.add(fijiHome + "misc/Fiji.jar");
 		expandGlob(fijiHome + "jars/**/*.jar", jars, cwd);
 		if (getPlatform().startsWith("win")) {
 			String[] paths =
