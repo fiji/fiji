15ee371b02866e460d16a88918e89b83ff7c5ac4
Johannes Schindelin <johannes.schindelin@gmx.de>
2010-03-21 17:28:22 +0100

    Fiji Updater: do not mistake batik.jar for a provider of JavaScript
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/Fiji_Updater/fiji/updater/util/Class2JarFilesMap.java
+++ src-plugins/Fiji_Updater/fiji/updater/util/Class2JarFilesMap.java
@@ -63,7 +63,8 @@ public class Class2JarFilesMap extends HashMap<String, ArrayList<String>> {
 		if (jar.endsWith("/batik.jar"))
 			return name.startsWith("org.xml.") ||
 				name.startsWith("org.w3c.") ||
-				name.startsWith("javax.xml.");
+				name.startsWith("javax.xml.") ||
+				name.startsWith("org.mozilla.javascript.");
 		if (jar.endsWith("/jython.jar") || jar.endsWith("/jruby.jar"))
 			return name.startsWith("com.sun.jna.") ||
 				name.startsWith("jline.");
