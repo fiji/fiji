33234023fa3e8014782970c3bdba681616b691b7
Ignacio Arganda-Carreras <iarganda@iarganda-laptop.(none)>
2010-03-22 16:02:02 +0100

    Added shear, scale and anisotropy correction options for the initial affine matrices.

--- src-plugins/bUnwarpJ_/bunwarpj/Param.java
+++ src-plugins/bUnwarpJ_/bunwarpj/Param.java
@@ -56,6 +56,14 @@ public class Param {
 	/** maximum scale deformation choices */
 	protected String[] sMaxScaleDeformationChoices = { "Very Coarse", "Coarse", "Fine", "Very Fine", "Super Fine" };
 	
+	// Initial affine matrix pre-process
+	/** percentage of shear correction in initial matrix */
+	private double shearCorrection = 0.0;
+	/** percentage of scale correction in initial matrix */
+	private double scaleCorrection = 0.0;
+	/** percentage of anisotropy correction in initial matrix */
+	private double anisotropyCorrection = 0.0;
+	
 	/**
 	 * Empty constructor
 	 */
@@ -168,6 +176,30 @@ public class Param {
 						  "Image weight = " + imageWeight + "\n" +
 						  "Consistency weight = " + consistencyWeight + "\n" +
 						  "Stopping threshold = " + stopThreshold);
+	}
+
+	public void setShearCorrection(double shearCorrection) {
+		this.shearCorrection = shearCorrection;
+	}
+
+	public double getShearCorrection() {
+		return shearCorrection;
+	}
+
+	public void setScaleCorrection(double scaleCorrection) {
+		this.scaleCorrection = scaleCorrection;
+	}
+
+	public double getScaleCorrection() {
+		return scaleCorrection;
+	}
+
+	public void setAnisotropyCorrection(double anisotropyCorrection) {
+		this.anisotropyCorrection = anisotropyCorrection;
+	}
+
+	public double getAnisotropyCorrection() {
+		return anisotropyCorrection;
 	}
 	
 	
