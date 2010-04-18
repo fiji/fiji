21118225f85f44cf5aac3c144e5fdad8da8017f3
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2010-04-18 20:24:16 +0200

    IEP2: implemented square root function
    Signed-off-by: Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>

---
+++ src-plugins/Image_Expression_Parser/fiji/expressionparser/function/ImgLibSquareRoot.java
@@ -0,0 +1,29 @@
+package fiji.expressionparser.function;
+
+import mpicbg.imglib.type.numeric.RealType;
+
+
+public final class ImgLibSquareRoot <T extends RealType<T>> extends SingleOperandPixelBasedAbstractFunction<T> {
+
+	public static final String DOCUMENTATION_STRING = 
+		"<h3>Element-wise square root</h3>" +
+		"This function computes the square root of an ImgLib image, taking " +
+		"each pixel as its operand. " +
+		"Calculations are done using Math.sqrt";
+
+	@Override
+	public final float evaluate(T alpha) {
+		return (float) Math.sqrt(alpha.getRealDouble());
+	}
+
+	@Override
+	public String getDocumentationString() {
+		return DOCUMENTATION_STRING;
+	}
+
+	@Override
+	public String getFunctionString() {
+		return "sqrt";
+	}
+
+}
