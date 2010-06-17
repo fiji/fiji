65a0e5c7874aa10efdc8c8728a74749fadf31d15
Daniel James White <dan@chalkie.org.uk>
2010-06-17 14:49:54 +0200

    Coloc 2: Add tagging of some special results to the DataContainer
    That is used for allowing algorithms to look after specific data in
    the DataContainer and test if it has been calculated.
    
    Signed-off-by: Tom Kazimiers <tom@voodo-arts.net>

--- src-plugins/Colocalisation_Analysis/DataContainer.java
+++ src-plugins/Colocalisation_Analysis/DataContainer.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import mpicbg.imglib.image.Image;
 import mpicbg.imglib.type.numeric.RealType;
@@ -15,6 +18,9 @@ import mpicbg.imglib.type.numeric.RealType;
  */
 public class DataContainer<T extends RealType<T>> implements Iterable<Result> {
 
+	// Tags for later finding the right piece of result data
+	enum DataTags { MeanCh1, MeanCh2 };
+
 	// The source images that the results are based on
 	Image<T> sourceImage1, sourceImage2;
 	// The channels of the source images that the result relate to
@@ -27,10 +33,14 @@ public class DataContainer<T extends RealType<T>> implements Iterable<Result> {
 	double ch1MinThreshold, ch1MaxThreshold, ch2MinThreshold, ch2MaxThreshold;
 	// The container of the results
 	List<Result> resultsObjectList = new ArrayList<Result>();
+	// Use a Map to make the connection between the results and the label we give the result to be tagged.
+	Map<DataTags, Result> taggedResults = Collections.synchronizedMap(new HashMap<DataTags, Result>());
 
 	/**
 	 * Creates a new {@link DataContainer} for a specific set of image and
 	 * channel combination.
+	 * We create default thresholds here that are the max and min of the
+	 * type of the source image channels.
 	 *
 	 * @param src1 The channel one image source
 	 * @param src2 The channel two image source
@@ -61,6 +71,40 @@ public class DataContainer<T extends RealType<T>> implements Iterable<Result> {
 	 */
 	public void add(Result result) {
 		resultsObjectList.add(result);
+	}
+
+	/**
+	 * Adds a {@link Result} to the container and tags
+	 * it with the given {@link DataTags} tag.
+	 * @param result The result to be stored
+	 * @param tag The tag for the new result
+	 * @return true if the result has been added, false otherwise
+	 */
+	public boolean add(Result result, DataTags tag) {
+		/* if a result tagged like this already exists
+		 * return false
+		 */
+		if (taggedResults.containsKey(tag)) {
+			return false;
+		}
+		// add result to the tagging map
+		taggedResults.put(tag, result);
+		// add result to the data store
+		add(result);
+
+		return true;
+	}
+
+	/**
+	 * Gets a stored result based on its tag.
+	 * @param tag The tag of the result looked for
+	 * @return The stored result or null if not found
+	 */
+	public Result get(DataTags tag) {
+		if (taggedResults.containsKey(tag))
+			return taggedResults.get(tag);
+		else
+			return null;
 	}
 
 	/**
