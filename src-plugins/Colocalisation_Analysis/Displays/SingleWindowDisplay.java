01b029ed5513f82010908cd7f346ca07b9a86a09
Daniel James White <dan@chalkie.org.uk>
2010-07-07 15:31:15 +0200

    Coloc 2: make SingleImageDisplay show the result image, and set it to fire LUT, and set display max to max value of the image

--- src-plugins/Colocalisation_Analysis/Displays/SingleWindowDisplay.java
+++ src-plugins/Colocalisation_Analysis/Displays/SingleWindowDisplay.java
@@ -17,10 +17,12 @@ import javax.swing.JLabel;
 
 import mpicbg.imglib.image.display.imagej.ImageJFunctions;
 
+import ij.IJ;
 import ij.ImagePlus;
 import ij.gui.ImageWindow;
 import ij.gui.NewImage;
 import ij.measure.ResultsTable;
+import ij.process.ImageProcessor;
 
 /**
  * This class displays the container contents in one single window
@@ -103,6 +105,10 @@ public class SingleWindowDisplay extends ImageWindow implements Display, ItemLis
 		}
 
 		setup();
+		if (listOfImageResults.size() > 0) {
+			drawImageResult(listOfImageResults.get(0));
+		}
+
 		this.show();
 	}
 
@@ -123,12 +129,20 @@ public class SingleWindowDisplay extends ImageWindow implements Display, ItemLis
 		}
 	}
 
-	public void itemStateChanged(ItemEvent e) {
-		Result.ImageResult result = (Result.ImageResult)(e.getItem());
+	protected void drawImageResult(Result.ImageResult result) {
 		ImagePlus imp = ImageJFunctions.displayAsVirtualStack( result.getData() );
-		if (this.imp != imp){
-			this.imp = imp;
-			System.out.println("ouch!");
+		this.imp.setProcessor(imp.getProcessor());
+		ImageProcessor ip = this.imp.getProcessor();
+		double max = BasicImageStats.getImageMax(result.getData());
+		this.imp.setDisplayRange(0.0, max);
+		IJ.run(this.imp, "Fire", null);
+		this.imp.updateAndDraw();
+	}
+
+	public void itemStateChanged(ItemEvent e) {
+		if (e.getStateChange() == ItemEvent.SELECTED) {
+			Result.ImageResult result = (Result.ImageResult)(e.getItem());
+			drawImageResult(result);
 		}
 	}
 }
