609566cb2cca0496b5fe4c984d2e2208a4ba2db4
Tom Kazimiers <tom@voodoo-arts.net>
2010-07-09 23:28:55 +0200

    Coloc 2: Make developement image launch code more portable

--- src-plugins/Colocalisation_Analysis/Coloc_2.java
+++ src-plugins/Colocalisation_Analysis/Coloc_2.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.io.File;
 
 import mpicbg.imglib.image.Image;
 import mpicbg.imglib.image.ImagePlusAdapter;
 import mpicbg.imglib.type.numeric.RealType;
 import ij.IJ;
 import ij.ImagePlus;
+import ij.WindowManager;
 import ij.plugin.PlugIn;
 
 /**
@@ -19,12 +21,19 @@ public class Coloc_2<T extends RealType<T>> implements PlugIn {
 	Image<T> img1, img2;
 
 	public void run(String arg0) {
+		// Development code
 		ImagePlus imp1 = IJ.openImage("/Users/dan/Documents/Dresden/ipf/colocPluginDesign/red.tif");
+		if (imp1 == null)
+			imp1 = WindowManager.getImage(1);
 		img1 = ImagePlusAdapter.wrap(imp1);
+
 		ImagePlus imp2 = IJ.openImage("/Users/dan/Documents/Dresden/ipf/colocPluginDesign/green.tif");
+		if (imp2 == null)
+			imp2 = WindowManager.getImage(2);
 		img2 = ImagePlusAdapter.wrap(imp2);
 
 		int theImg1Channel = 1, theImg2Channel = 1;
+		// Development code end
 
 		// create a new container for the selected images and channels
 		DataContainer container = new DataContainer(img1, img2, theImg1Channel, theImg2Channel);
