1477ce1b50c0a0b6baf58f2327fa1491780ab687
Tom Kazimiers <tom@voodoo-arts.net>
2010-07-21 18:35:15 +0200

    Coloc 2: Static methods for Pearson's calculation algorithm
    To be able to use Person's calculation from within other plugins the
    actual calculation methods have been made static. Furthermore a second
    implementation has been added which takes upper threshold limits into
    consideration.

--- src-plugins/Colocalisation_Analysis/Algorithms/PearsonsCorrelation.java
+++ src-plugins/Colocalisation_Analysis/Algorithms/PearsonsCorrelation.java
@@ -70,6 +70,10 @@ public class PearsonsCorrelation<T extends RealType<T>> extends Algorithm {
 		}
 		double pearsonNumerator = Math.sqrt(ch1diffSquaredSum * ch2diffSquaredSum);
 
+		// close the cursors
+		cursor1.close();
+		cursor2.close();
+
 		//put the result into the DataContainer
 		Result result = new Result.SimpleValueResult("Pearson's Correlation (Classic)", pearsonDenominator / pearsonNumerator);
 		container.add(result);
@@ -80,6 +84,14 @@ public class PearsonsCorrelation<T extends RealType<T>> extends Algorithm {
 		Image<T> img1 = container.getSourceImage1();
 		Image<T> img2 = container.getSourceImage2();
 
+		double r = fastPearsons(img1, img2);
+
+		//put the result into the DataContainer
+		Result result = new Result.SimpleValueResult("Pearson's Correlation (Fast)", r);
+		container.add(result);
+	}
+
+	public static <T extends RealType<T>> double fastPearsons(Image<T> img1, Image<T> img2) {
 		// get the cursors for iterating through pixels in images
 		Cursor<T> cursor1 = img1.createCursor();
 		Cursor<T> cursor2 = img2.createCursor();
@@ -102,13 +114,83 @@ public class PearsonsCorrelation<T extends RealType<T>> extends Algorithm {
 			N++;
 		}
 
-		double pearsons1 = sumProduct1_2 - (sum1*sum2/N);
-		double pearsons2 = sum1squared - (sum1*sum1/N);
-		double pearsons3 = sum2squared - (sum2*sum2/N);
+		// close the cursors
+		cursor1.close();
+		cursor2.close();
+
+		// for faster computation, have the inverse of N available
+		double invN = 1.0 / N;
+
+		double pearsons1 = sumProduct1_2 - (sum1 * sum2 * invN);
+		double pearsons2 = sum1squared - (sum1 * sum1 * invN);
+		double pearsons3 = sum2squared - (sum2 * sum2 * invN);
+		double pearsonsR = pearsons1 / (Math.sqrt(pearsons2 * pearsons3));
+
+		return pearsonsR;
+	}
+
+	/**
+	 * Calculates Person's R value by using a fast implementation of the
+	 * algorithm. This method allows the specification of upper limits for
+	 * the pixel values for each channel. One can also define if pixels
+	 * that are zero in both channels should be discarded or not.
+	 *
+	 * @param <T> The image base type
+	 * @param img1 Channel one
+	 * @param img2 Channel two
+	 * @param ch1ThreshMax Upper limit of channel one
+	 * @param ch2ThreshMax Upper limit of channel two
+	 * @param discardZeroPixels defines if zero pixels should be discarded
+	 * @return Person's R value
+	 */
+	public static <T extends RealType<T>> double fastPearsons(Image<T> img1, Image<T> img2,
+			double ch1ThreshMax, double ch2ThreshMax, boolean discardZeroPixels) {
+		// get the cursors for iterating through pixels in images
+		Cursor<T> cursor1 = img1.createCursor();
+		Cursor<T> cursor2 = img2.createCursor();
+		double sum1 = 0.0, sum2 = 0.0, sumProduct1_2 = 0.0, sum1squared= 0.0, sum2squared = 0.0;
+		// the total amount of pixels that have been taken into consideration
+		int N = 0;
+		// the amount of pixels that are zero in both channels
+		int Nzero = 0;
+		while (cursor1.hasNext() && cursor2.hasNext()) {
+			cursor1.fwd();
+			cursor2.fwd();
+			T type1 = cursor1.getType();
+			double ch1 = type1.getRealDouble();
+			T type2 = cursor2.getType();
+			double ch2 = type2.getRealDouble();
+			// is either channel one or channel two within the limits?
+			if ( (ch1 < ch1ThreshMax) || (ch2 < ch2ThreshMax)) {
+				// is the current pixels combination a zero pixel?
+				if (Math.abs(ch1 + ch2) < 0.00001)
+					Nzero++;
+
+				sum1 += ch1;
+				sumProduct1_2 += (ch1 * ch2);
+				sum1squared += (ch1 * ch1);
+				sum2squared += (ch2 *ch2);
+				sum2 += ch2;
+				N++;
+			}
+		}
+
+		// close the cursors
+		cursor1.close();
+		cursor2.close();
+
+		// if told to do so, discard the zero pixels
+		if (discardZeroPixels)
+			N = N - Nzero;
+
+		// for faster computation, have the inverse of N available
+		double invN = 1.0 / N;
+
+		double pearsons1 = sumProduct1_2 - (sum1 * sum2 * invN);
+		double pearsons2 = sum1squared - (sum1 * sum1 * invN);
+		double pearsons3 = sum2squared - (sum2 * sum2 * invN);
 		double pearsonsR = pearsons1/(Math.sqrt(pearsons2*pearsons3));
 
-		//put the result into the DataContainer
-		Result result = new Result.SimpleValueResult("Pearson's Correlation (Fast)", pearsonsR);
-		container.add(result);
+		return pearsonsR;
 	}
 }
