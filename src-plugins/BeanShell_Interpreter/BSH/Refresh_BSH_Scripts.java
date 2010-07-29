b6168ea41bd274809754d80fe919d5ba2f45458a
Johannes Schindelin <johannes.schindelin@gmx.de>
2010-07-29 10:00:36 +0200

    Beanshell: show much more informative error information
    Using the new runScript(inputStream, fileName) method, and handling the
    script stack trace better, it will now be a pleasure for users to see
    errors in their Beanshell scripts.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/BeanShell_Interpreter/BSH/Refresh_BSH_Scripts.java
+++ src-plugins/BeanShell_Interpreter/BSH/Refresh_BSH_Scripts.java
@@ -1,5 +1,7 @@
 package BSH;
 
+import bsh.EvalError;
+
 import common.RefreshScripts;
 import java.io.File;
 import java.io.FileReader;
@@ -11,6 +13,7 @@ import java.io.FileInputStream;
 import java.io.PrintStream;
 import bsh.Interpreter;
 import ij.IJ;
+
 
 public class Refresh_BSH_Scripts extends RefreshScripts {
 
@@ -28,7 +31,7 @@ public class Refresh_BSH_Scripts extends RefreshScripts {
 				return;
 			}
 			// The stream will be closed by runScript(InputStream)
-			runScript(new BufferedInputStream(new FileInputStream(new File(path))));
+			runScript(new BufferedInputStream(new FileInputStream(new File(path))), path);
 		} catch (Throwable error) {
 			printError(error);
 		}
@@ -36,12 +39,20 @@ public class Refresh_BSH_Scripts extends RefreshScripts {
 
 	/** Will consume and close the stream. */
 	public void runScript(InputStream istream) {
+		runScript(istream, null);
+	}
+
+	/** Will consume and close the stream. */
+	public void runScript(InputStream istream, String sourceFileName) {
 		try {
 			Interpreter interpreter = new Interpreter();
 			interpreter.setOut(new PrintStream(out));
 			interpreter.setErr(new PrintStream(err));
-			interpreter.eval(new InputStreamReader(istream));
+			interpreter.eval(new InputStreamReader(istream),
+				interpreter.getNameSpace(), sourceFileName);
 		} catch (Throwable error) {
+			if (error instanceof EvalError)
+				((EvalError)error).setMessage(error.toString());
 			printError(error);
 		}
 	}
