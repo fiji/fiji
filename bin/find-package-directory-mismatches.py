588ea82e6469975ce4cbf76c5aad8c2ff08a4cba
Mark Longair <longair@ini.phys.ethz.ch>
2010-08-11 11:46:26 +0200

    Add a script to find package declarations that don't match the directory they're in

---
+++ bin/find-package-directory-mismatches.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python2.6
+
+import os
+import re
+
+for root, dirs, files in os.walk('.'):
+    if re.search('^./java/',root):
+        continue
+    for filename in [ x for x in files if re.search('\.java$',x) ]:
+        package = None
+        with open(os.path.join(root,filename)) as f:
+            for line in f:
+                m = re.search('^\s*package\s*([\S^;]+)\s*;',line)
+                if m:
+                    package = m.group(1)
+                    break
+        if package:
+            package_parts = package.split('.')
+            directory_left = root[:]
+            while len(package_parts) > 0:
+                directory_left, basename = os.path.split(directory_left)
+                last_package_part = package_parts.pop()
+                if basename != last_package_part:
+                    print "Directory was wrong for: "+os.path.join(root,filename)
+                    print "    package declaration was: "+package
+                    print "    "+basename+" did not match "+last_package_part
