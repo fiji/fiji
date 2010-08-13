ec10f3bbc19ec91ae0d5f332bde8053b0241b0ce
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2010-08-13 13:05:46 +0200

    Ball3DCursor: finished implementation
    Signed-off-by: Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>

--- src-plugins/Embryo_Tracker/mpicbg/imglib/cursor/special/Ball3DCursor.java
+++ src-plugins/Embryo_Tracker/mpicbg/imglib/cursor/special/Ball3DCursor.java
@@ -2,10 +2,7 @@ package mpicbg.imglib.cursor.special;
 
 import ij.ImagePlus;
 
-import java.util.ArrayList;
 import java.util.Iterator;
-
-import com.sun.org.apache.bcel.internal.generic.GETSTATIC;
 
 import mpicbg.imglib.container.Container;
 import mpicbg.imglib.container.array.ArrayContainerFactory;
@@ -19,13 +16,19 @@ import mpicbg.imglib.type.numeric.integer.UnsignedByteType;
 
 /**
  * This class implements a {@link Cursor} that iterates over all the pixel within the volume
- * of a 3D ball, whose center and radius are given at construction. 
+ * of a 3D ball, whose center and radius are given at construction. It is made so that
+ * if the ball volume is made of N pixels, this cursor will go exactly over N iterations 
+ * before exhausting. The ball iterated is symmetrical along X, Y and Z equatorial planes.
  * <p>
- * Internally, it relies on a {@link LocalizableByDimCursor}.
+ * The iteration order is deterministic. Iteration starts from the middle Z plane, and fill circles
+ * away from this plane in alternating fashion: <code>Z = 0, 1, -1, 2, -2, ...</code>. For each 
+ * circle, lines are drawn in the X positive direction from the middle line and away from it also in
+ * an alternating fashion: <code>Y = 0, 1, -1, 2, -2, ...</code>
+ * <p>
+ * Internally, this cursor relies on a {@link LocalizableByDimCursor}. It makes intensive use
+ * of states to avoid calling the {@link Math#sqrt(double)} method too often. 
  * 
- * @author Jean-Yves Tinevez (jeanyves.tinevez@gmail.com)
- *
- * Aug 12, 2010
+ * @author Jean-Yves Tinevez (jeanyves.tinevez@gmail.com) -  August, 2010
  *
  * @param <T>
  */
@@ -37,36 +40,27 @@ public class Ball3DCursor<T extends Type<T>> implements Cursor<T> {
 	private LocalizableByDimCursor<T> cursor;
 	private boolean hasNext;
 	/** The state of the cursor. */
-	private CursorState state;
+	private CursorState state, nextState;
 	/** Store the position index. */
 	private int[] position;
-	/** For mirroring, indicate if we must take the mirror in the X direction. */
-	private boolean mirrorX;
-	/** For mirroring, indicate if we must take the mirror in the Y direction. */
-	private boolean mirrorY;
 	/** For mirroring, indicate if we must take the mirror in the Z direction. */
 	private boolean mirrorZ;
-	/** Indicate whether we finished iterating over a whole X line. */
-	private boolean doneX;
-	/** Indicate whether we finished iterating over possible Ys. */
-	private boolean doneY;
 	/** The line half-length. */
 	private int rx;
 	/** The XY circle radius at height Z. */
 	private int ry;
-	/** The XY circle squared radius at height Z. */
-	private double ry2;
+	/** Indicate whether we finished all Z planes. */
+	private boolean doneZ = false;
 	/**
 	 * Indicates what state the cursor is currently in, so as to choose the right routine 
 	 * to get coordinates */
  	private enum CursorState {
+ 		DRAWING_LINE					,
 		INITIALIZED						,
-		MIDDLE_Z_MIDDLE_Y				,
-		MIDDLE_Z_OTHER_Y_MIDDLE_X		,
-		MIDDLE_Z_OTHER_Y_OTHER_X		,
-		OTHER_Z_MIDDLE_Y				,
-		OTHER_Z_OTHER_Y_MIDDLE_X		,
-		OTHER_Z_OTHER_Y_OTHER_X			;		
+		INCREMENT_Y						,
+		MIRROR_Y						,
+		INCREMENT_Z						,
+		FINISHED						;
 	}
 	
 	
@@ -91,13 +85,10 @@ public class Ball3DCursor<T extends Type<T>> implements Cursor<T> {
 		this.cursor = img.createLocalizableByDimCursor();		
 		this.hasNext = true;
 		this.state = CursorState.INITIALIZED;
-		this.position = new int[] { 0, -radius, -radius } ;
-		
-		cursor.setPosition(center[0], 0);
-		cursor.setPosition(center[1] - radius, 1);
-		cursor.setPosition(center[2] - radius, 2);
-		rx = 0;
-		ry = 0;
+		this.position = new int[] {0, 0, 0};
+		cursor.setPosition(center);
+		mirrorZ = false;
+		doneZ = false;
 	}
 	
 	
@@ -107,63 +98,106 @@ public class Ball3DCursor<T extends Type<T>> implements Cursor<T> {
 	 */
 	public final int getDistanceSquared() {
 		return position[0] * position[0] + position[1] * position[1] + position[2] * position[2];  
-		
+	}
+	
+	/**
+	 * Store the relative position of the current cursor with respect to the ball center in 
+	 * the array given in argument.
+	 */
+	public final void getRelativePosition(int[] position) {
+		position[0] = this.position[0];
+		position[1] = this.position[1];
+		position[2] = this.position[2];
 	}
 	
 	@Override
 	public void fwd() {
-				
-		position[0]++;
-
-		if (position[0] > rx) {
+		
+		switch(state) {
+		
+		case DRAWING_LINE:
+			// Will draw a line towards positives x until we exceed rx
 			
-			// We finished the line at height Y
-			position[1]++;
 			
-			if (position[1] > ry) {
+			if (position[0] >= rx) 
+				state = nextState;
+			else {
+				cursor.fwd(0);
+				position[0]++;				
+			}
+			break;
+		
+		case INITIALIZED:
+			// Will draw the line a z=0, y=0 with x from -radius to +radius
+			rx = radius;
+			cursor.setPosition(center[0] - rx, 0);
+			position[0] = -rx;
+			state = CursorState.DRAWING_LINE;
+			nextState = CursorState.INCREMENT_Y;
+			ry = radius;
+			break;
+		
+		case INCREMENT_Y:
+			
+			position[1] = -position[1] + 1; // y should be negative (coming from mirroring or init = 0)
+			cursor.setPosition(center[1] + position[1], 1);
+			state = CursorState.DRAWING_LINE;
+			rx = (int) Math.sqrt(ry * ry - position[1] * position[1]);
+			position[0] = -rx;
+			cursor.setPosition(center[0] - rx, 0);
+			nextState = CursorState.MIRROR_Y;
+			break;
+			
+		case MIRROR_Y:
+			
+			position[0] = -rx;
+			position[1] = - position[1];
+			cursor.setPosition(center[1] + position[1], 1);
+			cursor.setPosition(center[0] - rx, 0);
+			state = CursorState.DRAWING_LINE;
+			if (position[1] <= - ry) 
+				nextState = CursorState.INCREMENT_Z;
+			else 
+				nextState = CursorState.INCREMENT_Y;
+			break;
+			
+		case INCREMENT_Z:
 
-				// We finished the circle at height Z
-				position[2]++;
-				
-				if (position[2] > radius) {
-				
-					// We finished the ball
-					hasNext = false;
-					position[0] = 0;
-					position[1] = 0;
-					position[2] = 0;
-					cursor.setPosition(center);
-					
-				
-				} else {
-					
-					// We did not finish the ball, move up in Z
-					ry2 = radius * radius - position[2] * position [2];
-					ry = (int) Math.sqrt( ry2 );
-					rx = (int) Math.sqrt(ry * ry - position[1] * position[1]);
-					position[1] = -ry; // Bottom of the circle
-					position[0] = 0;
-					cursor.setPosition(center[0]-rx, 0);
-					cursor.setPosition(center[1]-ry, 1);
-					cursor.fwd(2);
+			if (mirrorZ) {
 
-				}
-				
+				position[2] = - position[2];
+				mirrorZ = false;
+
 			} else {
 				
-				// We did not finish the circle, move up in Y
-				rx = (int) Math.sqrt(ry * ry - position[1] * position[1]);
-				position[0] = -rx;				
-				cursor.setPosition(center[0]-rx, 0);
-				cursor.fwd(1);
-				
+				position[2] = - position[2] + 1;
+				if (position[2] >= radius) 
+					doneZ = true;
+				mirrorZ = true;
+			}
+			if (doneZ) {
+				if (!mirrorZ)
+					nextState = CursorState.FINISHED;
+			} else {
+				nextState = CursorState.INCREMENT_Y;
 			}
 			
-		} else {
+			position[1] = 0;
+			ry = (int) Math.sqrt(radius * radius - position[2] * position[2]);
+			rx = ry; // We start at middle Y
+			cursor.setPosition(center[0]-rx, 0);
+			cursor.setPosition(center[1], 1);
+			cursor.setPosition(center[2] + position[2], 2);
+			position[0] = -rx;
+			state = CursorState.DRAWING_LINE;
+			break;
+			
+		case FINISHED:
+			hasNext = false;
+			break;
 
-			// We did not finish the line. Move forward in it.
-			cursor.fwd(0);
 		}
+		
 	}
 	
 
@@ -224,40 +258,37 @@ public class Ball3DCursor<T extends Type<T>> implements Cursor<T> {
 
 	@Override
 	public T next() {
-		// TODO Auto-generated method stub
-		return null;
+		fwd();
+		return cursor.getType();
 	}
 
 	@Override
 	public void remove() {
-		// TODO Auto-generated method stub
-		
+		cursor.remove();
 	}
 
 	@Override
 	public Iterator<T> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		reset();
+		return this;
 	}
 
 
 
 	@Override
 	public void fwd(long steps) {
-		// TODO Auto-generated method stub
-		
+		for (int i = 0; i < steps; i++) 
+			fwd();
 	}
 
 	@Override
 	public int[] getDimensions() {
-		// TODO Auto-generated method stub
-		return null;
+		return cursor.getDimensions();
 	}
 
 	@Override
 	public void getDimensions(int[] position) {
-		// TODO Auto-generated method stub
-		
+		cursor.getDimensions(position);
 	}
 
 	@Override
@@ -287,8 +318,7 @@ public class Ball3DCursor<T extends Type<T>> implements Cursor<T> {
 				15);
 		while(cursor.hasNext) {
 			cursor.fwd();
-//			cursor.getType().set(cursor.getDistanceSquared());
-			cursor.getType().set(255);
+			cursor.getType().set(cursor.getDistanceSquared());
 		}
 
 		ImagePlus imp = ImageJFunctions.copyToImagePlus(testImage);
