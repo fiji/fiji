cb02ffc6299a5a67fa665017baa9342195d5f6bf
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2010-08-13 14:59:18 +0200

    Ball3DCursor: the cursor now deal with physical calibration
    The physical calibration can be non-isotropic.
    Signed-off-by: Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>

--- src-plugins/Embryo_Tracker/mpicbg/imglib/cursor/special/Ball3DCursor.java
+++ src-plugins/Embryo_Tracker/mpicbg/imglib/cursor/special/Ball3DCursor.java
@@ -20,7 +20,12 @@ import mpicbg.imglib.type.numeric.integer.UnsignedByteType;
  * if the ball volume is made of N pixels, this cursor will go exactly over N iterations 
  * before exhausting. The ball iterated is symmetrical along X, Y and Z equatorial planes.
  * <p>
- * The iteration order is deterministic. Iteration starts from the middle Z plane, and fill circles
+ * It takes a spatial calibration into account, which may be non isotropic.
+ * <p>
+ * Bounding box: if we assume for instance that the calibration is 1 in every direction,
+ * then the whole ball will be contained in a cube which side is <code>2 * ceil(radius) + 1</code>.
+ * <p>
+ * The iteration order is always the same. Iteration starts from the middle Z plane, and fill circles
  * away from this plane in alternating fashion: <code>Z = 0, 1, -1, 2, -2, ...</code>. For each 
  * circle, lines are drawn in the X positive direction from the middle line and away from it also in
  * an alternating fashion: <code>Y = 0, 1, -1, 2, -2, ...</code>
@@ -34,15 +39,18 @@ import mpicbg.imglib.type.numeric.integer.UnsignedByteType;
  */
 public class Ball3DCursor<T extends Type<T>> implements Cursor<T> {
 	
-	private Image<T> img;
-	private int[] center;
-	private int radius;
-	private LocalizableByDimCursor<T> cursor;
+	private final Image<T> img;
+	private final int[] icenter;
+	private final double radius;
+	private final LocalizableByDimCursor<T> cursor;
+	private final int[] position;
+	/** The spatial calibration. */
+	private final double[] calibration;
+
 	private boolean hasNext;
 	/** The state of the cursor. */
 	private CursorState state, nextState;
 	/** Store the position index. */
-	private int[] position;
 	/** For mirroring, indicate if we must take the mirror in the Z direction. */
 	private boolean mirrorZ;
 	/** The line half-length. */
@@ -70,44 +78,74 @@ public class Ball3DCursor<T extends Type<T>> implements Cursor<T> {
 	 */
 	
 	/**
-	 * Construct a {@link Ball3DCursor} on a 3D image.
+	 * Construct a {@link Ball3DCursor} on a 3D image with a given spatial calibration.
 	 * @param img  the image, must be 3D
-	 * @param center  the ball center
-	 * @param radius  the ball radius
+	 * @param center  the ball center, in physical units
+	 * @param radius  the ball radius, in physical units
+	 * @param calibration  the spatial calibration (pixel size)
 	 */
-	public Ball3DCursor(Image<T> img, final int[] center, int radius) {
+	public Ball3DCursor(final Image<T> img, final double[] center, double radius, final double[] calibration) {
 		if (img.getDimensions().length != 3) 
 			throw new IllegalArgumentException(
 					String.format("Ball3DCursor: must get a 3D image, got a %dD image.", img.getDimensions().length));
 		this.img = img;
-		this.center = center;
 		this.radius = radius;
 		this.cursor = img.createLocalizableByDimCursor();		
 		this.hasNext = true;
 		this.state = CursorState.INITIALIZED;
 		this.position = new int[] {0, 0, 0};
-		cursor.setPosition(center);
+		this.calibration = calibration;
+		this.icenter = new int[] {
+				(int) (center[0] / calibration[0]), 
+				(int) (center[1] / calibration[1]), 
+				(int) (center[2] / calibration[2]) };
+		cursor.setPosition(icenter);
 		mirrorZ = false;
 		doneZ = false;
 	}
 	
+	/**
+	 * Construct a {@link Ball3DCursor} on a 3D image, using the spatial calibration
+	 * stored in the image {@link #img}.
+	 * 
+	 * @param img  the image, must be 3D
+	 * @param center  the ball center, in physical units
+	 * @param radius  the ball radius, in physical units
+	 * @see Image#setCalibration(float[])
+	 */
+	public Ball3DCursor(final Image<T> img, final double[] center, double radius) {
+		this(img, center, radius, 
+				new double[] {img.getCalibration(0), img.getCalibration(1), img.getCalibration(2)});
+	}
 	
 	/**
 	 * Return the square distance measured from the center of the ball to the current
-	 * cursor position.
+	 * cursor position, in physical units.
 	 */
-	public final int getDistanceSquared() {
-		return position[0] * position[0] + position[1] * position[1] + position[2] * position[2];  
+	public final double getDistanceSquared() {
+		return position[0] * position[0] * calibration[0] * calibration[0] + 
+				position[1] * position[1] * calibration[1] * calibration[1] +
+				position[2] * position[2] * calibration[2] * calibration[2];  
 	}
 	
 	/**
 	 * Store the relative position of the current cursor with respect to the ball center in 
-	 * the array given in argument.
+	 * the array given in argument. The position is returned in <b>pixel units</b> and as
+	 * such, is stored in an <code>int</code> array.
 	 */
 	public final void getRelativePosition(int[] position) {
 		position[0] = this.position[0];
 		position[1] = this.position[1];
 		position[2] = this.position[2];
+	}
+	
+	/**
+	 * Return the relative calibrated position of this cursor in physical units.
+	 */
+	public final void getPhysicalRelativeCoordinates(double[] coordinates) {
+		coordinates[0] = position[0] / calibration[0];
+		coordinates[1] = position[1] / calibration[1];
+		coordinates[2] = position[2] / calibration[2];
 	}
 	
 	@Override
@@ -116,8 +154,6 @@ public class Ball3DCursor<T extends Type<T>> implements Cursor<T> {
 		switch(state) {
 		
 		case DRAWING_LINE:
-			// Will draw a line towards positives x until we exceed rx
-			
 			
 			if (position[0] >= rx) 
 				state = nextState;
@@ -128,23 +164,24 @@ public class Ball3DCursor<T extends Type<T>> implements Cursor<T> {
 			break;
 		
 		case INITIALIZED:
+			
 			// Will draw the line a z=0, y=0 with x from -radius to +radius
-			rx = radius;
-			cursor.setPosition(center[0] - rx, 0);
+			rx = (int) (radius / calibration[0]);
+			cursor.setPosition(icenter[0] - rx, 0);
 			position[0] = -rx;
 			state = CursorState.DRAWING_LINE;
 			nextState = CursorState.INCREMENT_Y;
-			ry = radius;
+			ry = (int) (radius / calibration[1]);
 			break;
 		
 		case INCREMENT_Y:
 			
 			position[1] = -position[1] + 1; // y should be negative (coming from mirroring or init = 0)
-			cursor.setPosition(center[1] + position[1], 1);
+			cursor.setPosition(icenter[1] + position[1], 1);
 			state = CursorState.DRAWING_LINE;
-			rx = (int) Math.sqrt(ry * ry - position[1] * position[1]);
+			rx = (int) ( Math.sqrt( ry * ry - position[1] * position[1]) * calibration[1] / calibration[0] );
 			position[0] = -rx;
-			cursor.setPosition(center[0] - rx, 0);
+			cursor.setPosition(icenter[0] - rx, 0);
 			nextState = CursorState.MIRROR_Y;
 			break;
 			
@@ -152,8 +189,8 @@ public class Ball3DCursor<T extends Type<T>> implements Cursor<T> {
 			
 			position[0] = -rx;
 			position[1] = - position[1];
-			cursor.setPosition(center[1] + position[1], 1);
-			cursor.setPosition(center[0] - rx, 0);
+			cursor.setPosition(icenter[1] + position[1], 1);
+			cursor.setPosition(icenter[0] - rx, 0);
 			state = CursorState.DRAWING_LINE;
 			if (position[1] <= - ry) 
 				nextState = CursorState.INCREMENT_Z;
@@ -171,7 +208,7 @@ public class Ball3DCursor<T extends Type<T>> implements Cursor<T> {
 			} else {
 				
 				position[2] = - position[2] + 1;
-				if (position[2] >= radius) 
+				if (position[2] >= radius / calibration[2]) 
 					doneZ = true;
 				mirrorZ = true;
 			}
@@ -183,16 +220,17 @@ public class Ball3DCursor<T extends Type<T>> implements Cursor<T> {
 			}
 			
 			position[1] = 0;
-			ry = (int) Math.sqrt(radius * radius - position[2] * position[2]);
-			rx = ry; // We start at middle Y
-			cursor.setPosition(center[0]-rx, 0);
-			cursor.setPosition(center[1], 1);
-			cursor.setPosition(center[2] + position[2], 2);
+			ry = (int) ( Math.sqrt(radius * radius - position[2] * position[2] * calibration[2] * calibration[2]) / calibration[1] );
+			rx = (int) (ry * calibration[1] / calibration[0]); // We start at middle Y
+			cursor.setPosition(icenter[0]-rx, 0);
+			cursor.setPosition(icenter[1], 1);
+			cursor.setPosition(icenter[2] + position[2], 2);
 			position[0] = -rx;
 			state = CursorState.DRAWING_LINE;
 			break;
 			
 		case FINISHED:
+			
 			hasNext = false;
 			break;
 
@@ -244,6 +282,13 @@ public class Ball3DCursor<T extends Type<T>> implements Cursor<T> {
 	@Override
 	public void reset() {
 		cursor.reset();
+		cursor.setPosition(icenter);
+		state = CursorState.INITIALIZED;
+		mirrorZ = false;
+		doneZ = false;
+		position[0] = 0;
+		position[1] = 0;
+		position[2] = 0;
 	}
 
 	@Override
@@ -272,8 +317,6 @@ public class Ball3DCursor<T extends Type<T>> implements Cursor<T> {
 		reset();
 		return this;
 	}
-
-
 
 	@Override
 	public void fwd(long steps) {
@@ -310,18 +353,24 @@ public class Ball3DCursor<T extends Type<T>> implements Cursor<T> {
 		Image<UnsignedByteType> testImage = new ImageFactory<UnsignedByteType>(
 					new UnsignedByteType(),
 					new ArrayContainerFactory()
-				).createImage(new int[] {80, 80, 80});
+				).createImage(new int[] {80, 80, 40}); // 40µm x 40µm x 40µm
 		
+		double[] calibration = new double[] {0.5, 0.5, 1}; 
 		Ball3DCursor<UnsignedByteType> cursor = new Ball3DCursor<UnsignedByteType>(
 				testImage, 
-				new int[] {40, 40, 40}, 
-				15);
+				new double[] {20, 20, 20}, // in units
+				10, // µm
+				calibration);
 		while(cursor.hasNext) {
 			cursor.fwd();
-			cursor.getType().set(cursor.getDistanceSquared());
+			cursor.getType().set((int) cursor.getDistanceSquared());
 		}
 
 		ImagePlus imp = ImageJFunctions.copyToImagePlus(testImage);
+		imp.getCalibration().pixelWidth = calibration[0];
+		imp.getCalibration().pixelHeight = calibration[1];
+		imp.getCalibration().pixelDepth = calibration[2];
+		imp.getCalibration().setUnit("um");
 		imp.show();
 	}
 	
