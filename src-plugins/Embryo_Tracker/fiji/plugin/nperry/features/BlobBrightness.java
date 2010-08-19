45ddf309b3eaaf9fa0b47d6a8d60efaff6474622
Nick Perry <nicholas.m.perry@gmail.com>
2010-08-19 13:43:04 +0200

    BlobBrightness: Switched to using the spherecursor instead of using the ROI cursor.
    Signed-off-by: Nick Perry <nicholas.m.perry@gmail.com>

--- src-plugins/Embryo_Tracker/fiji/plugin/nperry/features/BlobBrightness.java
+++ src-plugins/Embryo_Tracker/fiji/plugin/nperry/features/BlobBrightness.java
@@ -1,9 +1,8 @@
 package fiji.plugin.nperry.features;
 
-import mpicbg.imglib.cursor.LocalizableByDimCursor;
-import mpicbg.imglib.cursor.special.RegionOfInterestCursor;
+import mpicbg.imglib.algorithm.math.MathLib;
+import mpicbg.imglib.cursor.special.SphereCursor;
 import mpicbg.imglib.image.Image;
-import mpicbg.imglib.outofbounds.OutOfBoundsStrategyValueFactory;
 import mpicbg.imglib.type.numeric.RealType;
 import fiji.plugin.nperry.Feature;
 import fiji.plugin.nperry.Spot;
@@ -33,6 +32,10 @@ public class BlobBrightness <T extends RealType<T>> extends IndependentFeatureAn
 		this.calibration = calibration;
 	}
 
+	public BlobBrightness(Image<T> originalImage, float diam) {
+		this(originalImage, diam, originalImage.getCalibration());
+	}
+	
 	/*
 	 * PUBLIC METHODS
 	 */
@@ -49,67 +52,23 @@ public class BlobBrightness <T extends RealType<T>> extends IndependentFeatureAn
 
 	@Override
 	public void process(Spot spot) {
-		final LocalizableByDimCursor<T> cursor = img.createLocalizableByDimCursor(new OutOfBoundsStrategyValueFactory<T>());
-		final float[] origin = spot.getCoordinates();  // physical coordinates
 		
-		// Convert physical coordinates to pixel coordinates
-		final int[] pOrigin = new int[origin.length];
-		for (int i = 0; i < pOrigin.length; i++) {
-			pOrigin[i] = (int) (origin[i] / calibration[i]);
-		}
-
-		// Create the size array for the ROI cursor
-		int size[] = new int[img.getNumDimensions()];
-		for (int i = 0; i < size.length; i++) {
-			size[i] = (int) (diam / calibration[i]);  // convert back to pixel units
-		}
-
-		// Adjust the integer coordinates of the spot to set the ROI correctly
-		int[] roiCoords = new int[img.getNumDimensions()];
-		for (int i = 0; i < origin.length; i++) {
-			roiCoords[i] = (int) (origin[i] - (size[i] / 2));  
+		// 1 - Initialize sphere cursor
+		SphereCursor<T> cursor = new SphereCursor<T>(img, spot.getCoordinates(), diam/2, calibration);
+		
+		// 2 - Iterate pixels in sphere, and aggregate total intensity.
+		float intensity = 0;
+		System.out.println();
+		System.out.println("New max: " + MathLib.printCoordinates(spot.getCoordinates()));
+		System.out.println();
+		while (cursor.hasNext()) {
+			cursor.next();
+			int[] pos = cursor.getPosition();
+			System.out.print(pos[0] + ", " + pos[1] + ", " + pos[2] + "; ");
+			intensity += cursor.getType().getRealFloat();
 		}
 		
-		// Use ROI cursor to search a sphere around the spot's coordinates
-		/* need to handle case where ROI is not in image anymore!! */
-		float sum = 0;
-		RegionOfInterestCursor<T> roi = cursor.createRegionOfInterestCursor(roiCoords, size);
-		//System.out.println();
-		//System.out.println("Maximum: " + origin[0] + ", " + origin[1] + ", " + origin[2] + "; ");
-		//System.out.println();
-		while (roi.hasNext()) {
-			roi.next();
-			if (inSphere(pOrigin, cursor.getPosition())) {
-				sum += roi.getType().getRealDouble();
-				//System.out.print(cursor.getPosition()[0] + ", " + cursor.getPosition()[1] + ", " + cursor.getPosition()[2] + "; ");
-			}
-		}
-
-		// Close cursors
-		roi.close();
-		cursor.close();
-		
-		// Add total intensity.
-		spot.addFeature(FEATURE, sum);
-	}
-	
-	/**
-	 * Determines if the coordinate coords is at least min distance away from the
-	 * origin, but within max distance. The distance metric used is Euclidean
-	 * distance.
-	 * 
-	 * @param origin 
-	 * @param coords
-	 * @param max
-	 * @param min
-	 * @return
-	 */
-	private boolean inSphere(int[] pOrigin, int[] coords) {
-		double euclDist = 0;
-		for (int i = 0; i < coords.length; i++) {
-			euclDist += Math.pow((pOrigin[i] - coords[i]), 2);
-		}
-		euclDist = Math.sqrt(euclDist);
-		return euclDist <= (diam / 2);
+		// 3 - Store the total intensity as a feature in this Spot
+		spot.addFeature(FEATURE, intensity);
 	}
 }
