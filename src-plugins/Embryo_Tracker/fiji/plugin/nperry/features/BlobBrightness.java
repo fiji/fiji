8058b82e83115e8d470c94a17dc92432452897d8
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2010-08-19 13:55:49 +0200

    Embryo_Tracker: brightness feature has a main method for debugging
    Signed-off-by: Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>

--- src-plugins/Embryo_Tracker/fiji/plugin/nperry/features/BlobBrightness.java
+++ src-plugins/Embryo_Tracker/fiji/plugin/nperry/features/BlobBrightness.java
@@ -1,9 +1,12 @@
 package fiji.plugin.nperry.features;
 
 import mpicbg.imglib.algorithm.math.MathLib;
+import mpicbg.imglib.container.array.ArrayContainerFactory;
 import mpicbg.imglib.cursor.special.SphereCursor;
 import mpicbg.imglib.image.Image;
+import mpicbg.imglib.image.ImageFactory;
 import mpicbg.imglib.type.numeric.RealType;
+import mpicbg.imglib.type.numeric.integer.UnsignedByteType;
 import fiji.plugin.nperry.Feature;
 import fiji.plugin.nperry.Spot;
 
@@ -64,11 +67,43 @@ public class BlobBrightness <T extends RealType<T>> extends IndependentFeatureAn
 		while (cursor.hasNext()) {
 			cursor.next();
 			int[] pos = cursor.getPosition();
-			System.out.print(pos[0] + ", " + pos[1] + ", " + pos[2] + "; ");
+			System.out.print(pos[0] + ", " + pos[1] + ", " + pos[2] + "; \n");
 			intensity += cursor.getType().getRealFloat();
 		}
 		
 		// 3 - Store the total intensity as a feature in this Spot
 		spot.addFeature(FEATURE, intensity);
+	}
+	
+	public static void main(String[] args) {
+
+		Image<UnsignedByteType> testImage = new ImageFactory<UnsignedByteType>(
+				new UnsignedByteType(),
+				new ArrayContainerFactory()
+		).createImage(new int[] {200, 200, 40} );
+		
+		float[] center = new float[]  {20, 20, 20};
+		Spot s1 = new Spot(center);
+
+		float radius = 2;
+		float[] calibration = new float[] {0.2f, 0.2f, 1};
+		SphereCursor<UnsignedByteType> cursor = new SphereCursor<UnsignedByteType>(
+				testImage, 
+				s1.getCoordinates(), 
+				radius, // µm
+				calibration);
+		int volume = 0;
+		while(cursor.hasNext()) {
+			volume++;
+			cursor.fwd();
+			cursor.getType().inc(); // to check we did not walk multiple times on a single pixel
+		}
+		cursor.close();
+		
+		BlobBrightness<UnsignedByteType> bb = new BlobBrightness<UnsignedByteType>(testImage, 2*radius, calibration);
+		bb.process(s1);
+		
+		
+		
 	}
 }
