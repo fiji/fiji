b0a925ad5243d35686c4c67680cec7867a0c6fa5
Mark Longair <longair@ini.phys.ethz.ch>
2010-08-20 13:18:29 +0200

    debian: Add some useful functions for unescaping HTML / XML entities

--- debian/common.py
+++ debian/common.py
@@ -1,6 +1,7 @@
 import os
 import re
 from subprocess import Popen, PIPE
+from htmlentitydefs import name2codepoint
 
 chroot_path = "/var/chroot/squeeze-i386"
 home_in_chroot = os.path.join(chroot_path,"home/mark")
@@ -25,3 +26,29 @@ def get_version_from_changelog(changelog_path):
         sys.exit(6)
 
     return version_from_changelog
+
+# These next two useful functions are from:
+#   http://snippets.dzone.com/posts/show/4569
+
+def substitute_entity(match):
+    ent = match.group(2)
+    if match.group(1) == "#":
+	return unichr(int(ent))
+    else:
+        cp = name2codepoint.get(ent)
+	if cp:
+            return unichr(cp)
+        else:
+            return match.group()
+
+def decode_htmlentities(string):
+    entity_re = re.compile("&(#?)(\d{1,5}|\w{1,8});")
+    return entity_re.subn(substitute_entity, string)[0]
+
+def trim_line(s,max_length):
+    rs = s.rstrip()
+    if len(rs) <= max_length:
+        return rs
+    else:
+        substitution = " [...]"
+        return rs[0:(max_length-len(substitution))].rstrip() + substitution
