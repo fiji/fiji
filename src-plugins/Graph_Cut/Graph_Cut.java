c22febd25afc1ae9220d2724a0437224e1c7ce46
Jan Funke <jan.funke@inf.tu-dresden.de>
2010-08-23 12:02:13 +0200

    Add the skeleton of the plugin "Graph Cut"
    Signed-off-by: Jan Funke <jan.funke@inf.tu-dresden.de>

---
+++ src-plugins/Graph_Cut/Graph_Cut.java
@@ -0,0 +1,19 @@
+/*
+ *
+ */
+
+
+import ij.*;
+import ij.ImagePlus.*;
+
+import ij.plugin.PlugIn;
+
+/**
+ * @author Jan Funke <jan.funke@inf.tu-dresden.de>
+ * @version 0.1
+ */
+public class Graph_Cut implements PlugIn {
+
+    public void run(String arg) {
+    }
+}
