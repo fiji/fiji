cf6e818a873923e6d28ed6744649cb6b7df1c1ee
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2010-08-30 16:27:39 +0200

    Embryo_Tracker: moved more methods in Util class
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src-plugins/Embryo_Tracker/fiji/plugin/nperry/Embryo_Tracker.java
+++ src-plugins/Embryo_Tracker/fiji/plugin/nperry/Embryo_Tracker.java
@@ -337,163 +337,33 @@ public class Embryo_Tracker<T extends RealType<T>> implements PlugIn {
 	 */
 	private void thresholdFeatures(Feature[] features, HashMap<Feature, Float> thresholds, ArrayList<Spot> spots) {
 		for (Feature feature : features) {
-			final float threshold = otsuThreshold(spots, feature);
-			System.out.println(String.format("Feature: %s, Value: %f", feature.getName(), threshold));
+			final float threshold = (float) Utils.otsuThreshold(histogram(spots, feature), spots.size());
+			System.out.println(String.format("Feature: %s, Value: %f", feature.toString(), threshold));
 			thresholds.put(feature, threshold);
 		}
 	}
 	
-	/**
-	 * Given a list of {@link Spots}, and a {@link Feature}, this method automatically
-	 * thresholds the Spots based on the value stored for that Feature. The threshold
-	 * is performed using the Otsu Threshold Method.
-	 * 
-	 * @param srcData An ArrayList containing the Spots to threshold.
-	 * @param feature The Feature to threshold.
-	 * @return A float value representing the value to threshold on based on the Otsu Thresholding Method.
-	 * @see Spots
-	 * @see Feature
-	 */
-	// TODO fix so that will work with one maxima. (thresholding alg throws error when only a single maxima)
-	// Code source: http://www.labbookpages.co.uk/software/imgProc/otsuThreshold.html
-	private float otsuThreshold(ArrayList<Spot> srcData, Feature feature)	{
-		// Prepare histogram
-		int histData[] = histogram(srcData, feature);
-		int count = srcData.size();
-
-		// Thresholding
-		float sum = 0;
-		for (int t=0 ; t<histData.length ; t++) sum += t * histData[t];
-
-		float sumB = 0;
-		int wB = 0;
-		int wF = 0;
-
-		float varMax = 0;
-		int threshold = 0;
-		
-		for (int t=0 ; t<histData.length ; t++)
-		{
-			wB += histData[t];					// Weight Background
-			if (wB == 0) continue;
-
-			wF = count - wB;					// Weight Foreground
-			if (wF == 0) break;
-
-			sumB += (float) (t * histData[t]);
-
-			float mB = sumB / wB;				// Mean Background
-			float mF = (sum - sumB) / wF;		// Mean Foreground
-
-			// Calculate Between Class Variance
-			float varBetween = (float)wB * (float)wF * (mB - mF) * (mB - mF);	
-
-			// Check if new maximum found
-			if (varBetween > varMax) {
-				varMax = varBetween;
-				threshold = t;
-			}
-		}
-		
-		return (threshold + 1) * (float) getRange(srcData, feature)[0] / (float) histData.length;  // Convert the integer bin threshold to a value
-	}
-	
-	
 	/** Generate a histogram of the specified feature, with a number of bins determined 
 	 * from the Freedman and Diaconis rule (bin_space = 2*IQR/n^(1/3)) 
 	 */
-	private int[] histogram (ArrayList<Spot> data, Feature feature) {
+	private int[] histogram (ArrayList<Spot> spots, Feature feature) {
 
-		// Calculate number of bins
-		final int size = data.size();
-		final double[] feature_values = new double[size];
-		for (int i = 0; i < feature_values.length; i++) {
-			feature_values[i] = data.get(i).getFeature(feature);
-		}
-		final double q1 = getPercentile(feature_values, 0.25);
-		final double q3 = getPercentile(feature_values, 0.75);
-		final double iqr = q3 - q1;
-		final double binWidth = 2 * iqr * Math.pow(size, -0.33);
-		final double[] range = getRange(data, feature);
-		final int nBins = (int) ( range[0] / binWidth + 1 ); 
-
+		double[] values = Utils.getFeature(spots, feature);
+		int nBins = Utils.getNBins(values);
+		double[] range = Utils.getRange(values);
+		double binWidth = range[0]/nBins;
+		
 		// Create array for histrogram with nBins
 		final int[] hist = new int[nBins];
 		int index;
 		// Populate the histogram with data
-		for (int i = 0; i < feature_values.length; i++) {
-			index = Math.min((int) Math.floor((feature_values[i] - range[1]) / binWidth), nBins - 1); // the max value ends up being 1 higher than nBins, so put it in the last bin.
+		for (int i = 0; i < values.length; i++) {
+			index = Math.min((int) Math.floor((values[i] - range[1]) / binWidth), nBins - 1); // the max value ends up being 1 higher than nBins, so put it in the last bin.
 			hist[index]++;
 		}
 		return hist;
 	}
 	
-	
-	/**
-     * Returns an estimate of the <code>p</code>th percentile of the values
-     * in the <code>values</code> array. Taken from commons-math.
-	 */
-	private static final double getPercentile(final double[] values, final double p) {
-
-		final int size = values.length;
-		if ((p > 1) || (p <= 0)) {
-            throw new IllegalArgumentException("invalid quantile value: " + p);
-        }
-        if (size == 0) {
-            return Double.NaN;
-        }
-        if (size == 1) {
-            return values[0]; // always return single value for n = 1
-        }
-        double n = (double) size;
-        double pos = p * (n + 1);
-        double fpos = Math.floor(pos);
-        int intPos = (int) fpos;
-        double dif = pos - fpos;
-        double[] sorted = new double[size];
-        System.arraycopy(values, 0, sorted, 0, size);
-        Arrays.sort(sorted);
-
-        if (pos < 1) {
-            return sorted[0];
-        }
-        if (pos >= n) {
-            return sorted[size - 1];
-        }
-        double lower = sorted[intPos - 1];
-        double upper = sorted[intPos];
-        return lower + dif * (upper - lower);
-	}
-	
-	
-	/** Returns <code>[range, min, max]</code> of the values for the {@link Feature} <code>feature</code>
-	 * in the list of {@link Spots}
-	 * 
-	 * @param An ArrayList of Spots to find the <code>range</code>, <code>min</code> and <code>max</code>
-	 * for.
-	 * @param The Feature of the Spots to compute the statistics for.
-	 * @return A double[] of length 3, where index 0 is the range, index 1 is the min, and index 2 is the max.
-	 */
-	private double[] getRange(ArrayList<Spot> data, Feature feature) {
-		double min = 0;
-		double max = 0;
-		
-		for (int i = 0; i < data.size(); i++) {
-			double value = data.get(i).getFeature(feature);
-			if (i == 0) {
-				min = value;
-				max = value;
-			}
-			
-			else {
-				if (value < min) min = value;
-				if (value > max) max = value;
-			}
-		}
-		
-		return new double[] {(max-min), min, max};
-	}
-
 
 	/**
 	 * TODO
@@ -630,8 +500,8 @@ public class Embryo_Tracker<T extends RealType<T>> implements PlugIn {
 			
 			// Add slider for this Feature to dialog
 			final float tr = thresholdsAllFrames.get(t).get(feature);
-			double[] range = getRange(extremaAllFrames.get(t), feature);
-			gd.addSlider(feature.getName() + " Threshold", range[1], range[2], tr);
+			double[] range = Utils.getRange(Utils.getFeature(extremaAllFrames.get(t), feature));
+			gd.addSlider(feature.toString() + " Threshold", range[1], range[2], tr);
 //			gd.addCheckbox("Auto", true);
 			
 			// Create a SliderAdjuster for this Feature
--- src-plugins/Embryo_Tracker/fiji/plugin/nperry/Utils.java
+++ src-plugins/Embryo_Tracker/fiji/plugin/nperry/Utils.java
@@ -1,6 +1,8 @@
 package fiji.plugin.nperry;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Iterator;
 
 import ij.ImagePlus;
@@ -217,5 +219,128 @@ public class Utils {
 		cursor.close();		
 	}
 	
+	/**
+     * Returns an estimate of the <code>p</code>th percentile of the values
+     * in the <code>values</code> array. Taken from commons-math.
+	 */
+	public static final double getPercentile(final double[] values, final double p) {
+
+		final int size = values.length;
+		if ((p > 1) || (p <= 0)) {
+            throw new IllegalArgumentException("invalid quantile value: " + p);
+        }
+        if (size == 0) {
+            return Double.NaN;
+        }
+        if (size == 1) {
+            return values[0]; // always return single value for n = 1
+        }
+        double n = (double) size;
+        double pos = p * (n + 1);
+        double fpos = Math.floor(pos);
+        int intPos = (int) fpos;
+        double dif = pos - fpos;
+        double[] sorted = new double[size];
+        System.arraycopy(values, 0, sorted, 0, size);
+        Arrays.sort(sorted);
+
+        if (pos < 1) {
+            return sorted[0];
+        }
+        if (pos >= n) {
+            return sorted[size - 1];
+        }
+        double lower = sorted[intPos - 1];
+        double upper = sorted[intPos];
+        return lower + dif * (upper - lower);
+	}
 	
+	
+	/** 
+	 * Returns <code>[range, min, max]</code> of the given double array.
+	 * @return A double[] of length 3, where index 0 is the range, index 1 is the min, and index 2 is the max.
+	 */
+	public static final double[] getRange(final double[] data) {
+		double min = Double.POSITIVE_INFINITY;
+		double max = Double.NEGATIVE_INFINITY;
+		double value;
+		for (int i = 0; i < data.length; i++) {
+			value = data[i];
+			if (value < min) min = value;
+			if (value > max) max = value;
+		}		
+		return new double[] {(max-min), min, max};
+	}
+
+	/**
+	 * Return the feature values of this Spot collection as a new double array.
+	 */
+	public static final double[] getFeature(final Collection<Spot> spots, final Feature feature) {
+		final double[] values = new double[spots.size()];
+		int index = 0;
+		for(Spot spot : spots) {
+			values[index] = spot.getFeature(feature);
+			index++;
+		}
+		return values;
+	}
+	
+	/**
+	 * Return the optimal bin number for a histogram of the data given in array, using the 
+	 * Freedman and Diaconis rule (bin_space = 2*IQR/n^(1/3)).
+	 */
+	public static final int getNBins(final double[] values) {
+		final int size = values.length;
+		final double q1 = Utils.getPercentile(values, 0.25);
+		final double q3 = Utils.getPercentile(values, 0.75);
+		final double iqr = q3 - q1;
+		final double binWidth = 2 * iqr * Math.pow(size, -0.33);
+		final double[] range = getRange(values);
+		return (int) ( range[0] / binWidth + 1 ); 
+	}
+	
+	/**
+	 * Given a histogram array <code>hist</code>, built with an initial amount of <code>nPoints</code>
+	 * data item, this method return a threshold for the histogram. The threshold
+	 * is performed using the Otsu Threshold Method, {@link http://www.labbookpages.co.uk/software/imgProc/otsuThreshold.html}.
+	 * @param hist  the histogram array
+	 * @param nPoints  the number of data items this histogram was built on
+	 * @return a threshold value that separates the histogram in two classes
+	 */
+	public static final double otsuThreshold(final int[] hist, final int nPoints)	{
+		int total = nPoints;
+
+		double sum = 0;
+		for (int t=0 ; t<256 ; t++) sum += t * hist[t];
+
+		double sumB = 0;
+		int wB = 0;
+		int wF = 0;
+
+		double varMax = 0;
+		double threshold = 0;
+
+		for (int t=0 ; t<256 ; t++) {
+			wB += hist[t];               // Weight Background
+			if (wB == 0) continue;
+
+			wF = total - wB;                 // Weight Foreground
+			if (wF == 0) break;
+
+			sumB += (float) (t * hist[t]);
+
+			double mB = sumB / wB;            // Mean Background
+			double mF = (sum - sumB) / wF;    // Mean Foreground
+
+			// Calculate Between Class Variance
+			double varBetween = (float)wB * (float)wF * (mB - mF) * (mB - mF);
+
+			// Check if new maximum found
+			if (varBetween > varMax) {
+				varMax = varBetween;
+				threshold = t;
+			}
+		}
+		return threshold;
+	}
 }
