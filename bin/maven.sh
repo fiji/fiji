205b1bf9ccf2df57b0346ddb693261a5dbddb160
Johannes Schindelin <johannes.schindelin@gmx.de>
2010-09-09 12:09:22 +0200

    Fix maven for MacOSX
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- bin/maven.sh
+++ bin/maven.sh
@@ -23,9 +23,15 @@ then
 	die "Could not get maven"
 fi
 
-FIJI="$CWD/../fiji"
-JAVA_HOME="$("$FIJI" --print-java-home)/.."
-JAVACMD="$JAVA_HOME/bin/java"
+if test Darwin = "$(uname -s)"
+then
+	JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/
+	JAVACMD=$JAVA_HOME/Commands/java
+else
+	FIJI="$CWD/../fiji"
+	JAVA_HOME="$("$FIJI" --print-java-home)/.."
+	JAVACMD="$JAVA_HOME/bin/java"
+fi
 export JAVACMD JAVA_HOME
 
 exec "$MVN" "$@"
