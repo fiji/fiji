6a50302a9a0783c9a965a0db7b8037dee3f74ff5
Johannes Schindelin <johannes.schindelin@gmx.de>
2010-09-25 20:50:34 +0200

    Fix bin/force-glibc-compat.sh for 64-bit Linux
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- bin/force-glibc-compat.sh
+++ bin/force-glibc-compat.sh
@@ -13,6 +13,15 @@ cat << EOF > glibc-compat.h
 
 EOF
 
+case "$(uname -m)" in
+x86_64)
+	version_regex='2\.2\.5'
+	;;
+*)
+	version_regex='2\.0'
+	;;
+esac
 objdump -t fiji |
-sed -n -e 's/.* \([^ ]*\)\@\@\(GLIBC_2\.[^0].*\)/GLIBC_COMPAT_SYMBOL(\1);/p' \
+grep -v -e "GLIBC_$version_regex" |
+sed -n -e 's/.* \([^ ]*\)\@\@\(GLIBC_.*\)/GLIBC_COMPAT_SYMBOL(\1);/p' \
 >> glibc-compat.h
