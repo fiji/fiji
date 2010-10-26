64bd2e657eaef049aa7920a0c3e0addfd19ee09b
Ignacio Arganda <iarganda@pons.(none)>
2010-10-26 17:35:29 -0400

    Weka Segmentation: updated FastRfBagging to use the new method for resampling with weights.

--- src-plugins/Trainable_Segmentation/hr/irb/fastRandomForest/FastRfBagging.java
+++ src-plugins/Trainable_Segmentation/hr/irb/fastRandomForest/FastRfBagging.java
@@ -156,7 +156,7 @@ class FastRfBagging extends RandomizableIteratedSingleClassifierEnhancer
 
         // create the in-bag dataset (and be sure to remember what's in bag)
         // for computing the out-of-bag error later
-        DataCache bagData = myData.resample(bagSize, random);
+        DataCache bagData = myData.resampleWithWeights( random );
         bagData.reusableRandomGenerator = bagData.getRandomNumberGenerator(
                 random.nextInt());
         inBag[treeIdx] = bagData.inBag;
