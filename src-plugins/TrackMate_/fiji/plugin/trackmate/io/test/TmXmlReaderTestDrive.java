9173756a6cb25edd8c675b1553ef18284df536f3
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2010-11-05 16:26:54 +0100

    TrackMate_: test files for the file loader
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

---
+++ src-plugins/TrackMate_/fiji/plugin/trackmate/io/test/TmXmlReaderTestDrive.java
@@ -0,0 +1,116 @@
+package fiji.plugin.trackmate.io.test;
+
+import ij.ImagePlus;
+import ij.gui.NewImage;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeMap;
+
+import mpicbg.imglib.image.Image;
+import mpicbg.imglib.image.display.imagej.ImageJFunctions;
+import mpicbg.imglib.type.numeric.real.FloatType;
+
+import org.jdom.DataConversionException;
+import org.jdom.JDOMException;
+import org.jgrapht.alg.ConnectivityInspector;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+
+import fiji.plugin.trackmate.Spot;
+import fiji.plugin.trackmate.io.TmXmlReader;
+import fiji.plugin.trackmate.visualization.SpotDisplayer2D;
+
+public class TmXmlReaderTestDrive {
+
+	private static final String FILE_NAME = "SmallFakeTracks_TrackMateData.xml";
+	
+	
+	public static void main(String args[]) {
+		
+		File file = new File(TmXmlReaderTestDrive.class.getResource(FILE_NAME).getFile());
+		System.out.println("Opening file: "+file.getAbsolutePath());		
+		TmXmlReader reader = new TmXmlReader(file);
+		
+		// Parse
+		try {
+			reader.parse();
+		} catch (JDOMException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// Image
+		Image<? extends FloatType> img = reader.getImage();
+		ImagePlus imp = ImageJFunctions.copyToImagePlus(img);
+		imp.show();
+		
+		/*
+		
+		// All spots
+		TreeMap<Integer, List<Spot>> spots = null;
+		try {
+			spots = reader.getAllSpots();
+		} catch (DataConversionException e) {
+			e.printStackTrace();
+		}
+		ArrayList<Spot> spotList = new ArrayList<Spot>();
+		for(int frame : spots.keySet())
+			for(Spot spot : spots.get(frame))
+				spotList.add(spot);
+		System.out.println("Found "+spotList.size()+" spots over "+spots.keySet().size()+" different frames.");
+//		for (Spot spot : spotList) {
+//			System.out.println(spot.toString());
+//		}
+		
+		// Spot selection
+		TreeMap<Integer, List<Spot>> selectedSpots = null;
+		try {
+			selectedSpots = reader.getSpotSelection(spots);
+		} catch (DataConversionException e) {
+			e.printStackTrace();
+		}
+		System.out.println("The spot selection contains "+spotList.size()+" spots over "+spots.keySet().size()+" different frames.");
+
+		// Tracks
+		SimpleGraph<Spot, DefaultEdge> trackGraph = null;
+		try {
+			trackGraph = reader.getTracks(selectedSpots);
+		} catch (DataConversionException e) {
+			e.printStackTrace();
+		}
+		List<Set<Spot>> tracks = new ConnectivityInspector<Spot, DefaultEdge>(trackGraph).connectedSets();		
+		System.out.println("Found "+tracks.size()+" tracks.");
+		
+		// Display
+		
+		// Create fake image
+		Integer[] frames = spots.keySet().toArray(new Integer[0]);
+		int minFrame = Integer.MAX_VALUE;
+		int maxFrame = Integer.MIN_VALUE;
+		for(int i : frames) {
+			if (i > maxFrame)
+				maxFrame = i;
+			if (i < minFrame)
+				minFrame = i;
+		}
+		ImagePlus imp = NewImage.createByteImage("FakeImage", 128, 128, maxFrame+1, NewImage.FILL_BLACK);
+		imp.setDimensions(1, 1, maxFrame+1);
+
+		// Instantiate displayer
+		SpotDisplayer2D displayer = new SpotDisplayer2D(imp);
+		displayer.render();
+		displayer.setSpots(spots);
+		displayer.setSpotsToShow(selectedSpots);
+		displayer.refresh();
+		displayer.setTrackGraph(trackGraph);
+		
+		*/
+	}
+	
+	
+}
