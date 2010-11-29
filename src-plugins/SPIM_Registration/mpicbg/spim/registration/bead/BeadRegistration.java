43b792163b795cc35a96046455918c77d6f62b11
Stephan Preibisch <stephan.preibisch@gmx.de>
2010-11-29 17:43:49 +0100

    last missing parts...

--- src-plugins/SPIM_Registration/mpicbg/spim/registration/bead/BeadRegistration.java
+++ src-plugins/SPIM_Registration/mpicbg/spim/registration/bead/BeadRegistration.java
@@ -550,7 +550,7 @@ public class BeadRegistration
 		return tc;
 	}
 
-	public static void concatenateAxialScaling( final AbstractAffineModel3D<?> model, final double zStretching )
+	public static void concatenateAxialScaling( final AbstractAffineModel3D model, final double zStretching )
 	{
 		if ( model != null )
 		{
