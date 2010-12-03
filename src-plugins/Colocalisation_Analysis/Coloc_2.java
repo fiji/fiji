a9011f361b9f2aafeea3a1307093704a42e33024
Daniel James White <dan@chalkie.org.uk>
2010-12-03 12:34:33 +0100

    Coloc 2: explicit GPL license text added... just in case.

--- src-plugins/Colocalisation_Analysis/Coloc_2.java
+++ src-plugins/Colocalisation_Analysis/Coloc_2.java
@@ -29,7 +29,25 @@ import algorithms.MissingPreconditionException;
 import algorithms.PearsonsCorrelation;
 
 /**
- * A plugin which does colocalisation on two images.
+   Copyright 2010, 2011 Daniel J. White, Tom Kazimiers, Johannes Schindelin
+   and the Fiji project. Fiji is just imageJ - batteries included.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see http://www.gnu.org/licenses/ .
+ */
+
+/**
+ * A plugin which does analysis colocalisation on a pair of images.
  *
  * @param <T>
  */
