2b9624df51810502c061d1f09d22f96f9d1d1196
Johannes Schindelin <johannes.schindelin@gmx.de>
2010-12-26 00:46:13 +0100

    Script Editor: use generics properly
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/Script_Editor/fiji/scripting/completion/Package.java
+++ src-plugins/Script_Editor/fiji/scripting/completion/Package.java
@@ -2,7 +2,7 @@ package fiji.scripting.completion;
 
 import java.util.TreeSet;
 
-public class Package extends TreeSet<Item> implements Item, Comparable {
+public class Package extends TreeSet<Item> implements Item, Comparable<Item> {
 	String key;
 
 	public Package() {}
@@ -15,8 +15,7 @@ public class Package extends TreeSet<Item> implements Item, Comparable {
 		return key;
 	}
 
-	public int compareTo(Object o) {
-		Item tree = (Item)o;
+	public int compareTo(Item tree) {
 		return(getName().compareTo(tree.getName()));
 	}
 }
