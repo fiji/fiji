605ea45826e396c8ce25c54c7d82672f3425e875
Johannes Schindelin <johannes.schindelin@gmx.de>
2011-01-05 18:53:46 +0100

    Make a warning message more meaningful
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- fiji.c
+++ fiji.c
@@ -634,7 +634,7 @@ size_t get_memory_size(int available_only)
 #else
 size_t get_memory_size(int available_only)
 {
-	fprintf(stderr, "Unsupported\n");
+	fprintf(stderr, "Cannot reserve optimal memory on this platform\n");
 	return 0;
 }
 #endif
