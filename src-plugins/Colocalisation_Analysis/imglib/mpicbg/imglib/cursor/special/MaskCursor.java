4cc4cd7834d2c1a4875d204328b584d896a5e837
Tom Kazimiers <tom@voodoo-arts.net>
2011-01-07 13:51:03 +0100

    Coloc 2: Make MaskCursor a LocalizableCursor and change the typing
    The mask time needs for now to be the same as the image type. This is due
    to Imglib converting all ImageJ inputs the same and because of the MetaCursor.

--- src-plugins/Colocalisation_Analysis/imglib/mpicbg/imglib/cursor/special/MaskCursor.java
+++ src-plugins/Colocalisation_Analysis/imglib/mpicbg/imglib/cursor/special/MaskCursor.java
@@ -10,146 +10,121 @@ import mpicbg.imglib.cursor.Cursor;
 import mpicbg.imglib.cursor.LocalizableCursor;
 import mpicbg.imglib.cursor.LocalizableByDimCursor;
 import mpicbg.imglib.cursor.Iterable;
+import imglib.mpicbg.imglib.cursor.special.meta.AlwaysTruePredicate;
+import imglib.mpicbg.imglib.cursor.special.meta.AboveThresholdPredicate;
 
 /**
  * A MaskCursor allows to specify a mask image for the image to walk
  * over.
+ *
+ * Author: Tom Kazimiers
  */
-public class MaskCursor<T extends Type<T>> implements Cursor<T> {
+public class MaskCursor< T extends Type<T> & Comparable<T> > extends ConstraintCursor<T> implements LocalizableCursor<T> {
 	// the curser of the original image
 	LocalizableByDimCursor<T> imageCursor;
-	// the curser of the original image
-	LocalizableCursor<ByteType> maskCursor;
 	// the mask image used for driving the cursor
-	Image<ByteType> mask;
+	Image<T> mask;
 
 	/**
-	 * Creates a new MetaCursor, based on a cursor over the existing
-	 * image and a mask that is used to drive the cursor.
+	 * Creates a new MaskCursor, based on a cursor over an existing
+	 * image and a mask that is used to drive the cursor. If the mask is
+	 * "on" or "off" is specified by the "offValue". Every value in the
+	 * mask that is langer than this, will be "on".
+	 *
+	 * It resumes the cursor to be reset and will require ownership
+	 * of it, i.e. it will be closed if the MaskCursor gets closed.
+	 *
+	 * @param cursor The cursor over which the masked walk should happen.
+	 * @param mask The mask for the cursor.
+	 * @param offValue The value specifing the "off" state in the mask.
 	 */
-	public MaskCursor(LocalizableByDimCursor cursor, Image<ByteType> mask) {
+	public MaskCursor(LocalizableByDimCursor<T> cursor, Image<T> mask, T offValue) {
+		super( cursor, mask.createCursor(),
+			new AlwaysTruePredicate(),
+			new AboveThresholdPredicate( offValue ) );
+		// for masking we want the forward mode to be "And"
+		setForwardMode( ForwardMode.And );
 		imageCursor = cursor;
-		// create a cursor for walking over the mask
-		maskCursor = mask.createLocalizableCursor();
 		this.mask = mask;
-	}
-
-	/**
-	 * Reset the images and the masks cursors.
-	 */
-	@Override
-	public void reset() {
-		imageCursor.reset();
-		maskCursor.reset();
-	}
-
-	@Override
-	public boolean isActive() {
-		return imageCursor.isActive() && maskCursor.isActive();
-	}
-
-	@Override
-	public Image<T> getImage() {
-		return imageCursor.getImage();
 	}
 
 	/**
 	 * Gets the mask that is used to drive the image cursor.
 	 */
-	public Image<ByteType> getMask() {
+	public Image<T> getMask() {
 		return mask;
-	}
-
-	@Override
-	public T getType() {
-		return imageCursor.getType();
-	}
-
-	@Override
-	public int getArrayIndex() {
-		return imageCursor.getArrayIndex();
-	}
-
-	@Override
-	public int getStorageIndex() {
-		return imageCursor.getStorageIndex();
-	}
-
-	@Override
-	public Container<T> getStorageContainer() {
-		return imageCursor.getStorageContainer();
-	}
-
-	@Override
-	public void setDebug( final boolean debug ) {
-		imageCursor.setDebug( debug );
-		maskCursor.setDebug( debug );
-	}
-
-	@Override
-	public int[] createPositionArray() {
-		return imageCursor.createPositionArray();
-	}
-
-	@Override
-	public void close() {
-		imageCursor.close();
-		maskCursor.close();
 	}
 
 	@Override
 	public Iterator<T> iterator()
 	{
-		return imageCursor.iterator();
+		throw new UnsupportedOperationException("This method has not been implemented, yet.");
+	}
+	@Override
+	public T getType() {
+		return getChannel1Type();
 	}
 
 	@Override
-	public void remove()
-	{
-		imageCursor.remove();
-		maskCursor.remove();
+	public Image<T> getImage() {
+		return cursor1.getImage();
 	}
 
 	@Override
-	public T next()
-	{
-		fwd();
-		return getType();
+	public int getArrayIndex() {
+		return cursor1.getArrayIndex();
 	}
 
 	@Override
-	public void fwd( final long steps )
-	{
-		// TODO
+	public int getStorageIndex() {
+		return cursor1.getStorageIndex();
 	}
 
 	@Override
-	public void fwd()
-	{
-		//if (i
-		// TODO
+	public Container<T> getStorageContainer() {
+		return cursor1.getStorageContainer();
 	}
 
 	@Override
-	public int getNumDimensions()
-	{
-		return imageCursor.getNumDimensions();
+	public int[] createPositionArray() {
+		return cursor1.createPositionArray();
+	}
+
+	@Override
+	public int getNumDimensions() {
+		return cursor1.getNumDimensions();
 	}
 
 	@Override
 	public int[] getDimensions() {
-		return imageCursor.getDimensions();
+		return cursor1.getDimensions();
 	}
 
 	@Override
-	public void getDimensions( int[] position )
-	{
-		imageCursor.getDimensions( position );
+	public void getDimensions( int[] position ) {
+		cursor1.getDimensions( position );
+	}
+
+	/* Localizable implementatio */
+
+	@Override
+	public void getPosition( int[] position ) {
+		imageCursor.getPosition( position );
 	}
 
 	@Override
-	public boolean hasNext()
-	{
-		return imageCursor.hasNext();
+	public int[] getPosition() {
+		return imageCursor.getPosition();
+	}
+
+	@Override
+	public int getPosition( int dim ) {
+		return imageCursor.getPosition( dim );
+	}
+
+	@Override
+	public String getPositionAsString() {
+		return imageCursor.getPositionAsString();
 	}
 }
+
