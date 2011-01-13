2d19fc9aea96c029d513cbe501ef4527a5bdfba0
Daniel James White <dan@chalkie.org.uk>
2011-01-13 15:13:13 +0100

    Coloc 2: Add one more Pearsons test with random noise data

--- src-plugins/Colocalisation_Analysis/tests/PearsonsCorrelationTest.java
+++ src-plugins/Colocalisation_Analysis/tests/PearsonsCorrelationTest.java
@@ -3,7 +3,9 @@ package tests;
 import static org.junit.Assert.assertTrue;
 import mpicbg.imglib.cursor.special.TwinValueRangeCursor;
 import mpicbg.imglib.cursor.special.TwinValueRangeCursorFactory;
+import mpicbg.imglib.image.Image;
 import mpicbg.imglib.type.numeric.integer.UnsignedByteType;
+import mpicbg.imglib.type.numeric.real.FloatType;
 
 import org.junit.Test;
 
@@ -78,5 +80,33 @@ public class PearsonsCorrelationTest extends ColocalisationTest {
 			.classicPearsons(cursor, positiveCorrelationImageCh1Mean, positiveCorrelationImageCh2Mean);
 		// check Pearsons R is close to 0.75
 		assertTrue(pearsonsR > 0.745 && pearsonsR < 0.755 );
+	}
+
+	/**
+	 * Tests Pearson's correlation stays close to zero for image pairs with the same mean and spread
+	 * of randomized pixel values around that mean.
+	 */
+	@Test
+	public void differentMeansTest() throws MissingPreconditionException {
+		final double initialMean = 0.2;
+		final double spread = 0.1;
+		final double[] sigma = new double[] {3.0, 3.0};
+
+		for (double mean = initialMean; mean < 1; mean += spread) {
+			Image<FloatType> ch1 = TestImageAccessor.produceMeanBasedNoiseImage(new FloatType(), 512, 512, mean, spread, sigma);
+			Image<FloatType> ch2 = TestImageAccessor.produceMeanBasedNoiseImage(new FloatType(), 512, 512, mean, spread, sigma);
+
+			// create a twin value range cursor that iterates over all pixels of the input data
+			TwinValueRangeCursor<FloatType> cursor = TwinValueRangeCursorFactory
+					.generateAlwaysTrueCursor(ch1, ch2);
+			double resultFast = PearsonsCorrelation.fastPearsons(cursor);
+			assertTrue(Math.abs(resultFast) < 0.1);
+
+			/* This test will throw Missing PreconsitionException, as the means are the same
+			 * which causes a numerical problem in the classic implementation of Pearson's
+			 * double resultClassic = PearsonsCorrelation.classicPearsons(cursor, mean, mean);
+			 * assertTrue(Math.abs(resultClassic) < 0.1);
+			 */
+		}
 	}
 }
