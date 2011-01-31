b9d8bbc02500ca067c524f868401e5f95aecec45
Johannes Schindelin <johannes.schindelin@gmx.de>
2011-01-31 15:39:26 -0600

    Auto Threshold: reimplement Huang a bit more efficiently
    Noticed by Jakub Sedzinski and Marc Bickle.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/Auto_Threshold/fiji/threshold/Auto_Threshold.java
+++ src-plugins/Auto_Threshold/fiji/threshold/Auto_Threshold.java
@@ -378,90 +378,57 @@ public class Auto_Threshold implements PlugIn {
 		return level;
 	}
 
-
 	public static int Huang(int [] data ) {
 		// Implements Huang's fuzzy thresholding method 
 		// Uses Shannon's entropy function (one can also use Yager's entropy function) 
 		// Huang L.-K. and Wang M.-J.J. (1995) "Image Thresholding by Minimizing  
 		// the Measures of Fuzziness" Pattern Recognition, 28(1): 41-51
-		// M. Emre Celebi  06.15.2007
-		// Ported to ImageJ plugin by G. Landini from E Celebi's fourier_0.8 routines
-		int threshold=-1;
-		int ih, it;
-		int first_bin;
-		int last_bin;
-		int sum_pix;
-		int num_pix;
-		double term;
-		double ent;  // entropy 
-		double min_ent; // min entropy 
-		double mu_x;
+		// Reimplemented (to handle 16-bit efficiently) by Johannes Schindelin Jan 31, 2011
 
-		/* Determine the first non-zero bin */
-		first_bin=0;
-		for (ih = 0; ih < data.length; ih++ ) {
-			if ( data[ih] != 0 ) {
-				first_bin = ih;
-				break;
+		// find first and last non-empty bin
+		int first, last;
+		for (first = 0; first < data.length && data[first] == 0; first++)
+			; // do nothing
+		for (last = data.length - 1; last > first && data[last] == 0; last--)
+			; // do nothing
+		if (first == last)
+			return 0;
+
+		// calculate the cumulative density and the weighted cumulative density
+		double[] S = new double[last + 1], W = new double[last + 1];
+		S[0] = data[0];
+		for (int i = Math.max(1, first); i <= last; i++) {
+			S[i] = S[i - 1] + data[i];
+			W[i] = W[i - 1] + i * data[i];
+		}
+
+		// precalculate the summands of the entropy given the absolute difference x - mu (integral)
+		double C = last - first;
+		double[] Smu = new double[last + 1 - first];
+		for (int i = 1; i < Smu.length; i++) {
+			double mu = 1 / (1 + Math.abs(i) / C);
+			Smu[i] = -mu * Math.log(mu) - (1 - mu) * Math.log(1 - mu);
+		}
+
+		// calculate the threshold
+		int bestThreshold = 0;
+		double bestEntropy = Double.MAX_VALUE;
+		for (int threshold = first; threshold <= last; threshold++) {
+			double entropy = 0;
+			int mu = (int)Math.round(W[threshold] / S[threshold]);
+			for (int i = first; i <= threshold; i++)
+				entropy += Smu[Math.abs(i - mu)] * data[i];
+			mu = (int)Math.round((W[last] - W[threshold]) / (S[last] - S[threshold]));
+			for (int i = threshold + 1; i <= last; i++)
+				entropy += Smu[Math.abs(i - mu)] * data[i];
+
+			if (bestEntropy > entropy) {
+				bestEntropy = entropy;
+				bestThreshold = threshold;
 			}
 		}
 
-		/* Determine the last non-zero bin */
-		last_bin=data.length - 1;
-		for (ih = data.length - 1; ih >= first_bin; ih-- ) {
-			if ( data[ih] != 0 ) {
-				last_bin = ih;
-				break;
-			}
-		}
-		term = 1.0 / ( double ) ( last_bin - first_bin );
-		double [] mu_0 = new double[data.length];
-		sum_pix = num_pix = 0;
-		for ( ih = first_bin; ih < data.length; ih++ ){
-			sum_pix += ih * data[ih];
-			num_pix += data[ih];
-			/* NUM_PIX cannot be zero ! */
-			mu_0[ih] = sum_pix / ( double ) num_pix;
-		}
-
-		double [] mu_1 = new double[data.length];
-		sum_pix = num_pix = 0;
-		for ( ih = last_bin; ih > 0; ih-- ){
-			sum_pix += ih * data[ih];
-			num_pix += data[ih];
-			/* NUM_PIX cannot be zero ! */
-			mu_1[ih - 1] = sum_pix / ( double ) num_pix;
-		}
-
-		/* Determine the threshold that minimizes the fuzzy entropy */
-		threshold = -1;
-		min_ent = Double.MAX_VALUE;
-		for ( it = 0; it < data.length; it++ ){
-			ent = 0.0;
-			for ( ih = 0; ih <= it; ih++ ) {
-				/* Equation (4) in Ref. 1 */
-				mu_x = 1.0 / ( 1.0 + term * Math.abs ( ih - mu_0[it] ) );
-				if ( !((mu_x  < 1e-06 ) || ( mu_x > 0.999999))) {
-					/* Equation (6) & (8) in Ref. 1 */
-					ent += data[ih] * ( -mu_x * Math.log ( mu_x ) - ( 1.0 - mu_x ) * Math.log ( 1.0 - mu_x ) );
-				}
-			}
-
-			for ( ih = it + 1; ih < data.length; ih++ ) {
-				/* Equation (4) in Ref. 1 */
-				mu_x = 1.0 / ( 1.0 + term * Math.abs ( ih - mu_1[it] ) );
-				if ( !((mu_x  < 1e-06 ) || ( mu_x > 0.999999))) {
-					/* Equation (6) & (8) in Ref. 1 */
-					ent += data[ih] * ( -mu_x * Math.log ( mu_x ) - ( 1.0 - mu_x ) * Math.log ( 1.0 - mu_x ) );
-				}
-			}
-			/* No need to divide by NUM_ROWS * NUM_COLS * LOG(2) ! */
-			if ( ent < min_ent ) {
-				min_ent = ent;
-				threshold = it;
-			}
-		}
-		return threshold;
+		return bestThreshold;
 	}
 
 	public static boolean bimodalTest(double [] y) {
