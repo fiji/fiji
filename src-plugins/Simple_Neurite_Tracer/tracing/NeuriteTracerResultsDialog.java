8124249e5ccdcb30c56a76fdfaaaa90f2630d599
Mark Longair <mhl@pobox.com>
2011-02-05 20:07:50 -0600

    Simple_Neurite_Tracer: remove an unused import

--- src-plugins/Simple_Neurite_Tracer/tracing/NeuriteTracerResultsDialog.java
+++ src-plugins/Simple_Neurite_Tracer/tracing/NeuriteTracerResultsDialog.java
@@ -50,8 +50,6 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
-
 import javax.swing.JLabel;
 import javax.swing.border.EmptyBorder;
 
