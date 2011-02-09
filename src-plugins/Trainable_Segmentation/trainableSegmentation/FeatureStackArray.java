7134e0be5d6b82ac563843940094bdf3179ddc7b
Ignacio Arganda Carreras <iarganda@kickass.local>
2011-02-09 12:40:49 -0600

    Weka Segmentation: added GPL license to FeatureStackArray.java

--- src-plugins/Trainable_Segmentation/trainableSegmentation/FeatureStackArray.java
+++ src-plugins/Trainable_Segmentation/trainableSegmentation/FeatureStackArray.java
@@ -1,6 +1,29 @@
 package trainableSegmentation;
 
+/**
+ *
+ * License: GPL
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Authors: Ignacio Arganda-Carreras (iarganda@mit.edu), Verena Kaynig (verena.kaynig@inf.ethz.ch),
+ *          Albert Cardona (acardona@ini.phys.ethz.ch)
+ */
+
 import ij.IJ;
+
+
 
 public class FeatureStackArray 
 {
