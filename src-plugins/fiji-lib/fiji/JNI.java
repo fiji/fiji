7121ee91a1d6ad2e1b1781cab6ad4821094fc0de
Johannes Schindelin <johannes.schindelin@gmx.de>
2011-02-11 10:24:24 -0800

    JNI helper: add loadLibraries() method accepting globs
    This is needed for OpenCL, where the library names depend on the OS.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/fiji-lib/fiji/JNI.java
+++ src-plugins/fiji-lib/fiji/JNI.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.util.HashSet;
 import java.util.Set;
 
+import java.util.regex.Pattern;
+
 /**
  * This class helps you with accessing libraries using JNI
  */
@@ -51,16 +53,61 @@ public class JNI {
 			+ (platform != null ? platform + "/" : "");
 	}
 
+	public static void loadLibraries(String nameWithWildcards) {
+		Pattern regex = glob2regex(libraryPrefix + nameWithWildcards + libraryExtension);
+		File dir = new File(libraryDirectory);
+		for (File file : dir.listFiles())
+			if (regex.matcher(file.getName()).matches())
+				loadLibrary(file);
+	}
+
 	public static void loadLibrary(String name) {
-		if (loadedLibraries.contains(name))
+		File library = new File(name);
+		if (!library.isAbsolute()) {
+			library = new File(libraryDirectory, libraryPrefix + name + libraryExtension);
+			if (fallbackLibraryExtension != null && !library.exists())
+				library = new File(libraryDirectory, libraryPrefix + name + fallbackLibraryExtension);
+		}
+		loadLibrary(library);
+	}
+
+	public static void loadLibrary(File library) {
+		if (loadedLibraries.contains(library.getAbsolutePath()))
 			return;
-		File library = new File(libraryDirectory, libraryPrefix + name + libraryExtension);
-		if (fallbackLibraryExtension != null && !library.exists())
-			library = new File(libraryDirectory, libraryPrefix + name + fallbackLibraryExtension);
 		if (library.exists())
 			System.load(library.getAbsolutePath());
 		else
-			System.loadLibrary(name);
-		loadedLibraries.add(name);
+			System.loadLibrary(library.getName());
+		loadedLibraries.add(library.getAbsolutePath());
+	}
+
+	public static Pattern glob2regex(String glob) {
+		StringBuffer result = new StringBuffer();
+		result.append("^");
+
+		char[] array = glob.toCharArray();
+		int len = array.length;
+		for (int i = 0; i < len; i++) {
+			char c = array[i];
+			if (".^$".indexOf(c) >= 0)
+				result.append("\\" + c);
+			else if (c == '?')
+				result.append("[^/]");
+			else if (c == '*') {
+				if (i + 1 >= len || array[i + 1] != '*')
+					result.append("[^/]*");
+				else {
+					result.append(".*");
+					i++;
+					if (i + 1 < len && array[i + 1]
+							== '/')
+						i++;
+				}
+			} else
+				result.append(c);
+		}
+
+		result.append("$");
+		return Pattern.compile(result.toString());
 	}
 }
