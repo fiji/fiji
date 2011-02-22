d819dca4cd619e9d4302e2089ab236f941a3c719
Benjamin Schmid <bene.schmid@gmail.com>
2011-02-23 00:36:24 +0800

    3D Viewer: Create an Iterable<ImagePlus> class in ContentCreator.
    It will replace getImages(file) and getImages(hyperstack), to avoid
    that all the data needs to be loaded together.

--- src-plugins/3D_Viewer/ij3d/ContentCreator.java
+++ src-plugins/3D_Viewer/ij3d/ContentCreator.java
@@ -11,6 +11,8 @@ import ij.process.StackConverter;
 import java.io.File;
 import java.util.TreeMap;
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import customnode.CustomMesh;
 import customnode.CustomMultiMesh;
@@ -151,46 +153,10 @@ public class ContentCreator {
 	 * @return
 	 */
 	public static ImagePlus[] getImages(ImagePlus imp) {
-
-		int nChannels = imp.getNChannels();
-		int nSlices = imp.getNSlices();
-		int nFrames = imp.getNFrames();
-
-		// setSliceWithoutUpdate() does not update the position!
-		int channel = imp.getChannel();
-		int slice = imp.getSlice();
-		int frame = imp.getFrame();
-
-		ImagePlus[] ret = new ImagePlus[nFrames];
-		int w = imp.getWidth(), h = imp.getHeight();
-
-		ImageStack oldStack = imp.getStack();
-		String oldTitle = imp.getTitle();
-		FileInfo fi = imp.getFileInfo();
-		for(int i = 0; i < nFrames; i++) {
-			ImageStack newStack = new ImageStack(w, h);
-			newStack.setColorModel(oldStack.getColorModel());
-			for(int j = 0; j < nSlices; j++) {
-				int index = imp.getStackIndex(1, j+1, i+1);
-				Object pixels;
-				if (nChannels > 1) {
-					imp.setPositionWithoutUpdate(1, j+1, i+1);
-					pixels = new ColorProcessor(imp
-						.getImage()).getPixels();
-				}
-				else
-					pixels = oldStack.getPixels(index);
-				newStack.addSlice(
-					oldStack.getSliceLabel(index),
-					pixels);
-			}
-			ret[i] = new ImagePlus(oldTitle
-				+ " (frame " + i + ")", newStack);
-			ret[i].setCalibration(imp.getCalibration().copy());
-			ret[i].setFileInfo((FileInfo)fi.clone());
-		}
-		if (nChannels > 1)
-			imp.setPositionWithoutUpdate(channel, slice, frame);
+		ImagePlus[] ret = new ImagePlus[imp.getNFrames()];
+		int i = 0;
+		for(ImagePlus frame : HyperStackIterator.getIterable(imp))
+			ret[i++] = frame;
 		return ret;
 	}
 
@@ -204,24 +170,10 @@ public class ContentCreator {
 	 * @return
 	 */
 	public static ImagePlus[] getImages(File file) {
-		if(!file.isDirectory()) {
-			ImagePlus image = IJ.openImage(
-				file.getAbsolutePath());
-			return image == null ? null : getImages(image);
-		}
-		// get the file names
-		String[] names = file.list();
-		if (names.length == 0)
-			return null;
-		Arrays.sort(names);
-		ImagePlus[] ret = new ImagePlus[names.length];
-		for(int i = 0, j = 0; i < ret.length; i++) {
-			File f = new File(file, names[i]);
-			ImagePlus imp = IJ.openImage(f.getAbsolutePath());
-			if(imp != null)
-				ret[j++] = imp;
-		}
-		return ret;
+		ArrayList<ImagePlus> images = new ArrayList<ImagePlus>();
+		for(ImagePlus frame : FileIterator.getIterable(file))
+			images.add(frame);
+		return images.toArray(new ImagePlus[] {});
 	}
 
 	public static void convert(ImagePlus image) {
@@ -239,6 +191,128 @@ public class ContentCreator {
 					new StackConverter(image).
 						convertToGray8();
 				break;
+		}
+	}
+
+	private static class FileIterator implements Iterator<ImagePlus>, Iterable<ImagePlus> {
+
+		public static Iterable<ImagePlus> getIterable(File file) {
+			if(!file.isDirectory()) {
+				return HyperStackIterator.getIterable(
+					IJ.openImage(file.getAbsolutePath()));
+			}
+			return new FileIterator(file);
+		}
+
+		private final String directory;
+		private final String[] names;
+		private int nextIndex = 0;
+
+		/**
+		 * file may be a single file or a directory.
+		 */
+		private FileIterator(File file) {
+			if(!file.isDirectory()) {
+				names = new String[] { file.getName() };
+				directory = file.getParentFile().getAbsolutePath();
+				return;
+			}
+			// get the file names
+			directory = file.getAbsolutePath();
+			names = file.list();
+			Arrays.sort(names);
+		}
+
+		public Iterator<ImagePlus> iterator() {
+			return this;
+		}
+
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+
+		public boolean hasNext() {
+			return nextIndex < names.length;
+		}
+
+		public ImagePlus next() {
+			if(nextIndex == names.length)
+				return null;
+
+			File f = new File(directory, names[nextIndex]);
+			nextIndex++;
+			try {
+				return IJ.openImage(f.getAbsolutePath());
+			} catch(Exception e) {
+				return null;
+			}
+		}
+	}
+
+	private static class HyperStackIterator implements Iterator<ImagePlus>, Iterable<ImagePlus> {
+
+		public static Iterable<ImagePlus> getIterable(ImagePlus image) {
+			return new HyperStackIterator(image);
+		}
+
+		private final ImagePlus image;
+		private final int nChannels;
+		private final int nSlices;
+		private final int nFrames;
+		private final int w;
+		private final int h;
+		private int nextFrame = 0;
+
+		private HyperStackIterator(ImagePlus image) {
+			this.image = image;
+			nChannels = image.getNChannels();
+			nSlices = image.getNSlices();
+			nFrames = image.getNFrames();
+			System.out.println("nFrames = " + nFrames);
+			w = image.getWidth();
+			h = image.getHeight();
+		}
+
+		public Iterator<ImagePlus> iterator() {
+			return this;
+		}
+
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+
+		public boolean hasNext() {
+			return nextFrame < nFrames;
+		}
+
+		public ImagePlus next() {
+			if(nextFrame == nFrames)
+				return null;
+
+			ImageStack oldStack = image.getStack();
+			String oldTitle = image.getTitle();
+			FileInfo fi = image.getFileInfo();
+			ImageStack newStack = new ImageStack(w, h);
+			newStack.setColorModel(oldStack.getColorModel());
+			for(int j = 0; j < nSlices; j++) {
+				int index = image.getStackIndex(1, j + 1, nextFrame + 1);
+				Object pixels;
+				if (nChannels > 1) {
+					image.setPositionWithoutUpdate(1, j + 1, nextFrame + 1);
+					pixels = new ColorProcessor(image.getImage()).getPixels();
+				} else {
+					pixels = oldStack.getPixels(index);
+				}
+				newStack.addSlice(
+					oldStack.getSliceLabel(index),
+					pixels);
+			}
+			ImagePlus ret = new ImagePlus(oldTitle
+				+ " (frame " + nextFrame + ")", newStack);
+			ret.setCalibration(image.getCalibration().copy());
+			ret.setFileInfo((FileInfo)fi.clone());
+			nextFrame++;
+			return ret;
 		}
 	}
 }
