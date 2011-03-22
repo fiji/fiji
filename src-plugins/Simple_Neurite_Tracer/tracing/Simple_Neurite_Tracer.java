02e6cbe2359290e6e4de6ddf7c14620d32bb24f0
Mark Longair <longair@ini.phys.ethz.ch>
2011-03-22 17:48:31 +0100

    Simple_Neurite_Tracer: grab the keystrokes the plugin needs, and pass on other
    This fixes a long-standing problem where the plugin was not correctly
    overriding ImageJ's default key presses for the key shortcuts it uses.
    
    I've fixed this by creating a new KeyListener, called
    QueueJumpingKeyListener which can have a list of fallback
    keylisteners added to it - it will try those if it's not
    interested in the keypress until the event is consumed.
    
    There's also a helper method, setAsFirstKeyListener, which
    will will remove and save all the key listeners from a
    component, add them to a new QueueJumpingKeyListener and
    then set the QueueJumpingKeyListener as the only key listener
    for the component.
    
    This is necessary because there's no guaranteed order in which
    the key listeners are called, so if you want to be called first
    you have to make sure that you're the only key listener.  There's
    some discussion of that here:
    
       http://stackoverflow.com/questions/5274172/is-the-order-in-which-keylisteners-will-be-called-guaranteed

--- src-plugins/Simple_Neurite_Tracer/tracing/Simple_Neurite_Tracer.java
+++ src-plugins/Simple_Neurite_Tracer/tracing/Simple_Neurite_Tracer.java
@@ -398,16 +398,19 @@ public class Simple_Neurite_Tracer extends SimpleNeuriteTracer
 				}
 			}
 
-			xy_tracer_canvas.addKeyListener( xy_tracer_canvas );
-			xy_window.addKeyListener( xy_tracer_canvas );
+			QueueJumpingKeyListener xy_listener = new QueueJumpingKeyListener( this, xy_tracer_canvas );
+			setAsFirstKeyListener( xy_tracer_canvas, xy_listener );
+			setAsFirstKeyListener( xy_window, xy_listener );
 
 			if( ! single_pane ) {
 
-				xz_tracer_canvas.addKeyListener( xz_tracer_canvas );
-				xz_window.addKeyListener( xz_tracer_canvas );
+				QueueJumpingKeyListener xz_listener = new QueueJumpingKeyListener( this, xz_tracer_canvas );
+				setAsFirstKeyListener( xz_tracer_canvas, xz_listener );
+				setAsFirstKeyListener( xz_window, xz_listener );
 
-				zy_tracer_canvas.addKeyListener( zy_tracer_canvas );
-				zy_window.addKeyListener( zy_tracer_canvas );
+				QueueJumpingKeyListener zy_listener = new QueueJumpingKeyListener( this, zy_tracer_canvas );
+				setAsFirstKeyListener( zy_tracer_canvas, zy_listener );
+				setAsFirstKeyListener( zy_window, zy_listener );
 
 			}
 
