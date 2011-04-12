5a46eb64723731a8074cd192f1b328141b3ec56e
Johannes Schindelin <johannes.schindelin@gmx.de>
2011-04-12 09:58:49 -0700

    build-itk: build SimpleITK, too
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- bin/build-itk.sh
+++ bin/build-itk.sh
@@ -24,12 +24,25 @@ if test ! -d Insight
 then
 	git clone contrib@pacific.mpi-cbg.de:/srv/git/Insight
 fi &&
-CMAKE_COMMAND="../cmake/bin/cmake -D BUILD_TESTING=OFF -D BUILD_EXAMPLES=OFF" &&
-if test ! -d ITK-build
+CMAKE_COMMAND="../cmake/bin/cmake -D BUILD_TESTING=OFF -D BUILD_EXAMPLES=OFF -D ITK_BUILD_ALL_MODULES=ON" &&
+if test ! -f ITK-build/bin/libITKAlgorithms.so
 then
-	mkdir ITK-build &&
+	mkdir -p ITK-build &&
 	(cd ITK-build &&
 	 eval $CMAKE_COMMAND ../Insight &&
+	 make $PARALLEL)
+fi &&
+echo SimpleITK &&
+if test ! -d SimpleITK
+then
+	git clone contrib@pacific.mpi-cbg.de:/srv/git/SimpleITK
+fi &&
+CMAKE_COMMAND_SIMPLEITK="../cmake/bin/cmake $(cat SimpleITK/Wrapping/CMakeLists.txt  | tr -c 'A-Z_' '\n' | grep ^WRAP | sort | uniq | sed 's/.*/-D &=OFF/')" &&
+if test ! -f SimpleITK-build/TODO
+then
+	mkdir -p SimpleITK-build &&
+	(cd SimpleITK-build &&
+	 eval $CMAKE_COMMAND_SIMPLEITK -DITK_DIR=$FIJI_ROOT/3rdparty/ITK-build ../SimpleITK &&
 	 make $PARALLEL)
 fi &&
 for target in macosx32 macosx64
@@ -48,6 +61,7 @@ do
 		SYSPREFIX="$SYSROOT/bin/x86_64-apple-darwin8"
 		;;
 	esac &&
+	echo "ITK ($target)" &&
 	TRYRUN=TryRunResults-$target.cmake &&
 	if test ! -e ITK-build-$target/bin/libITKAlgorithms.$SO
 	then
@@ -183,5 +197,13 @@ EOF
 				-C ../$TRYRUN ../Insight/ &&
 			make $PARALLEL
 		})
+	fi &&
+	echo "SimpleITK ($target)" &&
+	if test ! -f SimpleITK-build-$target/TODO
+	then
+		mkdir -p SimpleITK-build-$target &&
+		(cd SimpleITK-build-$target &&
+		 eval $CMAKE_COMMAND_SIMPLEITK -DITK_DIR=$FIJI_ROOT/3rdparty/ITK-build-$target ../SimpleITK &&
+		 make $PARALLEL)
 	fi || exit 1
 done
