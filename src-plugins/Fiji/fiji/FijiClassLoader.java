cf8054f784a0d3076ba30fa2c33103971a093cb7
Johannes Schindelin <johannes.schindelin@gmx.de>
2011-04-23 23:54:20 +0200

    FijiClassLoader: when adding paths recursively, skip hidden files & dirs
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/Fiji/fiji/FijiClassLoader.java
+++ src-plugins/Fiji/fiji/FijiClassLoader.java
@@ -98,7 +98,8 @@ public class FijiClassLoader extends URLClassLoader {
 			}
 			String[] paths = file.list();
 			for (int i = 0; i < paths.length; i++)
-				addPath(path + File.separator + paths[i]);
+				if (!paths[i].startsWith("."))
+					addPath(path + File.separator + paths[i]);
 		}
 		else if (path.endsWith(".jar")) {
 			try {
