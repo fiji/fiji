b3c3534cb45f43eff8a818343b0fb7fda6a0eb6a
Benjamin Schmid <bene.schmid@gmail.com>
2011-05-25 17:31:45 +0200

    3D Viewer: Fix switching between play/pause button.
    Same for forward only/bounce back buttons.

--- src-plugins/3D_Viewer/view4d/TimelineGUI.java
+++ src-plugins/3D_Viewer/view4d/TimelineGUI.java
@@ -163,11 +163,13 @@ public class TimelineGUI implements ActionListener, KeyListener {
 			return;
 		if (buttons[playIndex].getActionCommand().equals("PLAY")) {
 			buttons[playIndex].setActionCommand("PAUSE");
+			buttons[playIndex].setIcon(new ImageIcon(pauseImage));
 			buttons[playIndex].repaint();
 			timeline.play();
 		}
 		else {
 			buttons[playIndex].setActionCommand("PLAY");
+			buttons[playIndex].setIcon(new ImageIcon(playImage));
 			buttons[playIndex].repaint();
 			timeline.pause();
 		}
@@ -191,20 +193,25 @@ public class TimelineGUI implements ActionListener, KeyListener {
 			buttons[i].repaint();
 
 		String command = e.getActionCommand();
+
 		if(command.equals("BOUNCEBACK")) {
 			buttons[bbIndex].setActionCommand("NOBOUNCEBACK");
+			buttons[bbIndex].setIcon(new ImageIcon(nbbImage));
 			buttons[bbIndex].repaint();
 			timeline.setBounceBack(true);
 		} else if(command.equals("NOBOUNCEBACK")) {
 			buttons[bbIndex].setActionCommand("BOUNCEBACK");
+			buttons[bbIndex].setIcon(new ImageIcon(bbImage));
 			buttons[bbIndex].repaint();
 			timeline.setBounceBack(false);
 		} else if(command.equals("PLAY")) {
 			buttons[playIndex].setActionCommand("PAUSE");
+			buttons[playIndex].setIcon(new ImageIcon(pauseImage));
 			buttons[playIndex].repaint();
 			timeline.play();
 		} else if(command.equals("PAUSE")) {
 			buttons[playIndex].setActionCommand("PLAY");
+			buttons[playIndex].setIcon(new ImageIcon(playImage));
 			buttons[playIndex].repaint();
 			timeline.pause();
 		} else if(command.equals("RECORD")) {
