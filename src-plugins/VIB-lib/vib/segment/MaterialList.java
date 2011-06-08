b5f254d6919687cdb8faea2ab5e2c81f9a93b6bc
Johannes Schindelin <johannes.schindelin@gmx.de>
2011-06-08 16:01:04 +0200

    Segmentation Editor: sort some imports
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/VIB-lib/vib/segment/MaterialList.java
+++ src-plugins/VIB-lib/vib/segment/MaterialList.java
@@ -1,33 +1,31 @@
 package vib.segment;
 
+import amira.AmiraParameters;
+
 import ij.IJ;
 import ij.ImagePlus;
+
 import ij.gui.ColorChooser;
 import ij.gui.GenericDialog;
 
 import java.awt.AWTEvent;
 import java.awt.Canvas;
+import java.awt.CheckboxMenuItem;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Label;
-import java.awt.CheckboxMenuItem;
 import java.awt.MenuItem;
 import java.awt.Point;
 import java.awt.PopupMenu;
 import java.awt.ScrollPane;
-import java.awt.event.ItemListener;
-import java.awt.event.ItemEvent;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
-import java.util.Iterator;
-
-import javax.naming.OperationNotSupportedException;
-
-import amira.AmiraParameters;
 
 public class MaterialList extends ScrollPane implements ActionListener, ItemListener {
 	PopupMenu popup;
