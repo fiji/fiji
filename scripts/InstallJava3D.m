eda5b1e8b11fe4b855a449c19fbef6298581faea
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2011-07-23 11:23:05 +0200

    Miji: work on the InstallJava3D script
    - Check Java3D version
    - Be more verbose to the user.
    Signed-off-by: Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>

--- scripts/InstallJava3D.m
+++ scripts/InstallJava3D.m
@@ -39,9 +39,21 @@ end
 
 if isJava3DInstalled
     
-    fprintf('Java3D is already installed, you have nothing to do.\n');
-    fprintf('Exiting.\n');
-    return;
+    j3dvsersionstr = char(ij3d.Install_J3D.getJava3DVersion());
+    fprintf('Java3D is already installed.\n')
+    fprintf('Installed version is %s.\n', j3dvsersionstr)
+    
+    j3dvsersion = str2double(j3dvsersionstr);
+    
+    if j3dvsersion >= 1.5
+        fprintf('This is good enough, you have nothing to do.\n');
+        fprintf('Exiting.\n');
+        fprintf('\n');
+        return
+    end
+    
+    fprintf('We need at least version 1.5, so let us update it.\b')
+    fprintf('\n');
     
 else
     
@@ -49,91 +61,104 @@ else
     fprintf('Let us try to install it.\n');
     fprintf('\n');
     
-    fprintf('Determining the target JVM ext folder...\n')
-    path =  char(ij3d.Install_J3D.getFirstExtDir());
-    fprintf('Target path is %s.\n', path);
+end
+
+
+fprintf('Determining the target JVM ext folder...\n')
+path =  char(ij3d.Install_J3D.getFirstExtDir());
+fprintf('Target path is %s.\n', path);
+fprintf('\n');
+
+fprintf('Determining if we have write access to JVM ext folder...\n')
+[status attrib] = fileattrib(path); % STUCK HERE: RETURN WRONG PERMISSIONS
+
+if ~status
+    fprintf('Cannot determine the attributes of %s. Aborting.\n', path);
+    return
+end
+
+if isnan(attrib.UserWrite) || ~attrib.UserWrite
+    
+    fprintf('We do not have the right to write to %s,\ninstallation CANNOT be done automatically.\n', path)
+    fprintf('Turning to a MANUAL installation.\n');
     fprintf('\n');
     
-    fprintf('Determining if we have write access to JVM ext folder...\n')
-    [status attrib] = fileattrib(path); % STUCK HERE: RETURN WRONG PERMISSIONS
-    
-    if ~status
-        fprintf('Cannot determine the attributes of %s. Aborting.\n', path);
+    fprintf('Detecting OS...\n')
+    arch = computer;
+    fprintf('Found %s.\n', arch)
+    if strcmp(arch, 'GLNXA64')
+        filename =   LIN_64;
+    elseif strcmp(arch , 'GLNX86')
+        filename =  LIN_32;
+    elseif strcmp(arch , 'PCWIN64')
+        filename = WIN_64;
+    elseif strcmp(arch , 'PCWIN')
+        filename =  WIN_32;
+    elseif strcmp(arch , 'MACI64')
+        filename = MAC;
+    else
+        fprintf('Could not detect operating system, aborting.\n');
         return
     end
+    fprintf('\n');
     
-    if isnan(attrib.OtherWrite) || ~attrib.OtherWrite
+    fprintf('Downloading the Java3D files...\n')
+    j3dArchive = [JAVA3D_BASE_URL filename];
+    downloaded = ij3d.Install_J3D.download(j3dArchive);
+    fprintf('\n');
+    
+    destfolder =   ij3d.Install_J3D.createFolder([char(java.lang.System.getProperty('user.home')) filesep 'Desktop'], 'Java3D', false);
+    files =  ij3d.Install_J3D.unzip(downloaded, destfolder, 'j3d-jre.zip');
+    fprintf('\n');
+    
+    javaJREFolder = char(java.lang.System.getProperty('java.home'));
+    fprintf('OK, so this is where you have to manually intervene.\n')
+    fprintf('You will have to ask or impersonate your computer''s administrator\n')
+    fprintf('so that it gives you WRITE PERMISSION to the following folder:\n')
+    fprintf('\t -> %s.\n', javaJREFolder)
+    
+    fprintf('\n');
+    fprintf('Once he or you did that, you can either:\n');
+    fprintf(' - Relaunch this helper script, saying ''Yes'' to any question\n');
+    fprintf('   that might be asked.\n');
+    fprintf('\n');
+    fprintf(' - Or move the Java3D files yourself. You will find them in the folder:\n');
+    fprintf('\t -> %s.\n', char(destfolder.getPath()))
+    
+    fprintf('\n');
+    fprintf('In the later case, ');
+    if ~strcmp(arch , 'MACI64')
+        fprintf('since you do not have a MAC, you simply have to unzip the zip file\n');
+        zipfile = char(files.get(0).getPath());
         
-        fprintf('We do not have the right to write to %s,\ninstallation CANNOT be done automatically.\n', path)
-        fprintf('Turning to a MANUAL installation.\n');
+        fprintf('called %s\nin the Java JRE folder: %s.\n', zipfile, javaJREFolder);
         fprintf('\n');
-        
-        fprintf('Detecting OS...\n')
-        arch = computer;
-        fprintf('Found %s.\n', arch)
-        if strcmp(arch, 'GLNXA64')
-                filename =   LIN_64;
-        elseif strcmp(arch , 'GLNX86')
-                filename =  LIN_32;
-        elseif strcmp(arch , 'PCWIN64')
-                filename = WIN_64;
-        elseif strcmp(arch , 'PCWIN')
-                filename =  WIN_32;
-        elseif strcmp(arch , 'MACI64')
-            filename = MAC;
-        else
-            fprintf('Could not detect operating system, aborting.\n');
-            return
-        end
-        fprintf('\n');
-          
-        fprintf('Downloading the Java3D files...\n')
-        j3dArchive = [JAVA3D_BASE_URL filename];
-        downloaded = ij3d.Install_J3D.download(j3dArchive);
-        fprintf('\n');
-        
-        destfolder =   ij3d.Install_J3D.createFolder([char(java.lang.System.getProperty('user.home')) filesep 'Desktop'], 'Java3D', false);
-        files =  ij3d.Install_J3D.unzip(downloaded, destfolder, 'j3d-jre.zip');
-        fprintf('\n');
-        
-        javaJREFolder = char(java.lang.System.getProperty('java.home'));
-        fprintf('OK, so this is where you have to manually intervene.\n')
-        fprintf('You will have to ask or impersonate your computer''s administrator\n')
-        fprintf('so that it gives you WRITE PERMISSION to the following folder:\n')
-        fprintf('\t -> %s.\n', javaJREFolder)
-
-        fprintf('\n');
-        fprintf('Once he or you did that, you can either:\n');
-        fprintf(' - Relaunch this helper script, saying ''Yes'' to any question\n');
-        fprintf('   that might be asked.\n');
-        fprintf('\n');
-        fprintf(' - Or move the Java3D files yourself. You will find them in the folder:\n');
-        fprintf('\t -> %s.\n', char(destfolder.getPath()))
-
-        fprintf('\n');
-        fprintf('In the later case, ');
-        if ~strcmp(arch , 'MACI64')
-            fprintf('since you do not have a MAC, you simply have to unzip the zip file\n');
-            zipfile = char(files.get(0).getPath());
-            
-            fprintf('called %s\nin the Java JRE folder: %s.\n', zipfile, javaJREFolder);
-            fprintf('\n');
-            fprintf('The files will go where they need to.\n');
-        else 
-            
-            % TODO TODO
-            
-        end
-        
-        fprintf('After that, of course, you will still need to relaunch MATLAB.\n');
-        
-        return
-        
+        fprintf('The files will go where they need to.\n');
     else
         
-        fprintf('We can write to the path, installation will proceed.\n')
-        ij3d.Install_J3D().run('')
+        % TODO TODO
         
     end
     
+    fprintf('After that, of course, you will still need to relaunch MATLAB.\n');
+    
+    return
+    
+else
+    
+    fprintf('We can write to the path, installation will proceed.\n')
+    plugin = ij3d.Install_J3D();
+    plugin.run('')
+
+    fprintf('\n');
+    fprintf('Ok, so the files are in place.\n');
+    fprintf('You still need to RELAUNCH MATLAB so that they can be sourced.\n');
+    fprintf('(It will NOT work if you do not restart MATLAB.)\n');
+    fprintf('Then, calling this script again should tell you that everyting is in place.\n');
+    fprintf('Exiting.\n');
+    fprintf('\n');
+
+    
+    
+
 end
