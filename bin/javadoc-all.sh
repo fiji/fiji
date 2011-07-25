fd7d0d1be65d71aa2bfa2b5ff06059f24cb02894
Johannes Schindelin <johannes.schindelin@gmx.de>
2011-07-25 11:14:43 +0200

    javadoc-all: look for Maven's cached sezpoz copy
    ... needed for ImageJ2
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- bin/javadoc-all.sh
+++ bin/javadoc-all.sh
@@ -19,6 +19,7 @@ grep -ve ij-plugins/Sun_JAI_Sample_IO_Source_Code \
 	-e modules/micromanager1.4/Mac |
 ./fiji --javadoc --jarpath modules/bio-formats/jar \
 	--jarpath modules/jython/jython/extlibs \
+	--jarpath $HOME/.m2/repository/net/java/sezpoz/ \
 	-link http://download.java.net/media/java3d/javadoc/1.5.2/ \
 	-link http://java.sun.com/j2se/1.5.0/docs/api/ \
 	"$@" \
