7a78f8cd832b6e3acae1b679adde271243358fb7
Tom Kazimiers <tom@voodoo-arts.net>
2011-10-05 12:59:09 +0200

    Coloc 2: Use TwinCursor in Li's ICQ calculation

--- src-plugins/Colocalisation_Analysis/algorithms/LiICQ.java
+++ src-plugins/Colocalisation_Analysis/algorithms/LiICQ.java
@@ -1,10 +1,11 @@
 package algorithms;
 
-import results.ResultHandler;
 import gadgets.DataContainer;
-import mpicbg.imglib.cursor.Cursor;
+import mpicbg.imglib.cursor.special.TwinCursor;
 import mpicbg.imglib.image.Image;
+import mpicbg.imglib.type.logic.BitType;
 import mpicbg.imglib.type.numeric.RealType;
+import results.ResultHandler;
 
 /**
  * This algorithm calculates Li et al.'s ICQ (intensity
@@ -25,38 +26,36 @@ public class LiICQ<T extends RealType<T>> extends Algorithm<T> {
 		// get the 2 images for the calculation of Li's ICQ
 		Image<T> img1 = container.getSourceImage1();
 		Image<T> img2 = container.getSourceImage2();
+		Image<BitType> mask = container.getMask();
 
+		TwinCursor<T> cursor = new TwinCursor<T>(img1.createLocalizableByDimCursor(),
+				img2.createLocalizableByDimCursor(), mask.createLocalizableCursor());
 		// calculate ICQ value
-		icqValue = calculateLisICQ(img1, mean1, img2, mean2);
+		icqValue = calculateLisICQ(cursor, mean1, mean2);
+		cursor.close();
 	}
 
 	/**
 	 * Calculates Li et al.'s intensity correlation quotient (ICQ) for
 	 * two images.
 	 *
-	 * @param img1 The first image
+	 * @param cursor A TwinCursor that iterates over two images
 	 * @param mean1 The first images mean
-	 * @param img2 The second image
 	 * @param mean2 The second images mean
 	 * @return Li et al.'s ICQ value
 	 */
-	public static <T extends RealType<T>> double calculateLisICQ(Image<T> img1, double mean1, Image<T> img2, double mean2) {
-		// get the cursors for iterating through pixels in images
-		Cursor<T> cursor1 = img1.createCursor();
-		Cursor<T> cursor2 = img2.createCursor();
-
+	public static <T extends RealType<T>> double calculateLisICQ(TwinCursor<T> cursor, double mean1, double mean2) {
 		/* variables to count the positive and negative results
 		 * of Li's product of the difference of means.
 		 */
 		long numPositiveProducts = 0;
 		long numNegariveProducts = 0;
 		// iterate over image
-		while (cursor1.hasNext() && cursor2.hasNext()) {
-			cursor1.fwd();
-			cursor2.fwd();
-			T type1 = cursor1.getType();
+		while (cursor.hasNext()) {
+			cursor.fwd();
+			T type1 = cursor.getChannel1();
+			T type2 = cursor.getChannel1();
 			double ch1 = type1.getRealDouble();
-			T type2 = cursor2.getType();
 			double ch2 = type2.getRealDouble();
 
 			double productOfDifferenceOfMeans = (mean1 - ch1) * (mean2 - ch2);
@@ -67,10 +66,6 @@ public class LiICQ<T extends RealType<T>> extends Algorithm<T> {
 			else
 				++numPositiveProducts;
 		}
-
-		// close the cursors
-		cursor1.close();
-		cursor2.close();
 
 		/* calculate Li's ICQ value by dividing the amount of "positive pixels" to the
 		 * total number of pixels. Then shift it in the -0.5,0.5 range.
