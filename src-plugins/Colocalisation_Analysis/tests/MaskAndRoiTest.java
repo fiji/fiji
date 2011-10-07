c5bfea9f8647495674f8a9f1ac0e816c1040edfb
Tom Kazimiers <tom@voodoo-arts.net>
2011-10-07 15:56:21 +0200

    Coloc 2: adjust irregular mask test cases
    One test that checked the dimensions of previously created MaskImage
    objects has been removed. The things tested there made in their current
    form not much sense for tests against a cursor.

--- src-plugins/Colocalisation_Analysis/tests/MaskAndRoiTest.java
+++ src-plugins/Colocalisation_Analysis/tests/MaskAndRoiTest.java
@@ -3,10 +3,9 @@ package tests;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import gadgets.MaskFactory;
-import gadgets.MaskedImage;
-import mpicbg.imglib.cursor.Cursor;
 import mpicbg.imglib.cursor.LocalizableCursor;
 import mpicbg.imglib.cursor.special.PredicateCursor;
+import mpicbg.imglib.cursor.special.TwinCursor;
 import mpicbg.imglib.cursor.special.predicate.MaskPredicate;
 import mpicbg.imglib.cursor.special.predicate.Predicate;
 import mpicbg.imglib.image.Image;
@@ -124,31 +123,31 @@ public class MaskAndRoiTest extends ColocalisationTest {
 		// create a random noise 2D image -- set roiWidh/roiSize accordingly
 		Image<UnsignedByteType> img =
 			TestImageAccessor.produceSticksNoiseImage(300, 300, 50, 2, 10);
-		// invert the image
-		Image<UnsignedByteType> invImg = TestImageAccessor.invertImage(img);
-		// some general mask data
-		int[] maskOffset = new int[] {0, 0};
-		int[] maskSize = new int[] {invImg.getDimension(0), invImg.getDimension(1)};
 
 		/* first test - using itself as a mask */
-		MaskedImage<UnsignedByteType> maskedImg1 =
-			new MaskedImage<UnsignedByteType>(img, img, maskOffset, maskSize);
-		LocalizableCursor<UnsignedByteType> maskedCursor = maskedImg1.createLocalizableCursor();
-
-		while (maskedCursor.hasNext()) {
-			maskedCursor.fwd();
-			assertTrue( maskedCursor.getType().getInteger() != 0 );
+		Image<BitType> mask = MaskFactory.createMask(img.getDimensions(), img);
+		TwinCursor<UnsignedByteType> cursor = new TwinCursor<UnsignedByteType>(
+				img.createLocalizableByDimCursor(),
+				img.createLocalizableByDimCursor(),
+				mask.createLocalizableCursor());
+		while (cursor.hasNext()) {
+			cursor.fwd();
+			assertTrue( cursor.getType().getInteger() != 0 );
 		}
+		cursor.close();
 
 		/* second test - using inverted image */
-		MaskedImage<UnsignedByteType> invMaskedImg =
-			new MaskedImage<UnsignedByteType>(img, invImg, maskOffset, maskSize);
-		Cursor<UnsignedByteType> invMaskedCursor = invMaskedImg.createCursor();
-
-		while (invMaskedCursor.hasNext()) {
-			invMaskedCursor.fwd();
-			assertEquals( 0, invMaskedCursor.getType().getInteger() );
+		Image<UnsignedByteType> invImg = TestImageAccessor.invertImage(img);
+		Image<BitType> invMask = MaskFactory.createMask(img.getDimensions(), invImg);
+		cursor = new TwinCursor<UnsignedByteType>(
+				img.createLocalizableByDimCursor(),
+				img.createLocalizableByDimCursor(),
+				invMask.createLocalizableCursor());
+		while (cursor.hasNext()) {
+			cursor.fwd();
+			assertEquals( 0, cursor.getType().getInteger() );
 		}
+		cursor.close();
 	}
 
 	@Test
@@ -160,59 +159,23 @@ public class MaskAndRoiTest extends ColocalisationTest {
 		// define rectangular mask to be used as irregular mask
 		int[] roiOffset = new int[] {width / 4, height / 4};
 		int[] roiSize = new int[] {width / 2, height / 2};
-		Image<UnsignedByteType> mask
+		Image<UnsignedByteType> maskImg
 			= TestImageAccessor.createRectengularMaskImage(width, height, roiOffset, roiSize);
+		Image<BitType> mask = MaskFactory.createMask(img.getDimensions(), maskImg);
 
-		MaskedImage<UnsignedByteType> maskImage
-			= new MaskedImage<UnsignedByteType>(img, mask, roiOffset, roiSize);
-
-		// calculate volume of ROI box
+		TwinCursor<UnsignedByteType> cursor = new TwinCursor<UnsignedByteType>(
+				img.createLocalizableByDimCursor(),
+				img.createLocalizableByDimCursor(),
+				mask.createLocalizableCursor());
+		// calculate volume of mask bounding box
 		int roiVolume = roiSize[0] * roiSize[1] * img.getDimension(2);
-		// count pixels in ROI
-		Cursor<UnsignedByteType> maskCursor = maskImage.createCursor();
 		int count = 0;
-		while (maskCursor.hasNext()) {
-			maskCursor.fwd();
+		while (cursor.hasNext()) {
+			cursor.fwd();
 			count++;
 		}
-
-		maskCursor.close();
+		cursor.close();
 
 		assertEquals(roiVolume, count);
-	}
-
-	/**
-	 * This test makes sure the MaskedImage can figure out the
-	 * correct dimensions of the ROI in case on passes a ROI
-	 * with different dimensions than the image.
-	 */
-	@Test
-	public void irregularRoiDimensionsTest() {
-		// load a 3D test image
-		Image<UnsignedByteType> img = positiveCorrelationImageCh1;
-		int width = img.getDimension(0);
-		int height = img.getDimension(1);
-		// define rectangular mask to be used as irregular mask
-		int[] roiOffset = new int[] {width / 4, height / 4};
-		int[] roiSize = new int[] {width / 2, height / 2};
-		Image<UnsignedByteType> mask
-			= TestImageAccessor.createRectengularMaskImage(width, height, roiOffset, roiSize);
-
-		MaskedImage<UnsignedByteType> maskImage
-			= new MaskedImage<UnsignedByteType>(img, mask, roiOffset, roiSize);
-		// is the number of dimensions the same as in the image?
-		assertEquals(maskImage.getNumDimensions(), img.getNumDimensions());
-		assertEquals(maskImage.getOffset().length, img.getNumDimensions());
-		/* Is the ROIs dimension information correct? Is the z dimension
-		 * of the ROI the same as the images one?
-		 */
-		assertEquals(maskImage.getDimension(0), roiSize[0]);
-		assertEquals(maskImage.getDimension(1), roiSize[1]);
-		assertEquals(maskImage.getDimension(2), img.getDimension(2));
-		// Is the ROIs extend correct?
-		int[] ro = maskImage.getOffset();
-		assertEquals(ro[0], roiOffset[0]);
-		assertEquals(ro[1], roiOffset[1]);
-		assertEquals(ro[2], 0);
 	}
 }
