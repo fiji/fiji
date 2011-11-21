31d202a6a393cc58dea09127ea6536908656dfc9
Johannes Schindelin <johannes.schindelin@gmx.de>
2011-11-21 00:42:43 -0600

    MiniMaven: also fall back to Fiji's jars/ for versions like '[1.45p,)'
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/fake/fiji/build/MiniMaven.java
+++ src-plugins/fake/fiji/build/MiniMaven.java
@@ -488,7 +488,7 @@ public class MiniMaven {
 			String path = System.getProperty("user.home") + "/.m2/repository/" + groupId.replace('.', '/') + "/" + artifactId + "/";
 			if (version == null)
 				version = findLocallyCachedVersion(path);
-			if (version == null || artifactId.equals("tools")) {
+			if (version == null || version.startsWith("[") || artifactId.equals("tools")) {
 				// try to find the .jar in Fiji's jars/ dir
 				String jarName = artifactId.equals("tools") ? "javac.jar" : artifactId + ".jar";
 				File file = new File(System.getProperty("fiji.dir"), "jars/" + jarName);
