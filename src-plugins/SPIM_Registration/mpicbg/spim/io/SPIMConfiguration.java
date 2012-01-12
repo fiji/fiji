e4dec4c10885dc3db951ab35ad15a263678e5f76
Stephan Preibisch <stephan.preibisch@gmx.de>
2012-01-12 16:28:30 -0500

    fixed a bug if it is an "old" input stack, illumination was always null

--- src-plugins/SPIM_Registration/mpicbg/spim/io/SPIMConfiguration.java
+++ src-plugins/SPIM_Registration/mpicbg/spim/io/SPIMConfiguration.java
@@ -27,7 +27,7 @@ public class SPIMConfiguration
 	public int timepoints[];
 	public String anglePattern;	
 	public int angles[];
-	public int illuminations[];
+	public int illuminations[] = new int[ 1 ];
 
 	//public String angleString;
 	public String inputFilePattern;//spim_TL{i}_Angle\d*\.lsm
