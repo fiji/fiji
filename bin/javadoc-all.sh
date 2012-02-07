10e9a56ddb4b099c8883a2e02083afa8441c2d34
Johannes Schindelin <johannes.schindelin@gmx.de>
2012-02-06 21:59:19 -0600

    javadoc-all: accept Java3D from Ubuntu's default location
    Again, to support overriding the Java home.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- bin/javadoc-all.sh
+++ bin/javadoc-all.sh
@@ -21,6 +21,7 @@ grep -ve ij-plugins/Sun_JAI_Sample_IO_Source_Code \
 	--javadoc --jarpath modules/bio-formats/jar \
 	--jarpath modules/jython/jython/extlibs \
 	--jarpath $HOME/.m2/repository/net/java/sezpoz/ \
+	--jarpath /usr/share/java/ \
 	-link http://download.java.net/media/java3d/javadoc/1.5.2/ \
 	-link http://java.sun.com/j2se/1.5.0/docs/api/ \
 	@/dev/stdin 2>&1
