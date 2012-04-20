b9a20803862b6a2a0cd5e8891517bb576be720a6
Johannes Schindelin <johannes.schindelin@gmx.de>
2012-04-20 11:38:04 -0500

    Fiji Build: the launcher is now called 'ImageJ'...
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/fake/fiji/build/ExecuteProgram.java
+++ src-plugins/fake/fiji/build/ExecuteProgram.java
@@ -56,7 +56,7 @@ public class ExecuteProgram extends Rule {
 		}
 
 		// special-case ant, since it's slow
-		if (program.startsWith("../../fiji --ant") &&
+		if ((program.startsWith("../../fiji --ant") || program.startsWith("../../ImageJ --ant")) &&
 				prerequisites.size() == 0 &&
 				upToDateRecursive(parser.cwd, new File(target)))
 			return true;
