f1636fffbd67b773a8ad9c5be453239f4c2886e6
Tom Kazimiers <tom@voodoo-arts.net>
2012-06-26 16:33:05 +0200

    Coloc 2: fix slightly wrong bin width calculation of histograms

--- src-plugins/Colocalisation_Analysis/algorithms/Histogram2D.java
+++ src-plugins/Colocalisation_Analysis/algorithms/Histogram2D.java
@@ -271,9 +271,9 @@ public class Histogram2D<T extends RealType< T >> extends Algorithm<T> {
 	protected double getXBinWidth(DataContainer<T> container) {
 		double ch1Max = getMaxCh1(container);
 		// make sure we don't make the bins wider than one
-		ch1Max = ch1Max < xBins ? xBins : ch1Max;
+		ch1Max = ch1Max < xBins ? xBins : ch1Max + 1;
 		// return the width of one bin in the grid
-		return (double) xBins / (double)(ch1Max + 1);
+		return (double) xBins / (double) ch1Max;
 	}
 
 	/**
@@ -284,9 +284,9 @@ public class Histogram2D<T extends RealType< T >> extends Algorithm<T> {
 	protected double getYBinWidth(DataContainer<T> container) {
 		double ch2Max = getMaxCh2(container);
 		// make sure we don't make the bins higher than one
-		ch2Max = ch2Max < yBins ? yBins : ch2Max;
+		ch2Max = ch2Max < yBins ? yBins : ch2Max + 1;
 		// return the width of one bin in the grid
-		return (double) yBins / (double)(ch2Max + 1);
+		return (double) yBins / (double) ch2Max;
 	}
 
 	/**
