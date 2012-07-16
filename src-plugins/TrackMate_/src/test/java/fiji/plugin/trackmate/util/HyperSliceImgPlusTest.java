0a5a3519b35f43763c9eac00cd4fc7537d4bce7a
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2012-07-16 22:51:33 +0200

    Finish writing tests for HyperSliceImgPlus
    Now I can use it.
    Signed-off-by: Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>

--- src-plugins/TrackMate_/src/test/java/fiji/plugin/trackmate/util/HyperSliceImgPlusTest.java
+++ src-plugins/TrackMate_/src/test/java/fiji/plugin/trackmate/util/HyperSliceImgPlusTest.java
@@ -5,7 +5,7 @@ package fiji.plugin.trackmate.util;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import net.imglib2.Cursor;
 import net.imglib2.Localizable;
 import net.imglib2.RandomAccess;
 import net.imglib2.img.ImgPlus;
@@ -27,23 +27,23 @@ public class HyperSliceImgPlusTest {
 
 	private static final long[] dim = new long[] { 16, 16, 64, 8, 32 };
 	private static final AxisType[] axes = new AxisType[] {
-			Axes.X, Axes.Y, Axes.Z, Axes.CHANNEL, Axes.TIME };
+		Axes.X, Axes.Y, Axes.Z, Axes.CHANNEL, Axes.TIME };
 	private static final float[] calibration = new float[] { 0.5f, 0.5f, 0.8f, 1, 5 };
 	private static final String name = "Source";
 	private static final int REMOVED_DIM_1 = 2; // we will remove Z
 	private static final long DIM_POS_1 = dim[REMOVED_DIM_1] / 2; // fix it there
 	private static final int REMOVED_DIM_2 = 3; // we will remove T in the Z-removed imgplus
 	private static final long DIM_POS_2 = dim[REMOVED_DIM_2+1] / 2; // fix it there
-	
+
 	/** The source {@link ImgPlus}. */
 	private ImgPlus<UnsignedByteType> source;
 	/** We fix one Z. */
 	private HyperSliceImgPlus<UnsignedByteType> imgplusZ;
 	/** We fix one Z AND one T. */
 	private HyperSliceImgPlus<UnsignedByteType> imgplusZT;
-	
-	
-	
+
+
+
 	/**
 	 * Create a 5D image and store it in {@link #source}
 	 */
@@ -58,38 +58,52 @@ public class HyperSliceImgPlusTest {
 			source.setCalibration(calibration[d], d);
 		}
 		source.setName(name);
-		
+
 		// Set up first hyerpslice
 		imgplusZ = new HyperSliceImgPlus<UnsignedByteType>(source, REMOVED_DIM_1, dim[REMOVED_DIM_1]/2);
-		
+
 		// Set up second hyperslice
 		imgplusZT = new HyperSliceImgPlus<UnsignedByteType>(imgplusZ, REMOVED_DIM_2, dim[REMOVED_DIM_2+1]/2); // Time is now dim 3
 	}
 
-	
-	
+
+
 	private void reset() {
 		for (UnsignedByteType type : source) {
 			type.setZero();
 		}
 	}
-	
-	
+
+
 	@Test
 	public void testMetadata() {
-		fail("Not yet implemented.");
+		int index1 = 0;
+		for (int d = 0; d < dim.length; d++) {
+			if (d != REMOVED_DIM_1) {
+				assertEquals(source.calibration(d), imgplusZ.calibration(index1), Float.MIN_VALUE);
+				assertEquals(source.axis(d), imgplusZ.axis(index1));
+				index1++;
+			}
+		}
+
+		int index2 = 0;
+		for (int d = 0; d < dim.length; d++) {
+			if (d != REMOVED_DIM_1 && d != (REMOVED_DIM_2+1)) {
+				assertEquals(source.calibration(d), imgplusZT.calibration(index2), Float.MIN_VALUE);
+				assertEquals(source.axis(d), imgplusZT.axis(index2));
+				index2++;
+			}
+		}
 	}
-	
-	
-	
+
 	/**
 	 * Test method for {@link fiji.plugin.trackmate.util.HyperSliceImgPlus#randomAccess()}.
 	 */
 	@Test
 	public void testRandomAccess() {
-		
+
 		reset();
-		
+
 		// Set one value in source image
 		RandomAccess<UnsignedByteType> ra = source.randomAccess();
 		long[] position = new long[source.numDimensions()];
@@ -101,10 +115,10 @@ public class HyperSliceImgPlusTest {
 		ra.setPosition(position);
 		final int val = 50;
 		ra.get().set(val);
-		
+
 		int index1, index2;
-		
-		
+
+
 		// check value in target images
 		RandomAccess<UnsignedByteType> ra1 = imgplusZ.randomAccess();
 		RandomAccess<UnsignedByteType> ra2 = imgplusZT.randomAccess();
@@ -120,17 +134,17 @@ public class HyperSliceImgPlusTest {
 				index2++;  
 			}
 		}
-		
+
 		assertEquals(
 				String.format("Set the value %d at %s in source, so expected to find it at %s in target; instead got %d.", 
 						val, printCoords(ra), printCoords(ra1), ra1.get().get()),
-				val, ra1.get().get());
-		
+						val, ra1.get().get());
+
 		assertEquals(
 				String.format("Set the value %d at %s in source, so expected to find it at %s in target; instead got %d.", 
 						val, printCoords(ra), printCoords(ra2), ra2.get().get()),
-				val, ra2.get().get());
-		
+						val, ra2.get().get());
+
 	}
 
 	/**
@@ -200,7 +214,7 @@ public class HyperSliceImgPlusTest {
 		long[] expected1 = new long[imgplusZ.nDimensions];
 		long[] max2 = new long[imgplusZT.nDimensions];
 		long[] expected2 = new long[imgplusZT.nDimensions];
-		
+
 		int index1 = 0;
 		int index2 = 0;
 		for (int d = 0; d < dim.length; d++) {
@@ -213,7 +227,7 @@ public class HyperSliceImgPlusTest {
 				index2++;
 			}
 		}
-		
+
 		imgplusZ.max(max1);
 		imgplusZT.max(max2);
 		assertArrayEquals(
@@ -229,63 +243,20 @@ public class HyperSliceImgPlusTest {
 	 */
 	@Test
 	public void testDimensions() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link fiji.plugin.trackmate.util.HyperSliceImgPlus#dimension(int)}.
-	 */
-	@Test
-	public void testDimension() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link fiji.plugin.trackmate.util.HyperSliceImgPlus#realMin(int)}.
-	 */
-	@Test
-	public void testRealMinInt() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link fiji.plugin.trackmate.util.HyperSliceImgPlus#realMin(double[])}.
-	 */
-	@Test
-	public void testRealMinDoubleArray() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link fiji.plugin.trackmate.util.HyperSliceImgPlus#realMin(net.imglib2.RealPositionable)}.
-	 */
-	@Test
-	public void testRealMinRealPositionable() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link fiji.plugin.trackmate.util.HyperSliceImgPlus#realMax(int)}.
-	 */
-	@Test
-	public void testRealMaxInt() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link fiji.plugin.trackmate.util.HyperSliceImgPlus#realMax(double[])}.
-	 */
-	@Test
-	public void testRealMaxDoubleArray() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link fiji.plugin.trackmate.util.HyperSliceImgPlus#realMax(net.imglib2.RealPositionable)}.
-	 */
-	@Test
-	public void testRealMaxRealPositionable() {
-		fail("Not yet implemented");
+		int index1 = 0;
+		int index2 = 0;
+		for (int d = 0; d < imgplusZ.numDimensions(); d++) {
+			if (d != REMOVED_DIM_1) {
+				assertEquals(source.dimension(d), imgplusZ.dimension(index1));
+				index1++;
+			}
+		}
+		for (int d = 0; d < imgplusZT.numDimensions(); d++) {
+			if (d != REMOVED_DIM_1 && d != (REMOVED_DIM_2+1)) {
+				assertEquals(source.dimension(d), imgplusZ.dimension(index2));
+				index2++;
+			}
+		}
 	}
 
 	/**
@@ -293,7 +264,55 @@ public class HyperSliceImgPlusTest {
 	 */
 	@Test
 	public void testCursor() {
-		fail("Not yet implemented");
+		
+		reset();
+		
+		// Write something in the target image
+		Cursor<UnsignedByteType> cursor = imgplusZ.cursor();
+		while (cursor.hasNext()) {
+			cursor.fwd();
+			// Sum X & C in ( X, Y, C, T ) image
+			cursor.get().set( cursor.getIntPosition(0) + cursor.getIntPosition(2) ); 
+		}
+		
+		/* Compare what happened to the source. It should be zero everywhere
+		 * BUT in the slice we modified with the hyper slice accessor. */
+		Cursor<UnsignedByteType> scursor = source.cursor();
+		while (scursor.hasNext()) {
+			scursor.fwd();
+			if (scursor.getIntPosition(REMOVED_DIM_1) == DIM_POS_1) {
+				assertEquals( scursor.getIntPosition(0) + scursor.getIntPosition(3) , scursor.get().get() );
+			} else {
+				assertEquals( 0 , scursor.get().get() );
+			}
+		}
+
+		reset();
+		
+		// Write something in the target image
+		cursor = imgplusZT.cursor();
+		while (cursor.hasNext()) {
+			cursor.fwd();
+			// Still sum X & C in ( X, Y, C ) image		
+			cursor.get().set( cursor.getIntPosition(0) + cursor.getIntPosition(2) ); 
+		}
+		
+		/* Compare what happened to the source. It should be zero everywhere
+		 * BUT in the slice we modified with the hyper slice accessor. */
+		scursor = source.cursor();
+		while (scursor.hasNext()) {
+			scursor.fwd();
+			if (scursor.getIntPosition(REMOVED_DIM_1) == DIM_POS_1 && scursor.getIntPosition(REMOVED_DIM_2+1) == DIM_POS_2) {
+				int expected = scursor.getIntPosition(0) + scursor.getIntPosition(3);
+				int got = scursor.get().get();
+				assertEquals(
+						String.format("At position %s in the source,  expected %d, but got %d.", 
+								printCoords(scursor), expected, got), 
+						 expected, got );
+			} else {
+				assertEquals( 0 , scursor.get().get() );
+			}
+		}
 	}
 
 	/**
@@ -301,50 +320,21 @@ public class HyperSliceImgPlusTest {
 	 */
 	@Test
 	public void testSize() {
-		fail("Not yet implemented");
+		long expected1 = source.size() / source.dimension(REMOVED_DIM_1);
+		assertEquals(expected1, imgplusZ.size());
+
+		long expected2 = source.size() / source.dimension(REMOVED_DIM_1) / source.dimension(REMOVED_DIM_2+1);
+		assertEquals(expected2, imgplusZT.size());
 	}
 
-	/**
-	 * Test method for {@link fiji.plugin.trackmate.util.HyperSliceImgPlus#iterationOrder()}.
-	 */
-	@Test
-	public void testIterationOrder() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link fiji.plugin.trackmate.util.HyperSliceImgPlus#equalIterationOrder(net.imglib2.IterableRealInterval)}.
-	 */
-	@Test
-	public void testEqualIterationOrder() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link fiji.plugin.trackmate.util.HyperSliceImgPlus#iterator()}.
-	 */
-	@Test
-	public void testIterator() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link fiji.plugin.trackmate.util.HyperSliceImgPlus#HyperSliceImgPlus(net.imglib2.img.ImgPlus, int, long)}.
-	 */
-	@Test
-	public void testHyperSliceImgPlus() {
-		fail("Not yet implemented");
-	}
-
-	
 	/*
 	 * UTILS
 	 */
-	
+
 	private static final String printCoords(Localizable loc) {
 		int[] coords = new int[loc.numDimensions()];
 		loc.localize(coords);
 		return Util.printCoordinates(coords);
 	}
-	
+
 }
