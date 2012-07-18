874d385083e6d0ad7ac9f536194f012ea3c696a6
Johannes Schindelin <johannes.schindelin@gmx.de>
2012-07-17 20:30:58 -0500

    Make the debug helper for SPIM Registration more generic
    Now you can debug your plugin from within an IDE such as Eclipse using
    something like:
    
    	Debug.run("My plugin");
    
    or
    
    	Debug.run("My plugin", "parameter=Hello");
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/fiji-lib/src/main/java/fiji/Debug.java
+++ src-plugins/fiji-lib/src/main/java/fiji/Debug.java
@@ -1,5 +1,10 @@
 package fiji;
 
+import ij.IJ;
+import ij.ImageJ;
+
+import java.io.File;
+
 import fiji.debugging.Object_Inspector;
 
 public class Debug {
@@ -9,5 +14,39 @@ public class Debug {
 
 	public static Object get(Object object, String fieldName) {
 		return Object_Inspector.get(object, fieldName);
+	}
+
+	/**
+	 * Debug helper
+	 *
+	 * Call this function from your debugger to debug your plugin
+	 *
+	 * @param plugin the menu item label of the plugin to run
+	 * @param pass the parameters as recorded by the Recorder, or null if dialogs should pop up
+	 */
+	public static void run(String plugin, String parameters) {
+		if (IJ.getInstance() == null) {
+			// make sure that the calling .jar is found by ImageJ 1.x
+			StackTraceElement[] stackTrace = new Throwable().getStackTrace();
+			if (stackTrace.length > 1) {
+				String className = stackTrace[1].getClassName();
+				String relativePath = className.replace('.', '/') + ".class";
+				ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+				if (classLoader == null)
+					classLoader = Debug.class.getClassLoader();
+				String directory = classLoader.getResource(relativePath).getPath();
+				directory = directory.substring(0,  directory.length() - relativePath.length());
+				if (directory.endsWith("/classes/"))
+					directory = directory.substring(0, directory.length() - "/classes/".length());
+				else if (directory.startsWith("file:") && directory.endsWith("!/"))
+					directory = new File(directory.substring(5, directory.length() - 2)).getParent();
+				//System.setProperty("plugins.dir", directory);
+			}
+			new ImageJ();
+		}
+		if (parameters == null)
+			IJ.run(plugin);
+		else
+			IJ.run(plugin, parameters);
 	}
 }
