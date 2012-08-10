7ecf8e7b1ae281d0ea0ce6eefaebefa1761d80c8
Stephan Preibisch <stephan.preibisch@gmx.de>
2012-08-10 14:56:54 -0400

    Added debug output

--- src-plugins/SPIM_Registration/src/main/java/mpicbg/spim/postprocessing/deconvolution2/LRFFT.java
+++ src-plugins/SPIM_Registration/src/main/java/mpicbg/spim/postprocessing/deconvolution2/LRFFT.java
@@ -1,5 +1,7 @@
 package mpicbg.spim.postprocessing.deconvolution2;
 
+import ij.IJ;
+
 import java.util.ArrayList;
 
 import mpicbg.imglib.algorithm.fft.FourierConvolution;
@@ -208,6 +210,8 @@ public class LRFFT
 	{
 		if ( useCUDA )
 		{
+			IJ.log( "Using CUDA ... " );
+			
 			final Image< FloatType > result = image.createNewImage();
 			final Image< FloatType > block = factory.createImage( blockSize );
 			
@@ -228,6 +232,8 @@ public class LRFFT
 		{
 			if ( useBlocks )
 			{
+				IJ.log( "Using blocks ... " );
+				
 				final Image< FloatType > result = image.createNewImage();
 				final Image< FloatType > block = factory.createImage( blockSize );
 				
@@ -247,6 +253,8 @@ public class LRFFT
 			}
 			else
 			{
+				IJ.log( "Using standard way ... " );
+				
 				final FourierConvolution<FloatType, FloatType> fftConv = fftConvolution1;
 				fftConv.replaceImage( image );
 				fftConv.process();			
