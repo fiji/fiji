ae2fc9d85abf0aa62da99a4a514e7a0ee4741790
Johannes Schindelin <johannes.schindelin@gmx.de>
2012-08-28 13:41:17 -0500

    Unify jcommon to version 1.0.17 and jfreechart to 1.0.14
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/fake/src/main/java/fiji/build/minimaven/BuildEnvironment.java
+++ src-plugins/fake/src/main/java/fiji/build/minimaven/BuildEnvironment.java
@@ -203,7 +203,7 @@ public class BuildEnvironment {
 		else if (dependency.artifactId.equals("imglib2-io"))
 			pom.dependencies.add(new Coordinate("loci", "bio-formats", "${bio-formats.version}"));
 		else if (dependency.artifactId.equals("jfreechart"))
-			pom.dependencies.add(new Coordinate("jfree", "jcommon", "1.0.16"));
+			pom.dependencies.add(new Coordinate("jfree", "jcommon", "1.0.17"));
 
 		String key = dependency.getKey();
 		if (localPOMCache.containsKey(key))
