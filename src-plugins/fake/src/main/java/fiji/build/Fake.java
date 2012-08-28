906e1cfec8f85676fbdb24c61231874d7926699e
Johannes Schindelin <johannes.schindelin@gmx.de>
2012-08-28 16:39:18 -0500

    Fiji Build: enhance versionPattern to match bsh-2.0b4.jar
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/fake/src/main/java/fiji/build/Fake.java
+++ src-plugins/fake/src/main/java/fiji/build/Fake.java
@@ -206,7 +206,7 @@ public class Fake {
 	}
 
 	// keep this synchronized with imagej.updater.core.FileObject
-	private static Pattern versionPattern = Pattern.compile("(.+?)(-\\d+(\\.\\d+)+[a-z]?(-[A-Za-z0-9.]+|\\.GA)*)(\\.jar(-[a-z]*)?)");
+	private static Pattern versionPattern = Pattern.compile("(.+?)(-\\d+(\\.\\d+)+[a-z]?\\d?(-[A-Za-z0-9.]+|\\.GA)*)(\\.jar(-[a-z]*)?)");
 
 	public static Matcher matchVersionedFilename(String filename) {
 		return versionPattern.matcher(filename);
