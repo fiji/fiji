cf76b5a8d12b23acd0da90fc9ed606083313ee97
Johannes Schindelin <johannes.schindelin@gmx.de>
2012-10-03 12:21:02 -0500

    Clarify license even further
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src/main/java/Process_Pixels.java
+++ src/main/java/Process_Pixels.java
@@ -1,3 +1,11 @@
+/*
+ * To the extent possible under law, the Fiji developers have waived
+ * all copyright and related or neighboring rights to this tutorial code.
+ *
+ * See the CC0 1.0 Universal license for details:
+ *     http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
 import ij.IJ;
 import ij.ImageJ;
 import ij.ImagePlus;
