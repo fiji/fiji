eb8939cdd7c2eec5d492b98b09d249c1569f2c34
Johannes Schindelin <johannes.schindelin@gmx.de>
2012-10-20 19:30:44 +0200

    Package Maker (cmd-line): optionally include the JRE
    This will be put to good use in the Stable-Fiji Jenkins job at
    
    	http://jenkins.imagej.net/job/Stable-Fiji/
    
    opening the door for continuous releases.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/Fiji_Package_Maker/src/main/java/fiji/packaging/Packager.java
+++ src-plugins/Fiji_Package_Maker/src/main/java/fiji/packaging/Packager.java
@@ -10,6 +10,7 @@ import imagej.updater.util.StderrProgress;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -114,6 +115,83 @@ public abstract class Packager {
 			addFile(fileName, false);
 	}
 
+	public void addJRE(String... platforms) throws IOException {
+		final File javaDir = new File(ijDir, "java");
+		final List<String> directories = new ArrayList<String>();
+		if (platforms.length == 0) {
+			final String[] candidates = javaDir.list(new NoHiddenFiles());
+			if (candidates == null)
+				throw new IOException("No JREs found in " + javaDir);
+			for (final String candidate : candidates) {
+				String dir = getNewestJRE("java/" + candidate);
+				if (dir != null)
+					directories.add(dir);
+			}
+		}
+		else
+			for (int i = 0; i < platforms.length; i++) {
+				final String platform = platforms[i];
+				String dir = getNewestJRE("java/" + platform);
+				if (dir == null) {
+					if ("linux32".equals(platform))
+						dir = "linux";
+					else if ("linux64".equals(platform))
+						dir = "linux-amd64";
+					else if ("macosx".equals(platform) || "tiger".equals(platform))
+						dir = "macosx-java3d";
+
+					dir = getNewestJRE("java/" + dir);
+					if (dir == null) {
+						System.err.println("No JRE found for platform '" + platform);
+						System.exit(1);
+					}
+				}
+				directories.add(dir);
+			}
+
+		for (final String dir : directories)
+			addDirectory(dir);
+	}
+
+	private static class NoHiddenFiles implements FilenameFilter {
+		@Override
+		public boolean accept(final File file, final String name) {
+			return !name.startsWith(".");
+		}
+	}
+
+	private String getNewestJRE(String dirName) {
+		final File[] list = new File(ijDir, dirName).listFiles();
+		if (list == null)
+			return null;
+		File result = null;
+		for (final File file : list) {
+			if (!file.isDirectory())
+				continue;
+			final File jre = new File(file, "jre");
+			if (!jre.isDirectory())
+				continue;
+			if (result == null || result.lastModified() < jre.lastModified())
+				result = file;
+		}
+		if (result == null)
+			return null;
+		return dirName + "/" + result.getName() + "/jre";
+	}
+
+	private boolean addDirectory(String dirName) throws IOException {
+		final File[] list = new File(ijDir, dirName).listFiles(new NoHiddenFiles());
+		if (list == null)
+			return false;
+		boolean result = true;
+		for (final File file : list)
+			if (file.isDirectory())
+				result = addDirectory(dirName + "/" + file.getName()) && result;
+			else if (file.isFile())
+				result = addFile(dirName + "/" + file.getName(), false) && result;
+		return result;
+	}
+
 	protected boolean addFile(String fileName, boolean executable) throws IOException {
 		count++;
 		if (fileName.equals("ImageJ-macosx") || fileName.equals("ImageJ-tiger"))
@@ -141,10 +219,13 @@ public abstract class Packager {
 	}
 
 	public static void main(String[] args) {
+		boolean includeJRE = false;
 		String[] platforms = {};
 		int i = 0;
 		while (i < args.length && args[i].startsWith("-")) {
-			if (args[i].startsWith("--platforms=")) {
+			if (args[i].equals("--jre"))
+				includeJRE = true;
+			else if (args[i].startsWith("--platforms=")) {
 				platforms = args[i].substring("--platforms=".length()).split(",");
 				if (platforms.length == 1 && "".equals(platforms[0]))
 					platforms = new String[0];
@@ -177,7 +258,7 @@ public abstract class Packager {
 			}
 		}
 		else {
-			System.err.println("Usage: Package_Maker [--platform=<platform>[,<platform>]] <filename>");
+			System.err.println("Usage: Package_Maker [--platform=<platform>[,<platform>]] [--jre] <filename>");
 			System.exit(1);
 		}
 		String path = args[i];
@@ -186,6 +267,8 @@ public abstract class Packager {
 			packager.initialize(platforms);
 			packager.open(new FileOutputStream(path));
 			packager.addDefaultFiles();
+			if (includeJRE)
+				packager.addJRE(platforms);
 			packager.close();
 		}
 		catch (Exception e) {
