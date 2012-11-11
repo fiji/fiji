933f62ebec60fc0fb6001c12999fcb820c141dc8
Kota@mac <miura@embl.de>
2012-11-11 20:57:27 +0100

    get ready for a new plugins

---
+++ src/main/java/emblcmci/scalainterp/Scala_Interpreter.java
@@ -0,0 +1,25 @@
+package emblcmci.scalainterp;
+ 
+import common.AbstractInterpreter;
+
+public class Scala_Interpreter extends AbstractInterpreter{
+
+	@Override
+	protected Object eval(String arg0) throws Throwable {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String getImportStatement(String arg0, Iterable<String> arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String getLineCommentMark() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
