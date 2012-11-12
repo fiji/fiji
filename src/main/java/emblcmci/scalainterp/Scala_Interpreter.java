e3623b0915e111a9f7cafa9a851d0d36181dde13
Kota Miura <miura@embl.de>
2012-11-12 13:09:03 +0100

    trying out to upgrade initial imports, not working yet

--- src/main/java/emblcmci/scalainterp/Scala_Interpreter.java
+++ src/main/java/emblcmci/scalainterp/Scala_Interpreter.java
@@ -1,9 +1,21 @@
 package emblcmci.scalainterp;
  
 import java.io.PrintWriter;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 import ij.IJ;
 import common.AbstractInterpreter;
+import common.RefreshScripts;
+import fiji.InspectJar;
+import scala.Option;
+//import scala.collection.Map;
+import scala.collection.immutable.List;
 import scala.tools.nsc.Settings;
 import scala.tools.nsc.interpreter.IMain;
 import scala.tools.nsc.interpreter.IMain.ReadEvalPrint;
@@ -22,6 +34,8 @@ public class Scala_Interpreter extends AbstractInterpreter{
 		Settings settings = new Settings();
 //		the line below is currently set as VM argument
 //		settings.usejavacp(). = true;
+		List<String> param = List.make(1, "true");
+		settings.usejavacp().tryToSet(param);
 		PrintWriter stream = new PrintWriter(this.out);
 		imain = new IMain(settings, stream);
 		rep = imain.new ReadEvalPrint();
@@ -32,13 +46,22 @@ public class Scala_Interpreter extends AbstractInterpreter{
 	}
 	//not done. output should appear in the 
 	// interpreter.
-	String varname, varval;
+	String varname, vartype, varval, aline;
+	Option<Object> varobj;
 	@Override
 	protected Object eval(String arg0) throws Throwable {
 		imain.interpret(arg0);
 		varname = imain.mostRecentVar();
-		varval = imain.valueOfTerm(varname).toString();
-		return varname + ": " + varval;
+		varobj = imain.valueOfTerm(varname);
+		if (varobj.toList().size()>0){
+			varval = imain.valueOfTerm(varname).productElement(0).toString();
+			vartype = imain.valueOfTerm(varname).productElement(0).getClass().getSimpleName();
+			aline = varname + ": " + vartype + " = " + varval;
+		} else{
+			aline = varname + ": None";
+		}
+		return aline;
+
 	}
 
 	@Override
@@ -59,6 +82,82 @@ public class Scala_Interpreter extends AbstractInterpreter{
 			"import " + packageName + "." + buffer + "\n";
 	}
 
+    /** pre-import all ImageJ java classes and TrakEM2 java classes */
+	/*
+	public void importAll(IMain imain) {
+        if (System.getProperty("jnlp") != null) {
+            println("Because Fiji was started via WebStart, no packages were imported implicitly");
+            return;
+        }
+ 
+        Map<String, ArrayList<String>> classNames = getDefaultImports2();
+        try {        
+        	for (String packageName: classNames.keySet())      	
+        		imain.quietImport(packageName+"."+ classNames.get(packageName));
+//            eval(statement);
+        } catch (Throwable e) {
+            RefreshScripts.printError(e);
+            return;
+        }
+        println("All ImageJ and java.lang classes imported.");
+    }
+    protected static Map<String, ArrayList<String>> defaultImports;
+    
+    public static Map<String, ArrayList<String>> getDefaultImports2() {
+        if (defaultImports != null)
+            return defaultImports;
+ 
+        final String[] classNames = {
+            "ij.IJ", "java.lang.String", "script.imglib.math.Compute"
+        };
+        InspectJar inspector = new InspectJar();
+        for (String className : classNames) try {
+            String baseName = className.substring(className.lastIndexOf('.') + 1);
+            URL url = Class.forName(className).getResource(baseName + ".class");
+            inspector.addJar(url);
+        } catch (Exception e) {
+            if (IJ.debugMode)
+                IJ.log("Warning: class " + className
+                        + " was not found!");
+        }
+        defaultImports = new HashMap<String, ArrayList<String>>();
+        Set<String> prefixes = new HashSet<String>();
+        prefixes.add("script.");
+        for (String className : classNames)
+            prefixes.add(className.substring(0, className.lastIndexOf('.')));
+        for (String className : inspector.classNames(true)) {
+            if (!hasPrefix(className, prefixes))
+                continue;
+            int dot = className.lastIndexOf('.');
+            String packageName = dot < 0 ? "" : className.substring(0, dot);
+            String baseName = className.substring(dot + 1);
+            ArrayList<String> list = defaultImports.get(packageName);
+            if (list == null) {
+                list = new ArrayList<String>();
+                defaultImports.put(packageName, list);
+            }
+            list.add(baseName);
+        }
+        // remove non-unique class names
+        Map<String, String> reverse = new HashMap<String, String>();
+        for (String packageName : defaultImports.keySet()) {
+            Iterator<String> iter = defaultImports.get(packageName).iterator();
+            while (iter.hasNext()) {
+                String className = iter.next();
+                if (reverse.containsKey(className)) {
+                    if (IJ.debugMode)
+                        IJ.log("Not auto-importing " + className + " (is in both " + packageName + " and " + reverse.get(className) + ")");
+                    iter.remove();
+                    defaultImports.get(reverse.get(className)).remove(className);
+                }
+                else
+                    reverse.put(className, packageName);
+            }
+        }
+        return defaultImports;
+    }    
+*/
+	
 	@Override
 	protected String getLineCommentMark() {
 		return "//";
