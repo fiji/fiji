10177e9766865212bb55d5e815dc0c1924b73d26
Kota@mac <miura@embl.de>
2012-11-12 02:51:31 +0100

    interpreter is now working

--- src/main/java/emblcmci/scalainterp/Scala_Interpreter.java
+++ src/main/java/emblcmci/scalainterp/Scala_Interpreter.java
@@ -1,25 +1,71 @@
 package emblcmci.scalainterp;
  
+import java.io.PrintWriter;
+
+import ij.IJ;
 import common.AbstractInterpreter;
+import scala.tools.nsc.Settings;
+import scala.tools.nsc.interpreter.IMain;
+import scala.tools.nsc.interpreter.IMain.ReadEvalPrint;
+//import scala.collection.convert.DecorateAsScala;
 
 public class Scala_Interpreter extends AbstractInterpreter{
 
+	IMain imain = null;
+	ReadEvalPrint rep = null;
+	public void run(String args){
+		Thread.currentThread().setContextClassLoader(IJ.getClassLoader());
+		super.run(args);
+		setTitle("Scala Interpreter");
+		println("Starting Scala...");
+		prompt.setEnabled(false);
+		Settings settings = new Settings();
+//		the line below is currently set as VM argument
+//		settings.usejavacp(). = true;
+		PrintWriter stream = new PrintWriter(this.out);
+		imain = new IMain(settings, stream);
+		rep = imain.new ReadEvalPrint();
+		//import all ImageJ classes, modify this later. 
+		//using IMain.quiteImport() should be faster
+		importAll();
+		prompt.setEnabled(true);
+	}
+	//not done. output should appear in the 
+	// interpreter.
+	String varname, varval;
 	@Override
 	protected Object eval(String arg0) throws Throwable {
-		// TODO Auto-generated method stub
-		return null;
+		imain.interpret(arg0);
+		varname = imain.mostRecentVar();
+		varval = imain.valueOfTerm(varname).toString();
+		return varname + ": " + varval;
 	}
 
 	@Override
-	protected String getImportStatement(String arg0, Iterable<String> arg1) {
+	//modified jython interpreter
+	// 
+	protected String getImportStatement(String packageName, Iterable<String> classNames) {
 		// TODO Auto-generated method stub
-		return null;
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("{");
+		for (String className : classNames) {
+			if (buffer.length() > 2)
+				buffer.append(", ");
+			buffer.append(className);
+		}
+		buffer.append("}");
+		return "".equals(packageName) ?
+			"import " + buffer + "\n":
+			"import " + packageName + "." + buffer + "\n";
 	}
 
 	@Override
 	protected String getLineCommentMark() {
-		// TODO Auto-generated method stub
-		return null;
+		return "//";
+	}
+	static public void main(String[] args){
+		Scala_Interpreter si = new Scala_Interpreter();
+		si.run(null);
 	}
 
 }
