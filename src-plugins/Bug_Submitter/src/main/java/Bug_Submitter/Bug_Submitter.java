8d2ab9f84b8a25308b359ce0570ebde51b9e82ca
Curtis Rueden <ctrueden@wisc.edu>
2013-02-02 15:09:26 -0600

    Bug_Submitter: move inner classes to separate files

--- src-plugins/Bug_Submitter/src/main/java/Bug_Submitter/Bug_Submitter.java
+++ src-plugins/Bug_Submitter/src/main/java/Bug_Submitter/Bug_Submitter.java
@@ -24,7 +24,6 @@
 package Bug_Submitter;
 
 import ij.IJ;
-import ij.WindowManager;
 
 import ij.gui.GUI;
 
@@ -52,42 +51,14 @@ import java.util.regex.Matcher;
 
 import ij.Prefs;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JButton;
 import javax.swing.JComponent;
-import javax.swing.JTextField;
-import javax.swing.JPasswordField;
-import javax.swing.JTextArea;
-import javax.swing.text.JTextComponent;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JCheckBox;
 import javax.swing.AbstractAction;
-import javax.swing.ActionMap;
-import javax.swing.InputMap;
 import javax.swing.KeyStroke;
-import javax.swing.undo.UndoManager;
-import javax.swing.event.UndoableEditListener;
-import javax.swing.event.UndoableEditEvent;
-import javax.swing.undo.CannotUndoException;
-import javax.swing.undo.CannotRedoException;
 
 import java.awt.event.ActionListener;
-import java.awt.event.WindowListener;
-import java.awt.event.FocusListener;
 import java.awt.event.ActionEvent;
-import java.awt.event.FocusEvent;
-import java.awt.event.WindowEvent;
-import java.awt.event.KeyEvent;
 
-import java.awt.BorderLayout;
 import java.awt.Component;
-import java.awt.Container;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
-import java.awt.Toolkit;
 
 import fiji.updater.Adapter;
 
@@ -140,25 +111,6 @@ public class Bug_Submitter implements PlugIn {
 	static final int OTHER_FAILURE = 3;
 	static final int CC_UNKNOWN_FAILURE = 4; // Not used any more...
 	static final int IOEXCEPTION_FAILURE = 5;
-
-	static class SubmissionResult {
-		public SubmissionResult( int resultCode,
-					 int bugNumber,
-					 String bugURL,
-					 String authenticationResultPage,
-					 String submissionResultPage ) {
-			this.resultCode = resultCode;
-			this.bugNumber = bugNumber;
-			this.bugURL = bugURL;
-			this.authenticationResultPage = authenticationResultPage;
-			this.submissionResultPage = submissionResultPage;
-		}
-		int resultCode;
-		int bugNumber;
-		String bugURL;
-		String authenticationResultPage;
-		String submissionResultPage;
-	}
 
 	public String getUsefulSystemInformation() {
 		String [] interestingProperties = {
@@ -406,338 +358,6 @@ public class Bug_Submitter implements PlugIn {
 		}
 	}
 
-	@SuppressWarnings("serial")
-	class NewBugDialog extends JFrame implements ActionListener, WindowListener {
-
-		JButton bugzillaAccountCreation;
-		JButton submitReport;
-		JButton cancel;
-
-		JTextField username;
-		JPasswordField password;
-		JCheckBox rememberPassword;
-
-		JTextField summary;
-		JTextArea description;
-		UndoManager undo;
-
-		private class HighlightingFocusListener implements FocusListener {
-			String stringToHighlight;
-			boolean notYetFocussed = true;
-			JTextComponent textComponent;
-			public HighlightingFocusListener( String stringToHighlight, JTextComponent textComponent ) {
-				this.stringToHighlight = stringToHighlight;
-				this.textComponent = textComponent;
-			}
-			public void focusGained(FocusEvent e) {
-				if( notYetFocussed ) {
-					String text = textComponent.getText();
-					int startIndex = text.indexOf( stringToHighlight );
-					if( startIndex >= 0 ) {
-						textComponent.setSelectionStart(startIndex);
-						textComponent.setSelectionEnd(startIndex+stringToHighlight.length());
-					}
-					notYetFocussed = false;
-				}
-			}
-			public void focusLost(FocusEvent e) { }
-		}
-
-		private class JTextAreaTabFocus extends JTextArea {
-			public JTextAreaTabFocus( int rows, int columns ) {
-				super( rows, columns );
-			}
-			@Override
-			protected void processComponentKeyEvent( KeyEvent e ) {
-				if( e.getID() == KeyEvent.KEY_PRESSED &&
-				    e.getKeyCode() == KeyEvent.VK_TAB ) {
-					e.consume();
-					if (e.isShiftDown())
-						transferFocusBackward();
-					else
-						transferFocus();
-				} else {
-					super.processComponentKeyEvent( e );
-				}
-			}
-		}
-
-		// This example is derived from:
-		//   http://java.sun.com/docs/books/tutorial/uiswing/components/generaltext.html#undo
-		protected class SimpleEditListener implements UndoableEditListener {
-			public void undoableEditHappened(UndoableEditEvent e) {
-				undo.addEdit(e.getEdit());
-			}
-		}
-
-		// More "Programming with Google" from:
-		//   http://www.exampledepot.com/egs/javax.swing.undo/UndoText.html
-
-		protected class UndoAction extends AbstractAction {
-			public UndoAction() {
-				super("Undo");
-			}
-			public void actionPerformed(ActionEvent evt) {
-				try {
-					if( undo.canUndo() )
-						undo.undo();
-				} catch (CannotUndoException e) { }
-			}
-		}
-
-		protected class RedoAction extends AbstractAction {
-			public RedoAction() {
-				super("Redo");
-			}
-			public void actionPerformed(ActionEvent evt) {
-				try {
-					if( undo.canRedo() )
-						undo.redo();
-				} catch (CannotRedoException e) { }
-			}
-		}
-
-		boolean askedToSubmit = false;
-		boolean alreadyDisposed = false;
-
-		@Override
-		public void setVisible(boolean visible) {
-			if (visible)
-				WindowManager.addWindow(this);
-			else
-				WindowManager.removeWindow(this);
-			super.setVisible(visible);
-		}
-
-		@Override
-		public synchronized void show() {
-			WindowManager.addWindow(this);
-			super.show();
-			try {
-				wait();
-			} catch (InterruptedException e) { }
-		}
-
-		@Override
-		public synchronized void dispose() {
-			WindowManager.removeWindow(this);
-			notify();
-			super.dispose();
-	        }
-
-
-		public NewBugDialog( String suggestedUsername,
-				     String suggestedPassword,
-				     String suggestedSummary,
-				     String suggestedDescription ) {
-
-			super( "Bug Report Form" );
-
-			Container contentPane = getContentPane();
-
-			addWindowListener( this );
-
-			setLayout(new GridBagLayout());
-
-			GridBagConstraints c = new GridBagConstraints();
-
-			c.gridx = 0;
-			c.gridy = 0;
-			c.weightx = 1;
-			c.weighty = 0;
-			c.insets = new Insets( 5, 3, 5, 3 );
-			c.fill = GridBagConstraints.HORIZONTAL;
-			c.anchor = GridBagConstraints.CENTER;
-
-			{
-				JPanel labelsPanel = new JPanel();
-				labelsPanel.setLayout( new GridBagLayout() );
-
-				JTextArea instructions = new JTextArea(3,30);
-				instructions.setEditable(false);
-				instructions.setCursor(null);
-				instructions.setOpaque(false);
-				instructions.setFocusable(false);
-				instructions.setLineWrap(true);
-				instructions.setWrapStyleWord(true);
-
-				instructions.setText(
-					"In order to report a bug, we ask that you create a Bugzilla account. "+
-					" This is so that you can follow the progress of fixing the problem by"+
-					" email and enables us to ask follow-up questions if that's necessary.");
-
-				bugzillaAccountCreation = new JButton( "Visit the Bugzilla account creation page" );
-				bugzillaAccountCreation.addActionListener(this);
-
-				GridBagConstraints clabels = new GridBagConstraints();
-				clabels.fill = GridBagConstraints.BOTH;
-				clabels.gridx = 0;
-				clabels.gridy = 0;
-				labelsPanel.add( instructions, clabels );
-				clabels.gridx = 1;
-				clabels.gridy = 0;
-				labelsPanel.add( bugzillaAccountCreation, clabels );
-
-				contentPane.add( labelsPanel, c );
-			}
-
-			username = new JTextField(20);
-			username.setText( suggestedUsername );
-			password = new JPasswordField(20);
-			password.setEchoChar('*');
-			if( suggestedPassword != null )
-				password.setText( suggestedPassword );
-			rememberPassword = new JCheckBox("",suggestedPassword != null);
-
-			{
-				JPanel p = new JPanel();
-				p.setLayout( new GridBagLayout() );
-
-				GridBagConstraints cl = new GridBagConstraints();
-
-				cl.gridx = 0; cl.gridy = 0; cl.anchor = GridBagConstraints.LINE_END;
-				p.add( new JLabel("Bugzilla username (your email address): "), cl );
-				cl.gridx = 1; cl.gridy = 0; cl.anchor = GridBagConstraints.LINE_START;
-				p.add( username, cl );
-
-				cl.gridx = 0; cl.gridy = 1; cl.anchor = GridBagConstraints.LINE_END;
-				p.add( new JLabel("Bugzilla password: "), cl );
-				cl.gridx = 1; cl.gridy = 1; cl.anchor = GridBagConstraints.LINE_START;
-				p.add( password, cl );
-
-				cl.gridx = 0; cl.gridy = 2; cl.anchor = GridBagConstraints.LINE_END;
-				p.add( new JLabel("Remember password (insecure): "), cl );
-				cl.gridx = 1; cl.gridy = 2; cl.anchor = GridBagConstraints.LINE_START;
-				p.add( rememberPassword, cl );
-
-				c.anchor = GridBagConstraints.LINE_START;
-				++ c.gridy;
-				contentPane.add( p, c );
-			}
-
-			summary = new JTextField(30);
-			summary.setText( suggestedSummary );
-			summary.addFocusListener( new HighlightingFocusListener(
-							      dummyBugTextSummary,
-							      summary) );
-			description = new JTextAreaTabFocus(16,42);
-			description.setLineWrap(true);
-			description.setWrapStyleWord(true);
-			description.setText( suggestedDescription );
-			undo = new UndoManager();
-			description.getDocument().addUndoableEditListener(
-				new SimpleEditListener() );
-			ActionMap actionMap = description.getActionMap();
-			actionMap.put("Undo",new UndoAction());
-			actionMap.put("Redo",new RedoAction());
-			InputMap inputMap = description.getInputMap();
-			inputMap.put(KeyStroke.getKeyStroke("control Z"), "Undo");
-			inputMap.put(KeyStroke.getKeyStroke("control Y"), "Redo");
-
-			description.addFocusListener( new HighlightingFocusListener(
-							      dummyBugTextDescription,
-							      description) );
-
-			{
-				JPanel summaryPanel = new JPanel();
-				summaryPanel.setLayout( new BorderLayout() );
-				summaryPanel.add( new JLabel("Summary of the bug: "), BorderLayout.WEST );
-				summaryPanel.add( summary, BorderLayout.CENTER );
-				++ c.gridy;
-				c.anchor = GridBagConstraints.LINE_START;
-				contentPane.add( summaryPanel, c );
-			}
-
-			++ c.gridy;
-			contentPane.add( new JLabel("A full description of the bug:"), c );
-
-			JScrollPane scrollPane = new JScrollPane(description);
-
-			++ c.gridy;
-			c.fill = GridBagConstraints.BOTH;
-			c.weighty = 1;
-			contentPane.add( scrollPane, c );
-			c.fill = GridBagConstraints.HORIZONTAL;
-			c.weighty = 0;
-
-			{
-				JPanel p = new JPanel();
-
-				submitReport = new JButton( "Submit Bug Report" );
-				submitReport.addActionListener( this );
-				p.add( submitReport );
-
-				cancel = new JButton( "Cancel" );
-				cancel.addActionListener( this );
-				p.add( cancel );
-
-				c.anchor = GridBagConstraints.CENTER;
-				++ c.gridy;
-				contentPane.add( p, c );
-			}
-
-			// Call pack twice to workaround:
-			//   http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4446522
-			pack();
-
-			addAccelerator(cancel, (JComponent)contentPane, this,
-				KeyEvent.VK_W, Toolkit.getDefaultToolkit()
-					.getMenuShortcutKeyMask());
-		}
-
-		public void resetForm() {
-			askedToSubmit = false;
-		}
-
-		public void actionPerformed( ActionEvent e ) {
-			Object source = e.getSource();
-			if( source == bugzillaAccountCreation ) {
-				new BrowserLauncher().run(bugzillaBaseURI+"createaccount.cgi");
-			} else if( source == cancel ) {
-				alreadyDisposed = true;
-				dispose();
-			} else if( source == submitReport ) {
-				boolean success = validateForm();
-				if (!success) return;
-				askedToSubmit = true;
-				alreadyDisposed = true;
-				dispose();
-			}
-		}
-
-		public void windowClosing( WindowEvent e ) {
-			dispose();
-		}
-
-		public void windowActivated( WindowEvent e ) { }
-		public void windowDeactivated( WindowEvent e ) { }
-		public void windowClosed( WindowEvent e ) { }
-		public void windowOpened( WindowEvent e ) { }
-		public void windowIconified( WindowEvent e ) { }
-		public void windowDeiconified( WindowEvent e ) { }
-
-		private boolean validateForm() {
-			if (username.getText().trim().length() == 0) {
-				IJ.error("You must supply a username");
-				return false;
-			}
-			if (password.getPassword().length == 0) {
-				IJ.error("You must supply a password");
-				return false;
-			}
-			if (summary.getText().trim().length() == 0) {
-				IJ.error("You must supply a summary of the bug");
-				return false;
-			}
-			if (description.getText().trim().length() == 0) {
-				IJ.error("You must supply a description of the bug");
-				return false;
-			}
-			return true;
-		}
-
-	}
-
 	String usernamePreferenceKey = "Bug_Submitter.username";
 	String passwordPreferenceKey = "Bug_Submitter.password";
 
@@ -768,7 +388,7 @@ public class Bug_Submitter implements PlugIn {
 			else
 				suggestedPassword = rot13( suggestedPassword );
 
-			NewBugDialog dialog = new NewBugDialog( suggestedUsername, suggestedPassword, summary, description );
+			NewBugDialog dialog = new NewBugDialog( this, suggestedUsername, suggestedPassword, summary, description );
 			GUI.center(dialog);
 			dialog.show();
 
