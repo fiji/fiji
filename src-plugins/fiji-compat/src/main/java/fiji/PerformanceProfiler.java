d4cd7d2920c5b1a65bb580ddba9494854c1283a1
Johannes Schindelin <johannes.schindelin@gmx.de>
2013-02-26 17:25:29 -0600

    Add a primitive Javassist-based performance profiler
    This profiler is loosely modeled on the ideas of OProfile, but uses
    Javassist to instrument the loaded classes.
    
    The gist: every instrumented method is added to a list, and gets two
    static fields in the same class: the call count and the total amount
    of time spent in the method (or in methods called from there).
    
    Unfortunately, it seems that the precision with which Java can measure
    CPU time per thread is a bit crude: in my tests, the granularity is
    five milliseconds.
    
    To use it, call fiji.PerformanceProfiler as main class with the "real
    main class" and its arguments as parameters.
    
    But it is a start at something we can use, re-use and enhance.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ src-plugins/fiji-compat/src/main/java/fiji/PerformanceProfiler.java
@@ -0,0 +1,246 @@
+package fiji;
+
+import java.io.PrintStream;
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtBehavior;
+import javassist.CtClass;
+import javassist.CtConstructor;
+import javassist.CtField;
+import javassist.CtMethod;
+import javassist.Loader;
+import javassist.Modifier;
+import javassist.NotFoundException;
+import javassist.Translator;
+
+public class PerformanceProfiler implements Translator {
+	private Set<String> only;
+
+	protected static final boolean debug = false;
+	private static Loader loader;
+	private static Field activeField;
+	private static Map<CtBehavior, Integer> counters = new TreeMap<CtBehavior, Integer>(new BehaviorComparator());
+	private static ThreadMXBean bean;
+
+	private static class BehaviorComparator implements Comparator<CtBehavior> {
+
+		@Override
+		public int compare(CtBehavior a, CtBehavior b) {
+			return a.getLongName().compareTo(b.getLongName());
+		}
+
+	}
+
+	public static void init() {
+		try {
+			ClassPool pool = ClassPool.getDefault();
+			loader = new Loader();
+			CtClass that = pool.get(PerformanceProfiler.class.getName());
+			CtField active = new CtField(CtClass.booleanType, "active", that);
+			active.setModifiers(Modifier.PUBLIC | Modifier.STATIC);
+			that.addField(active);
+			Class<?> thatClass = that.toClass(loader, null);
+			activeField = thatClass.getField("active");
+			Field beanField = thatClass.getDeclaredField("bean");
+			beanField.setAccessible(true);
+			beanField.set(null, ManagementFactory.getThreadMXBean());
+			loader.addTranslator(pool, new PerformanceProfiler());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static long getNanos() {
+		return bean.getCurrentThreadCpuTime();
+	}
+
+	public PerformanceProfiler() {
+		this(System.getenv("PERFORMANCE_PROFILE_ONLY"));
+	}
+
+	public PerformanceProfiler(String only) {
+		this(only == null ? null : Arrays.asList(only.split(" +")));
+	}
+
+	public PerformanceProfiler(Collection<String> only) {
+		if (only != null) {
+			this.only = new HashSet<String>();
+			this.only.addAll(only);
+		}
+	}
+
+	@Override
+	public synchronized void start(ClassPool pool) throws NotFoundException, CannotCompileException {
+		// ignore
+	}
+
+	@Override
+	public void onLoad(ClassPool pool, String classname) throws NotFoundException {
+		// do not instrument yourself
+		if (classname.equals(getClass().getName())) {
+			return;
+		}
+
+		// do not instrument anything javassist
+		if (classname.startsWith("javassist.")) {
+			return;
+		}
+
+		if (only != null && !only.contains(classname))
+			return;
+
+		if (debug)
+			System.err.println("instrumenting " + classname);
+
+		CtClass cc = pool.get(classname);
+		if (cc.isFrozen())
+			return;
+
+		// instrument all methods and constructors
+		if (debug)
+			System.err.println("Handling class " + cc.getName());
+		for (CtMethod method : cc.getDeclaredMethods())
+			handle(cc, method);
+		for (CtConstructor constructor : cc.getDeclaredConstructors())
+			handle(cc, constructor);
+	}
+
+	private static String toCounterName(int i) {
+		return "__counter" + i + "__";
+	}
+
+	private static String toNanosName(int i) {
+		return "__nanos" + i + "__";
+	}
+
+	private void handle(CtClass clazz, CtBehavior behavior) {
+		try {
+			if (clazz != behavior.getDeclaringClass()) {
+				if (debug)
+					System.err.println("Skipping superclass' method: " + behavior.getName()
+							+ " (" + behavior.getDeclaringClass().getName() + " is superclass of " + clazz);
+				return;
+			}
+			if (debug)
+				System.err.println("instrumenting " + behavior.getClass().getName() + "." + behavior.getName());
+			if (behavior.isEmpty())
+				return;
+
+			int i;
+			for (i = 1; ; i++) {
+				if (!hasField(clazz, toCounterName(i)) && !hasField(clazz, toNanosName(i))) {
+					break;
+				}
+			}
+			final String counterFieldName = toCounterName(i);
+			final String nanosFieldName = toNanosName(i);
+
+			CtField counterField = new CtField(CtClass.longType, counterFieldName, clazz);
+			counterField.setModifiers(Modifier.STATIC);
+			clazz.addField(counterField);
+			CtField nanosField = new CtField(CtClass.longType, nanosFieldName, clazz);
+			nanosField.setModifiers(Modifier.STATIC);
+			clazz.addField(nanosField);
+
+			final String thisName = getClass().getName();
+			final String that = clazz.getName() + ".";
+			behavior.addLocalVariable("__startTime__", CtClass.longType);
+			behavior.insertBefore("__startTime__ = " + thisName + ".getNanos();");
+			behavior.insertAfter("if (" + thisName + ".active) {"
+					+ that + counterFieldName + "++;"
+					+ that + nanosFieldName + " += " + thisName + ".getNanos() - __startTime__;"
+					+ "}");
+			counters.put(behavior, i);
+		}
+		catch (CannotCompileException e) {
+			if (!e.getMessage().equals("no method body"))
+				e.printStackTrace();
+		}
+	}
+
+	private static boolean hasField(final CtClass clazz, final String name) {
+		try {
+			return clazz.getField(name) != null;
+		} catch (NotFoundException e) {
+			return false;
+		}
+	}
+
+	public static void setActive(boolean active) {
+		if (loader == null) init();
+		try {
+			activeField.setBoolean(null, active);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static boolean isActive() {
+		try {
+			return activeField.getBoolean(null);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public static void report(PrintStream writer) {
+		synchronized(PerformanceProfiler.class) {
+			if (!isActive()) {
+				return;
+			}
+			setActive(false);
+			for (CtBehavior behavior : counters.keySet()) try {
+				int i = counters.get(behavior);
+				Class<?> clazz = loader.loadClass(behavior.getDeclaringClass().getName());
+				Field counter = clazz.getDeclaredField(toCounterName(i));
+				counter.setAccessible(true);
+				long count = counter.getLong(null);
+				if (count == 0) continue;
+				Field nanosField = clazz.getDeclaredField(toNanosName(i));
+				nanosField.setAccessible(true);
+				long nanos = nanosField.getLong(null);
+				writer.println(behavior.getLongName() + "; " + count + "x; " + formatNanos(nanos / count));
+				counter.set(null, 0l);
+				nanosField.set(null, 0l);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private static String formatNanos(long nanos) {
+		if (nanos < 1000) return "" + nanos + "ns";
+		if (nanos < 1000000) return (nanos / 1000.0) + "µs";
+		if (nanos < 1000000000) return (nanos / 1000000.0) + "ms";
+		return (nanos / 1000000000.0) + "s";
+	}
+
+	public static void main(String[] args) throws Throwable {
+		Thread.currentThread().setContextClassLoader(PerformanceProfiler.class.getClassLoader());
+
+		if (args.length == 0) {
+			System.err.println("Usage: java " + PerformanceProfiler.class + " <main-class> [<argument>...]");
+			System.exit(1);
+		}
+
+		String mainClass = args[0];
+		String[] mainArgs = new String[args.length - 1];
+		System.arraycopy(args, 1, mainArgs, 0, mainArgs.length);
+
+		setActive(true);
+		loader.run(mainClass, mainArgs);
+		report(System.err);
+	}
+}
