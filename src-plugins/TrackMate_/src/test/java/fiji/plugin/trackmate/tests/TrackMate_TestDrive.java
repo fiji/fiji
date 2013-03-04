ab716727edc31b478e7ca505d9dfb41175b0a369
Johannes Schindelin <johannes.schindelin@gmx.de>
2013-03-04 15:28:29 -0600

    TrackMate: Use the sample downloader instead of hard-coded paths
    We now serve the TrackMate example from http://fiji.sc/samples/,
    so we can reuse Fiji's sample-downloader to avoid carrying the file
    in the repository.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/TrackMate_/src/test/java/fiji/plugin/trackmate/tests/TrackMate_TestDrive.java
+++ src-plugins/TrackMate_/src/test/java/fiji/plugin/trackmate/tests/TrackMate_TestDrive.java
@@ -1,23 +1,33 @@
 package fiji.plugin.trackmate.tests;
 
+import fiji.SampleImageLoader;
 import fiji.plugin.trackmate.TrackMate_;
 import ij.IJ;
+import ij.ImageJ;
+import ij.ImagePlus;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.scijava.util.AppUtils;
 
 public class TrackMate_TestDrive {
 	
 	public static void main(String[] args) {
 		
-		File file;
-		if (IJ.isWindows()) {
-			file = new File("E:/Users/JeanYves/Desktop/Data/FakeTracks.tif");
-		} else {
-			file = new File("/Users/tinevez/Desktop/Data/FakeTracks.tif");
-		}
+		final File file = new File(AppUtils.getBaseDirectory(TrackMate_.class), "samples/FakeTracks.tif");
 		
-		ij.ImageJ.main(args);
-		ij.ImagePlus imp = IJ.openImage(file.getAbsolutePath());
+		new ImageJ();
+		if (!file.exists()) try {
+			final File parent = file.getParentFile();
+			if (!parent.isDirectory()) parent.mkdirs();
+			SampleImageLoader.download(new URL("http://fiji.sc/samples/FakeTracks.tif").openConnection(), file, 0, 1, true);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+		ImagePlus imp = IJ.openImage(file.getAbsolutePath());
 		imp.show();
 		
 		final TrackMate_ st = new TrackMate_();
