6460add56d1f21fa9535dc729fc29adb79398c5b
Johannes Schindelin <johannes.schindelin@gmx.de>
2013-06-25 16:34:10 -0500

    Fix compiler warnings in fiji.Main
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/fiji-compat/src/main/java/fiji/Main.java
+++ src-plugins/fiji-compat/src/main/java/fiji/Main.java
@@ -2,38 +2,13 @@ package fiji;
 
 import fiji.gui.FileDialogDecorator;
 import fiji.gui.JFileChooserDecorator;
-
 import ij.IJ;
 import ij.ImageJ;
-
 import ij.plugin.PlugIn;
 
-import java.awt.AWTEvent;
-import java.awt.Component;
-import java.awt.Container;
-import java.awt.Dialog;
-import java.awt.Frame;
 import java.awt.Image;
-import java.awt.Label;
 import java.awt.Toolkit;
-import java.awt.Window;
-
-import java.awt.image.ImageProducer;
-
-import java.awt.event.AWTEventListener;
-import java.awt.event.WindowEvent;
-
-import java.io.File;
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-
-import java.net.URL;
-
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.WeakHashMap;
 
 public class Main {
 	protected Image icon;
@@ -50,7 +25,7 @@ public class Main {
 
 	public static void gentlyRunPlugIn(String className, String arg) {
 		try {
-			Class clazz = IJ.getClassLoader()
+			Class<?> clazz = IJ.getClassLoader()
 				.loadClass(className);
 			if (clazz != null) {
 				PlugIn plugin = (PlugIn)clazz.newInstance();
