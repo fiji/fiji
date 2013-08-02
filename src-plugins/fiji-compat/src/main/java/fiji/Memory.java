02ae2890d698073eec22ffa892fa423cc3f1ea6a
Johannes Schindelin <johannes.schindelin@gmx.de>
2013-08-02 10:51:59 -0500

    Undo changes in fiji-compat
    This reverts it to the version in 'master'.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/fiji-compat/src/main/java/fiji/Memory.java
+++ src-plugins/fiji-compat/src/main/java/fiji/Memory.java
@@ -1,28 +1,52 @@
 package fiji;
 
 import ij.IJ;
-import ij.Menus;
 import ij.Prefs;
-
 import ij.gui.GenericDialog;
-
 import ij.plugin.PlugIn;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/** This plugin implements the Edit/Options/Memory command. */
+/** This plugin implements the Edit/Options/Memory & Threads... command. */
 public class Memory implements PlugIn {
 	public void run(String arg) {
+		if (IJ.isMacOSX()) {
+			// Unfortunately, ImageJ 1.x' Memory & Threads makes way too many assumptions to be
+			// reused on MacOSX. A pity.
+			runMacOSX();
+			return;
+		}
+
+		final File configFile = new File(FijiTools.getFijiDir(), "ImageJ.cfg");
+		if (!configFile.exists()) try {
+			final PrintStream out = new PrintStream(new FileOutputStream(configFile));
+			out.println(".");
+			out.println(IJ.isWindows() ? "jre\\bin\\javaw.exe" : "jre/bin/java");
+			out.println("-Xmx" +(maxMemory() >> 20) + "m -cp ij.jar ij.ImageJ");
+			out.close();
+		} catch (final IOException e) {
+			IJ.error("Could not write initial ImageJ.cfg!");
+			return;
+		}
+		new ij.plugin.Memory().run(arg);
+	}
+
+	private static long maxMemory() {
+			return Runtime.getRuntime().maxMemory();
+	}
+
+	private void runMacOSX() {
 
 		long memory = maxMemory() >> 20;
 		int threads = Prefs.getThreads();
 
-		GenericDialog gd = new GenericDialog("Memory "
+		final GenericDialog gd = new GenericDialog("Memory "
 			+ (IJ.is64Bit() ? "(64-bit)" : "(32-bit)"));
 		gd.addNumericField("Maximum Memory:", memory, 0, 5, "MB");
 		gd.addNumericField("Parallel Threads for Stacks:",
@@ -31,16 +55,17 @@ public class Memory implements PlugIn {
 		if (gd.wasCanceled())
 			return;
 
-		memory = (long)gd.getNextNumber();
-		threads = (int)gd.getNextNumber();
-		Prefs.setThreads(threads);
-
 		if (gd.invalidNumber()) {
 			IJ.showMessage("Memory",
 					"The number entered was invalid.");
 			return;
 		}
-		int limit = IJ.isWindows() ? 1600 : 1700;
+
+		memory = (long)gd.getNextNumber();
+		threads = (int)gd.getNextNumber();
+		Prefs.setThreads(threads);
+
+		final int limit = 1700;
 		if (!IJ.is64Bit() && memory > limit) {
 			if (!IJ.showMessageWithCancel("Memory",
 					"Note: setting the memory limit to a "
@@ -50,81 +75,41 @@ public class Memory implements PlugIn {
 				return;
 		}
 
-		writeFile(memory);
+		writeFileMacOSX(memory);
 	}
 
-	void writeFile(long memory) {
-		String fileName;
-		String pattern;
-
-		String dir = Menus.getPlugInsPath();
-		if (dir == null || dir.equals("")) {
-			IJ.error("Could not find Fiji directory");
-			return;
-		}
-		if (dir.endsWith(File.separator + "plugins" + File.separator) ||
-				dir.endsWith(File.separator + "plugins"))
-			dir = new File(dir).getParent();
-
-		if (IJ.isMacOSX())
-			fileName = dir + "/Contents/Info.plist";
-		else
-			fileName = dir + "/jvm.cfg";
+	private void writeFileMacOSX(final long memory) {
+		final File infoPList = new File(FijiTools.getFijiDir(), "Contents/Info.plist");
 
 		try {
-			String contents = readFile(fileName);
+			String contents = readFile(infoPList);
 			String mem = "" + memory + "m";
 
-			FileOutputStream out = new FileOutputStream(fileName);
-			out.write(edit(contents, mem).getBytes("UTF-8"));
+			FileOutputStream out = new FileOutputStream(infoPList);
+			out.write(editInfoPList(contents, mem).getBytes("UTF-8"));
 			out.close();
 			IJ.showMessage("Memory", "The new " + memory
 				+ " limit will take effect after Fiji"
 				+ " is restarted.");
 		} catch (Exception e) {
-			IJ.error("Could not write " + fileName);
+			IJ.error("Could not write " + infoPList);
 		}
 	}
 
-	public static String readFile(String fileName) {
-		try {
-			File file = new File(fileName);
-			if (file.exists()) {
-				int size = (int)file.length();
-				byte[] buffer = new byte[size];
-				FileInputStream in = new FileInputStream(file);
-				in.read(buffer, 0, size);
-				in.close();
-				return new String(buffer, 0, size, "UTF-8");
-			}
-		} catch(Exception e) { }
-		return "";
-	}
+	private static String readFile(final File file) throws IOException {
+		if (!file.exists()) return "";
 
-	public static String edit(String contents, String memory) {
-		return IJ.isMacOSX() ?
-			editInfoPlist(contents, memory) :
-			editJVMOptions(contents, memory);
+		byte[] buffer = new byte[(int)file.length()];
+		FileInputStream in = new FileInputStream(file);
+		in.read(buffer, 0, buffer.length);
+		in.close();
+		return new String(buffer, 0, buffer.length, "UTF-8");
 	}
 
 	private static int flags = Pattern.MULTILINE | Pattern.DOTALL;
 
-	public static String editJVMOptions(String contents, String memory) {
-		Pattern p = Pattern.compile(".*-Xmx([0-9][0-9]*[kmg]?).*",
-				flags);
-		Matcher matcher = p.matcher(contents);
-		if (matcher.matches()) {
-			int start = matcher.start(1);
-			int start2 = matcher.end(1);
-			return contents.substring(0, start)
-				+ memory + contents.substring(start2);
-		}
-		if (!contents.equals(""))
-			return contents + " -Xmx" + memory;
-		return "-Xmx" + memory;
-	}
-
-	public static String editInfoPlist(String contents, String memory) {
+	// TODO: handle ImageJ64.app/Contents/Info.plist, too
+	private static String editInfoPList(String contents, String memory) {
 		String key = "\n\t\t<key>memory</key>"
 			+ "\n\t\t<string>" + memory + "</string>" ;
 
@@ -165,26 +150,4 @@ public class Memory implements PlugIn {
 			+ "</plist>";
 	}
 
-	public long maxMemory() {
-			return Runtime.getRuntime().maxMemory();
-	}
-
-	public long getMemorySetting() {
-		if (IJ.getApplet() != null)
-			return 0L;
-		return maxMemory();
-	}
-
-	public static void main(String[] args) {
-		String mem = "MEMORY";
-
-		for (int i = 0; i < args.length; i++) {
-			String contents = readFile(args[i]);
-			System.err.println("File: " + args[i]
-				+ "\n=========\n"
-				+ (args[i].endsWith(".plist") ?
-					editInfoPlist(contents, mem) :
-					editJVMOptions(contents, mem)));
-		}
-	}
 }
