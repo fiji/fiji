25c74ea81d2ac01b7318d40b1164120bcdadcdd5
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2013-08-02 10:52:58 +0200

    TrackMate_: update info text of the HyperStack viewer
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src-plugins/TrackMate_/src/main/java/fiji/plugin/trackmate/visualization/hyperstack/HyperStackDisplayer.java
+++ src-plugins/TrackMate_/src/main/java/fiji/plugin/trackmate/visualization/hyperstack/HyperStackDisplayer.java
@@ -36,14 +36,19 @@ public class HyperStackDisplayer extends AbstractTrackMateModelView  {
 			"<tt>alt</tt> key down and rotate the mouse-wheel. Holding the <br>" +
 			"<tt>shift</tt> key on top changes it faster. " +
 			"<p>" +
-			"Alternatively, keyboard can be used to edit spots: " +
-			"<ul>" +
-			"	<li><b>A</b> creates a new spot under the mouse" +
-			"	<li><b>D</b> deletes the spot under the mouse" +
-			"	<li><b>Q</b> and <b>E</b> decreases and increases the radius of the spot " +
-			"under the mouse (shift to go faster)" +
-			"	<li><b>Space</b> + mouse drag moves the spot under the mouse" +
-			"</ul>" +
+			"Alternatively, keyboard can be used to edit spots:<br/>" +
+			" - <b>A</b> creates a new spot under the mouse.<br/>" +
+			" - <b>D</b> deletes the spot under the mouse.<br/>" +
+			" - <b>Q</b> and <b>E</b> decreases and increases the radius of the spot " +
+			"under the mouse (shift to go faster).<br/>" +
+			" - <b>Space</b> + mouse drag moves the spot under the mouse.<br/>" +
+			"<p>" +
+			"To toggle links between two spots, select two spots (Shift+Click), <br>" +
+			"then press <b>L</b>. "
+			+ "<p>"
+			+ "<b>Shift+L</b> toggle the auto-linking mode on/off. <br>"
+			+ "If on, every spot created will be automatically linked with the spot <br>"
+			+ "currently selected, if they are in subsequent frames." +
 			"</html>";
 	protected final ImagePlus imp;
 	protected SpotOverlay spotOverlay;
