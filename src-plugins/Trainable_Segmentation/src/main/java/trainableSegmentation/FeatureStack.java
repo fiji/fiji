0a5d7a6cd1312dda5a5f0b2509fa530765320b44
Ignacio Arganda-Carreras <ignacio.arganda@gmail.com>
2013-09-19 14:44:33 +0200

    Trainable Weka Segmentation: remove e-mails from authors.

--- src-plugins/Trainable_Segmentation/src/main/java/trainableSegmentation/FeatureStack.java
+++ src-plugins/Trainable_Segmentation/src/main/java/trainableSegmentation/FeatureStack.java
@@ -43,8 +43,7 @@ package trainableSegmentation;
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * Authors: Verena Kaynig (verena.kaynig@inf.ethz.ch), Ignacio Arganda-Carreras (iarganda@mit.edu)
- *          Albert Cardona (acardona@ini.phys.ethz.ch)
+ * Authors: Verena Kaynig, Ignacio Arganda-Carreras, Albert Cardona
  */
 
 import java.io.BufferedWriter;
