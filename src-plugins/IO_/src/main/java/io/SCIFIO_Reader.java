dfd7d055f30378e87225258eac5203003cd927ea
Johannes Schindelin <johannes.schindelin@gmx.de>
2013-12-19 13:29:32 -0600

    IO: add the SCIFIO importer plugin
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ src-plugins/IO_/src/main/java/io/SCIFIO_Reader.java
@@ -0,0 +1,54 @@
+package io;
+
+import ij.IJ;
+import ij.ImagePlus;
+import ij.io.OpenDialog;
+import ij.plugin.PlugIn;
+import io.scif.img.IO;
+import io.scif.img.ImgIOException;
+
+import java.io.File;
+
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.meta.ImgPlus;
+
+/**
+ * Opens images using SCIFIO.
+ * 
+ * @author Johannes Schindelin
+ */
+public class SCIFIO_Reader extends ImagePlus implements PlugIn {
+
+	@Override
+	public void run(String arg) {
+		File file = null;
+		if (arg != null && arg.length() > 0)
+			file = new File(arg);
+		else {
+			OpenDialog od =
+				new OpenDialog("Choose .icns file", null);
+			String directory = od.getDirectory();
+			if (null == directory)
+				return;
+			file = new File(directory + "/" + od.getFileName());
+		}
+
+		try {
+			@SuppressWarnings("rawtypes")
+			ImgPlus img = IO.open(file.getPath());
+
+			final String title = file.getName();
+			setTitle(title);
+			@SuppressWarnings("unchecked")
+			final ImagePlus imp = ImageJFunctions.wrap(img, title);
+			setStack(imp.getImageStack());
+
+			if (arg.equals(""))
+				show();
+		} catch (ImgIOException e) {
+			e.printStackTrace();
+			IJ.error("Could not open " + file + ": " + e.getMessage());
+		}
+	}
+
+}
