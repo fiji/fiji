bd88afe2dfccf71d30141dc3270570bdc8e977d8
Curtis Rueden <ctrueden@wisc.edu>
2014-01-07 15:34:22 -0600

    Remove TrackMate from Fiji repository
    It has migrated to its own repository at:
        https://github.com/fiji/TrackMate

--- src-plugins/TrackMate_/src/main/java/fiji/plugin/trackmate/graph/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * A package containing utility classes for the manipulation and analysis of 
- * tracks as JGraphT graphs.
- * 
- * @author Jean-Yves Tinevez
- */
-package fiji.plugin.trackmate.graph;
