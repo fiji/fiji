edb5402d511237346e7b5f7fb355851a7a210e0e
Johannes Schindelin <johannes.schindelin@gmx.de>
2014-01-10 16:49:00 -0600

    fiji.Debug#run(): make sure that ij-legacy had a chance to patch ImageJ 1.x
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/fiji-lib/src/main/java/fiji/Debug.java
+++ src-plugins/fiji-lib/src/main/java/fiji/Debug.java
@@ -1,15 +1,22 @@
 package fiji;
 
-import ij.IJ;
-import ij.ImageJ;
-import ij.ImagePlus;
-import ij.WindowManager;
-
-import java.io.File;
-
 import fiji.debugging.Object_Inspector;
 
 public class Debug {
+	static {
+		// if ij-legacy is in the class path, run the preinit() method
+		try {
+			final ClassLoader loader = Thread.currentThread().getContextClassLoader();
+			final Class<?> clazz = loader.loadClass("imagej.legacy.DefaultLegacyService");
+			clazz.getMethod("preinit").invoke(null);
+		} catch (Exception e) {
+			// ignore if it is not in the class path
+			if (!(e instanceof ClassNotFoundException)) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 	public static void show(Object object) {
 		Object_Inspector.openFrame("" + object, object);
 	}
@@ -27,11 +34,7 @@ public class Debug {
 	 * @param pass the parameters as recorded by the Recorder, or null if dialogs should pop up
 	 */
 	public static void run(String plugin, String parameters) {
-		makeSureCallerIsInPluginsDir();
-		if (parameters == null)
-			IJ.run(plugin);
-		else
-			IJ.run(plugin, parameters);
+		LegacyHelper.run(plugin, parameters);
 	}
 
 	/**
@@ -44,35 +47,6 @@ public class Debug {
 	 * @param pass the parameters as recorded by the Recorder, or null if dialogs should pop up
 	 */
 	public static void runFilter(String imagePath, String plugin, String parameters) {
-		makeSureCallerIsInPluginsDir();
-	    ImagePlus image = IJ.openImage(imagePath);
-	    image.show();
-	    WindowManager.addWindow(image.getWindow());
-	    run(plugin, parameters);
+		LegacyHelper.runFilter(imagePath, plugin, parameters);
 	}
-
-	protected static void makeSureCallerIsInPluginsDir() {
-		if (IJ.getInstance() != null)
-			return; // too late anyway
-		// make sure that the calling .jar is found by ImageJ 1.x
-		StackTraceElement[] stackTrace = new Throwable().getStackTrace();
-		if (stackTrace.length > 2) {
-			String className = stackTrace[2].getClassName();
-			String relativePath = className.replace('.', '/') + ".class";
-			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-			if (classLoader == null)
-				classLoader = Debug.class.getClassLoader();
-			String directory = classLoader.getResource(relativePath).getPath();
-			directory = directory.substring(0,  directory.length() - relativePath.length());
-			if (directory.endsWith("/classes/"))
-				directory = directory.substring(0, directory.length() - "/classes/".length());
-			else if (directory.endsWith("/test-classes/"))
-				directory = directory.substring(0, directory.length() - "/test-classes/".length());
-			else if (directory.startsWith("file:") && directory.endsWith("!/"))
-				directory = new File(directory.substring(5, directory.length() - 2)).getParent();
-			System.setProperty("plugins.dir", directory);
-		}
-		new ImageJ();
-	}
-
 }
