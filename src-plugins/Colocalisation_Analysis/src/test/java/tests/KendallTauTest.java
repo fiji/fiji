7f6dfc5d91cad020cb71e94eb057b0761b211361
Johannes Schindelin <johannes.schindelin@gmx.de>
2014-01-26 20:32:45 -0600

    Kendall-Tau: add an exhaustive test
    It is always good to verify that a robust but slow algorithm is replaced
    by a fast *and accurate* algorithm.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/Colocalisation_Analysis/src/test/java/tests/KendallTauTest.java
+++ src-plugins/Colocalisation_Analysis/src/test/java/tests/KendallTauTest.java
@@ -1,6 +1,8 @@
 package tests;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeTrue;
 import net.imglib2.PairIterator;
 import net.imglib2.type.numeric.real.DoubleType;
 
@@ -13,19 +15,25 @@ import algorithms.MissingPreconditionException;
  * Tests the Kendall Tau implementation.
  */
 public class KendallTauTest {
+
+	private boolean exhaustive = false;
+
 	@Test
 	public void testSimple() throws MissingPreconditionException {
+		assumeTrue(!exhaustive);
 		// From Armitage P, Berry G. Statistical Methods in Medical Research (3rd edition). Blackwell 1994, p. 466.
 		assertTau(23.0 / 45.0, new int[] { 4, 10, 3, 1, 9, 2, 6, 7, 8, 5 }, new int[] { 5, 8, 6, 2, 10, 3, 9, 4, 7, 1 });
 	}
 
 	@Test
 	public void testPathological() throws MissingPreconditionException {
+		assumeTrue(!exhaustive);
 		assertTau(Double.NaN, new int[] { 1, 1, 1, 1 }, new int[] { 2, 2, 2, 2 });
 	}
 
 	@Test
 	public void testSomeDuplicates() throws MissingPreconditionException {
+		assumeTrue(!exhaustive);
 		// for pairs (1, 3), (1, 2), (2, 1), (3, 1),
 		// n = 4,
 		// n0 = n * (n - 1) / 2 = 4 * 3 / 2 = 6
@@ -37,9 +45,9 @@ public class KendallTauTest {
 		assertTau(-0.8, new int[] { 1, 1, 2, 3 }, new int[] { 3, 2, 1, 1 });
 	}
 
-	private void assertTau(final double expected, final int[] values1, final int[] values2) throws MissingPreconditionException {
+	private PairIterator<DoubleType> pairIterator(final int[] values1, final int[] values2) {
 		assertEquals(values1.length, values2.length);
-		final PairIterator<DoubleType> iter = new PairIterator<DoubleType>() {
+		return new PairIterator<DoubleType>() {
 			private int i = -1;
 			private DoubleType ch1 = new DoubleType();
 			private DoubleType ch2 = new DoubleType();
@@ -71,7 +79,39 @@ public class KendallTauTest {
 				return ch2;
 			}
 		};
+	}
 
+	private void assertTau(final double expected, final int[] values1, final int[] values2) throws MissingPreconditionException {
+		final PairIterator<DoubleType> iter = pairIterator(values1, values2);
 		assertEquals(expected, KendallTauRankCorrelation.calculateMergeSort(iter), 1e-10);
+	}
+
+	private int seed;
+
+	private int pseudoRandom()
+	{
+		return seed = 3170425 * seed + 132102;
+	}
+
+	@Test
+	public void exhaustiveTesting() throws Exception {
+		assumeTrue(exhaustive);
+		final int n = 5, m = 10;
+		final int[] values1 = new int[n], values2 = new int[n];
+		for (int i = 0; i < 100; i++) {
+			for (int j = 0; j < n; j++) {
+				values1[j] = Math.abs(pseudoRandom()) % m;
+				values2[j] = Math.abs(pseudoRandom()) % m;
+			}
+			final PairIterator<DoubleType> iter = pairIterator(values1, values2);
+			double value1 = KendallTauRankCorrelation.calculateNaive(iter);
+			iter.reset();
+			double value2 = KendallTauRankCorrelation.calculateMergeSort(iter);
+			if (Double.isNaN(value1)) {
+				assertTrue("i: " + i + ", value2: " + value2, Double.isInfinite(value2) || Double.isNaN(value2));
+			} else {
+				assertEquals("i: " + i, value1, value2, 1e-10);
+			}
+		}
 	}
 }
