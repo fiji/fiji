85117ad9076f42e92b968936af70ce5e22d3aa39
Johannes Schindelin <johannes.schindelin@gmx.de>
2014-01-29 12:39:30 -0600

    Coloc 2: make it easier to apply interactive test to custom images
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/Colocalisation_Analysis/src/test/java/tests/TestInteractively.java
+++ src-plugins/Colocalisation_Analysis/src/test/java/tests/TestInteractively.java
@@ -8,11 +8,13 @@ public class TestInteractively {
 	public static void main(String[] args) {
 		// dummy call: initialize debug session
 		Debug.run("Close All", "");
-		String testsDataDir = System.getProperty("plugins.dir") + "/../tests/Data";
-		Debug.run("Open...", "open=" + testsDataDir + "/colocsample1b-green.tif");
-		Debug.run("Open...", "open=" + testsDataDir + "/colocsample1b-red.tif");
+		final String image1 = "colocsample1b-green.tif";
+		final String image2 = "colocsample1b-red.tif";
+		String testsDataDir = System.getProperty("plugins.dir") + "/../tests/Data/";
+		Debug.run("Open...", "open=" + testsDataDir + image1);
+		Debug.run("Open...", "open=" + testsDataDir + image2);
 		Debug.run("Coloc 2",
-			"channel_1=colocsample1b-green.tif channel_2=colocsample1b-red.tif roi_or_mask=<None> "
+			"channel_1=" + image1 + " channel_2=" + image2 + " roi_or_mask=<None> "
 			+ "li_histogram_channel_1 "
 			+ "li_histogram_channel_2 "
 			+ "li_icq "
