18a51af7cfb7c3cb7629986f9e23364eb28b013c
Johannes Schindelin <johannes.schindelin@gmx.de>
2014-03-14 18:54:47 -0500

    Replace fiji.Debug's run method with one based on ij1-patcher
    We used to do our own home-brewed thing. The experience was valuable,
    though, contributing vastly to what eventually became the ij1-patcher
    project. Let's now use the matured ij1-patcher to do the same thing we
    did before, just better.
    
    Incidentally, this also addresses the problem that setting plugins.dir
    to the Maven output directory found 'duplicate plugins' because the
    classes were found both in target/classes/ as well as in
    target/<artifactId>-<version>.jar.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/fiji-lib/src/main/java/fiji/Debug.java
+++ src-plugins/fiji-lib/src/main/java/fiji/Debug.java
@@ -26,7 +26,7 @@ public class Debug {
 	 * @param pass the parameters as recorded by the Recorder, or null if dialogs should pop up
 	 */
 	public static void run(String plugin, String parameters) {
-		LegacyHelper.run(plugin, parameters);
+		runFilter(null, plugin, parameters);
 	}
 
 	/**
@@ -39,6 +39,31 @@ public class Debug {
 	 * @param pass the parameters as recorded by the Recorder, or null if dialogs should pop up
 	 */
 	public static void runFilter(String imagePath, String plugin, String parameters) {
-		LegacyHelper.runFilter(imagePath, plugin, parameters);
+		runFilter(imagePath, plugin, parameters, false);
+	}
+
+	/**
+	 * Debug helper
+	 *
+	 * Call this function from your debugger to debug your filter plugin
+	 *
+	 * @param imagePath the path to the example image to test with
+	 * @param plugin the menu item label of the plugin to run
+	 * @param pass the parameters as recorded by the Recorder, or null if dialogs should pop up
+	 */
+	public static void runFilter(String imagePath, String plugin, String parameters, final boolean headless) {
+		try {
+			final LegacyEnvironment ij1 = new LegacyEnvironment(null, headless);
+			ij1.addPluginClasspath(Thread.currentThread().getContextClassLoader());
+			// show UI
+			if (!headless) ij1.main();
+			if (imagePath != null) {
+				ij1.runMacro("open('" + imagePath + "');", "");
+			}
+			ij1.run(plugin, parameters);
+		}
+		catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
