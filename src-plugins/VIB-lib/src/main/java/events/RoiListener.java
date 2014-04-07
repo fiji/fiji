c05fd256ceb9b93282a561964883a05733e3bf24
Johannes Schindelin <johannes.schindelin@gmx.de>
2014-04-07 17:32:48 -0500

    VIB-lib is an external project now
    As of a3ee6a2(Add a .gitignore file), VIB-lib lives in
    
    	https://github.com/fiji/VIB-lib
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/VIB-lib/src/main/java/events/RoiListener.java
+++
@@ -1,8 +0,0 @@
-/*
- * Created on 31-May-2006
- */
-package events;
-
-public interface RoiListener {
-	void roiChanged(RoiEvent e);
-}
--- src-plugins/VIB-lib/src/main/java/vib/app/module/ModuleListener.java
+++
@@ -1,6 +0,0 @@
-package vib.app.module;
-
-public interface ModuleListener {
-	public void moduleFinished(Module module, int index);
-	public void exceptionOccurred(Module module, int index);
-}
