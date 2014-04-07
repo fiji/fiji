bd83c2d9f93fa99565f9dafd0d42b259bfc2d3ea
Johannes Schindelin <johannes.schindelin@gmx.de>
2014-04-07 17:34:40 -0500

    fiji-lib is an external project now
    As of 94fe60c(Add a .gitignore file), fiji-lib lives in
    
    	https://github.com/fiji/fiji-lib
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src-plugins/fiji-lib/src/main/java/fiji/tool/SliceListener.java
+++
@@ -1,7 +0,0 @@
-package fiji.tool;
-
-import ij.ImagePlus;
-
-public interface SliceListener {
-	public void sliceChanged(ImagePlus image);
-}
--- src-plugins/fiji-lib/src/main/java/fiji/tool/ToolToggleListener.java
+++
@@ -1,5 +0,0 @@
-package fiji.tool;
-
-public interface ToolToggleListener {
-	public void toolToggled(boolean enabled);
-}
