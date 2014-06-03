b9f9c11c25f1cd5bcb7e17d983c9fb2103df96ee
Johannes Schindelin <johannes.schindelin@gmx.de>
2014-06-03 11:58:51 -0500

    Update the .jar files in Fix_Updater.js
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- plugins/Scripts/Plugins/Utilities/Fix_Updater.js
+++ plugins/Scripts/Plugins/Utilities/Fix_Updater.js
@@ -12,11 +12,10 @@ importClass(Packages.java.net.URLClassLoader);
 
 baseURL = 'http://update.imagej.net/jars/';
 jars = [
-	'imagej-ui-swing-0.4.0.jar-20140516211031',
-	'imagej-updater-0.3.2.jar-20140520164735',
-	'scijava-common-2.19.2.jar-20140520164735',
-	'imagej-common-0.5.1.jar-20140516211031',
-	'jai_imageio-1.0.1.jar-20130716162630',
+	'imagej-ui-swing-0.4.3.jar-20140603151316',
+	'imagej-updater-0.3.5.jar-20140603151316',
+	'scijava-common-2.22.2.jar-20140603151316',
+	'imagej-common-0.7.2.jar-20140603151316',
 	'mapdb-0.9.7.jar-20131230181631',
 	'udunits-4.3.18.jar-20131018164809',
 	'eventbus-1.4.jar-20120404210913',
