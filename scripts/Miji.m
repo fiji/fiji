c04b5e78e7280a1af68e54d65f9ddb6fa5e8defb
Mark Hiner <hinerm@gmail.com>
2014-07-18 09:37:35 -0500

    Miji.m: add jars in subdirectories
    The Miji.m script adds Fiji.app/jars and Fiji.app/plugins to the MATLAB
    classpath by explicitly iterating over *.jar in these directories. Thus
    it was not recursing to subdirectories - notably /jars/bio-formats.
    
    The script will now recurse through subdirectories to find jars.
    
    Adapted from: http://stackoverflow.com/questions/2652630/
    how-to-get-all-files-under-a-specific-directory-in-matlab

--- scripts/Miji.m
+++ scripts/Miji.m
@@ -52,17 +52,29 @@ end
 
 function add_to_classpath(classpath, directory)
     % Get all .jar files in the directory
-    test = dir(strcat([directory filesep '*.jar']));
+    dirData = dir(directory);
+    dirIndex = [dirData.isdir];
+    jarlist = dir(fullfile(directory,'*.jar'));
     path_= cell(0);
-    for i = 1:length(test)
-        if not_yet_in_classpath(classpath, test(i).name)
-            path_{length(path_) + 1} = strcat([directory filesep test(i).name]);
+    for i = 1:length(jarlist)
+      disp(jarlist(i).name);
+        if not_yet_in_classpath(classpath, jarlist(i).name)
+            path_{length(path_) + 1} = fullfile(directory,jarlist(i).name);
         end
     end
 
     %% Add them to the classpath
     if ~isempty(path_)
         javaaddpath(path_, '-end');
+    end
+
+    %# Recurse over subdirectories
+    subDirs = {dirData(dirIndex).name};
+    validIndex = ~ismember(subDirs,{'.','..'});
+
+    for iDir = find(validIndex)
+      nextDir = fullfile(directory,subDirs{iDir});
+      add_to_classpath(classpath, nextDir);
     end
 end
 
