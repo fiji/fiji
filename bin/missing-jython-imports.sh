e5d22d2a75ab3d1914baf3ec1cbf6551fab2607d
Johannes Schindelin <johannes.schindelin@gmx.de>
2015-03-08 11:38:23 +0100

    missing-jython-imports: spit out statements ready to be pasted
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- bin/missing-jython-imports.sh
+++ bin/missing-jython-imports.sh
@@ -23,10 +23,40 @@ candidates="$(printf "%s\n%s\n%s\n" "$candidates" "$imported" "$imported" |
 
 ij1classes="$(tar tf jars/ij-1*.jar |
 	sed -n 's/\.class$//p' |
-	sed 's/.*\///' |
+	tr '/' '.' |
 	sort |
 	uniq)"
 
-printf "%s\n%s\n%s\n" "$candidates" "$ij1classes" |
+ij1classnames="$(echo "$ij1classes" |
+	sed 's/.*\.//')"
+
+missing="$(printf "%s\n%s\n%s\n" "$candidates" "$ij1classnames" |
 	sort |
-	uniq -d
+	uniq -d)"
+
+test -n "$missing" ||
+exit 0
+
+toimport="$(echo "$ij1classes" |
+	eval grep $(echo "$missing" | sed 's/.*/-e \\.&$/'))"
+
+imports="$(p=; c=; echo "$toimport" |
+	while read class
+	do
+		c1="${class##*.}"
+		p1="${class%.$c1}"
+		if test "$p" = "$p1"
+		then
+			c="$c, $c1"
+		else
+			test -z "$p" ||
+			printf 'from %s import %s\n' "$p" "$c"
+			p="$p1"
+			c="$c1"
+		fi
+	done
+	test -z "$p" ||
+	printf 'from %s import %s\n' "$p" "$c")"
+
+test -z "$imports" ||
+echo "$imports"
