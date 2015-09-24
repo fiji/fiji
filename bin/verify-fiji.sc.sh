8a12384e91456d8e0dec1f09038b7ed07486c4cb
Curtis Rueden <ctrueden@wisc.edu>
2015-09-23 20:14:14 -0500

    Update verify-fiji.sc.sh script
    This update makes the following changes:
    
    * Use -L flag for curl, to follow redirects. Several services
      historically provided by fiji.sc are now handled elsewhere,
      but we provide 301 redirects to the new URLs.
    
    * Eliminate the search test. The fiji.sc search is discontinued
      in favor of search.imagej.net.
    
    * Use http://fiji.sc/git/fiji.git for the Gitweb test, not
      http://fiji.sc/fiji.git. The two mean different things:
      with the /git refers to Gitweb, whereas without refers to
      the raw repo served over HTTP.

--- bin/verify-fiji.sc.sh
+++ bin/verify-fiji.sc.sh
@@ -4,9 +4,6 @@
 
 set -e
 
-echo Search
-curl http://fiji.sc/verify-fiji.sc.sh | grep "Sources for verify-fiji.sc"
-
 echo Fiji Wiki
 curl http://fiji.sc/Fiji | grep "Fiji Is Just ImageJ"
 
@@ -14,16 +11,16 @@ echo Fiji Wiki legacy
 curl http://fiji.sc/wiki/Fiji | grep "Fiji Is Just ImageJ"
 
 echo Fiji Wiki legacy \#2
-curl http://fiji.sc/mediawiki/phase3/Fiji | grep "Fiji Is Just ImageJ"
+curl -L http://fiji.sc/mediawiki/phase3/Fiji | grep "Fiji Is Just ImageJ"
 
 echo Fiji Wiki page starting with a digit
-curl http://fiji.sc/2013-02-25_-_TrackMate_v2.0.0_released | grep "We just released"
+curl -L http://fiji.sc/2013-02-25_-_TrackMate_v2.0.0_released | grep "We just released"
 
 echo Smart HTTP
 git ls-remote http://fiji.sc/fiji.git master
 
 echo GitWeb
-curl http://fiji.sc/cgi-bin/gitweb.cgi | grep fiji.git
+curl -L http://fiji.sc/git/ | grep fiji.git
 
 echo GitWeb, link to repository
-curl http://fiji.sc/fiji.git | grep commitdiff
+curl -L http://fiji.sc/git/fiji.git | grep commitdiff
