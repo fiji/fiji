058d805cbe7d4ea0f18bedc46655ca1624242d7d
Curtis Rueden <ctrueden@wisc.edu>
2017-02-25 16:18:26 -0600

    WIP: Wrap the BDV in a JavaFX window
    And listen to multi-touch events.
    
    It mostly works, although you must (?) click on the BDV canvas
    before Zoom and Rotate events start firing.
    
    I also tried with a JFXPanel inside a JFrame, which also did not work,
    as discussed in this thread:
    
        https://community.oracle.com/thread/3578491
    
    See also this gist, which offers another working example of
    embedding Swing in a JavaFX window and receiving these events:
    
        https://github.com/Inervo/BookNaviger/issues/45

--- src/main/java/sc/fiji/Main.java
+++ src/main/java/sc/fiji/Main.java
@@ -22,7 +22,34 @@
 
 package sc.fiji;
 
+import java.awt.BorderLayout;
+import java.awt.Window;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import net.imagej.Dataset;
 import net.imagej.ImageJ;
+import net.imglib2.realtransform.AffineTransform3D;
+
+import bdv.util.BdvFunctions;
+import bdv.util.BdvOptions;
+import bdv.viewer.ViewerFrame;
+import bdv.viewer.ViewerPanel;
+import javafx.application.Application;
+import javafx.embed.swing.SwingNode;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.RotateEvent;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.input.SwipeEvent;
+import javafx.scene.input.TouchEvent;
+import javafx.scene.input.ZoomEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  * Launches Fiji.
@@ -30,6 +57,7 @@ import net.imagej.ImageJ;
  * @author Curtis Rueden
  * @see net.imagej.ImageJ
  */
+@SuppressWarnings("restriction")
 public final class Main {
 
 	private Main() {
@@ -38,7 +66,7 @@ public final class Main {
 
 	// -- Main method --
 
-	public static void main(final String[] args) {
+	public static void main(final String[] args) throws IOException {
 		// NB: If you set the plugins.dir system property to a valid Fiji
 		// installation, you will have access to that installation's scripts
 		// and ImageJ 1.x plugins.
@@ -47,7 +75,173 @@ public final class Main {
 		// installation's plugins folder over the JARs on the classpath!
 		System.setProperty("plugins.dir", "/path/to/your/Fiji.app");
 
-		new ImageJ().launch(args);
+		ImageJ ij = new ImageJ();
+		ij.launch(args);
+		final Dataset t1Head = //
+			ij.scifio().datasetIO().open("/Users/curtis/data/t1-head.tif");
+		BdvFunctions.show(t1Head, "T1-Head").setDisplayRange(0, 742);
+		for (Window w : Window.getWindows()) {
+			if (w instanceof ViewerFrame) {
+				// Convert it to JavaFX!
+				convertToJavaFX((ViewerFrame) w);
+			}
+		}
+	}
+
+	public static ViewerPanel viewer;
+	public static class MyApplication extends Application {
+
+		@Override
+		public void start(Stage primaryStage) throws Exception {
+			AnchorPane root = new AnchorPane();
+
+			final SwingNode swingNode = new SwingNode();
+			swingNode.setContent(viewer);
+			addEventHandlers(swingNode);
+			root.getChildren().add(swingNode);
+			Scene scene = new Scene(root, 500, 500);
+
+			primaryStage.setTitle("BDV");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		}
+	}
+
+	private static void convertToJavaFX(final ViewerFrame viewerFrame) {
+		viewerFrame.setVisible(false);
+		viewer = viewerFrame.getViewerPanel(); // AAAAARGH
+		viewerFrame.remove(viewer);
+		Application.launch(MyApplication.class);
+	}
+	
+	public static void addEventHandlers(final Node node) {
+		// add event handlers
+		node.setOnScroll(new EventHandler<ScrollEvent>() {
+
+			@Override
+			public void handle(final ScrollEvent event) {
+				System.err.println(event);
+			}
+
+		});
+
+		node.setOnZoom(new EventHandler<ZoomEvent>() {
+
+			@Override
+			public void handle(ZoomEvent event) {
+				double x = event.getX();
+				double y = event.getY();
+//				viewer.setCurrentViewerTransform(hb);
+//				AffineTransform3D affine = viewer.getDisplay().getTransformEventHandler().getTransform();
+//				affine.translate(translationVector);
+//				affine.scale(s);
+				System.err.println(event);
+			}
+		});
+
+		node.setOnRotate(new EventHandler<RotateEvent>() {
+
+			@Override
+			public void handle(RotateEvent event) {
+				System.err.println(event);
+			}
+		});
+
+		node.setOnScrollStarted(new EventHandler<ScrollEvent>() {
+
+			@Override
+			public void handle(ScrollEvent event) {
+				System.err.println(event);
+			}
+		});
+
+		node.setOnScrollFinished(new EventHandler<ScrollEvent>() {
+
+			@Override
+			public void handle(ScrollEvent event) {
+				System.err.println(event);
+			}
+		});
+
+		node.setOnZoomStarted(new EventHandler<ZoomEvent>() {
+
+			@Override
+			public void handle(ZoomEvent event) {
+				System.err.println(event);
+			}
+		});
+
+		node.setOnZoomFinished(new EventHandler<ZoomEvent>() {
+
+			@Override
+			public void handle(ZoomEvent event) {
+				System.err.println(event);
+			}
+		});
+
+		node.setOnRotationStarted(new EventHandler<RotateEvent>() {
+
+			@Override
+			public void handle(RotateEvent event) {
+				System.err.println(event);
+			}
+		});
+
+		node.setOnRotationFinished(new EventHandler<RotateEvent>() {
+
+			@Override
+			public void handle(RotateEvent event) {
+				System.err.println(event);
+			}
+		});
+
+		node.setOnMousePressed(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				System.err.println(event);
+			}
+		});
+
+		node.setOnMouseReleased(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				System.err.println(event);
+			}
+		});
+
+		node.setOnTouchPressed(new EventHandler<TouchEvent>() {
+
+			@Override
+			public void handle(TouchEvent event) {
+				System.err.println("Touch pressed event");
+			}
+		});
+
+		node.setOnTouchReleased(new EventHandler<TouchEvent>() {
+
+			@Override
+			public void handle(TouchEvent event) {
+				System.err.println("Touch released event");
+			}
+		});
+
+		node.setOnSwipeRight(new EventHandler<SwipeEvent>() {
+
+			@Override
+			public void handle(SwipeEvent event) {
+				System.err.println("Swipe right event");
+			}
+		});
+
+		node.setOnSwipeLeft(new EventHandler<SwipeEvent>() {
+
+			@Override
+			public void handle(SwipeEvent event) {
+				System.err.println("Swipe left event");
+			}
+		});
 	}
 
 }
