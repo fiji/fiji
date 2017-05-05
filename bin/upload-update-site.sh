20fb7de2044696ea054f5a96dee4b6d7ee39cb8c
Curtis Rueden <ctrueden@wisc.edu>
2017-05-05 11:41:08 -0500

    upload-update-site: inline the example of usage
    The referenced script was removed from the repository.

--- bin/upload-update-site.sh
+++ bin/upload-update-site.sh
@@ -5,7 +5,32 @@
 #
 
 # Uploads JARs from an external location to an ImageJ update site.
-# See upload-bf.sh for an example of usage.
+#
+# Here is how this script was once used to update the Bio-Formats update site:
+#
+#   export JAR_URL="https://ci.openmicroscopy.org/job/BIOFORMATS-DEV-latest/lastSuccessfulBuild/artifact/artifacts/"
+#
+#   export JAR_FILES="
+#   jars/bio-formats/formats-api
+#   jars/bio-formats/formats-bsd
+#   jars/bio-formats/formats-common
+#   jars/bio-formats/formats-gpl
+#   jars/bio-formats/jai_imageio
+#   jars/bio-formats/mdbtools-java
+#   jars/bio-formats/metakit
+#   jars/bio-formats/ome-jxr
+#   jars/bio-formats/ome-poi
+#   jars/bio-formats/ome-xml
+#   jars/bio-formats/specification
+#   jars/bio-formats/turbojpeg
+#   plugins/bio-formats_plugins
+#   "
+#
+#   export UPDATE_SITE_NAME="Bio-Formats"
+#   export UPDATE_SITE_USER="Bio-Formats"
+#   export UPDATE_SITE_URL="http://sites.imagej.net/$UPDATE_SITE_USER/"
+#
+#   sh "$(dirname "$0")"/upload-update-site.sh
 
 # -- Constants --
 
