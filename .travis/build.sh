b6b9fae26b6dac2452a575c70e249e3b4b1d48dd
Curtis Rueden <ctrueden@wisc.edu>
2017-11-20 13:36:16 -0600

    Update Travis configuration

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
-sh travis-build.sh
+sh travis-build.sh $encrypted_ae5c55655408_key $encrypted_ae5c55655408_iv
