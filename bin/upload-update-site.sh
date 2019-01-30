cd57756276fb860265f9fc27925e63454d92efd4
Hadrien Mary <hadrien.mary@gmail.com>
2019-01-30 11:08:01 -0500

    [upload-update-site.sh] Don't install deps if UPLOAD_WITH_DEPENDENCIES is set to false

--- bin/upload-update-site.sh
+++ bin/upload-update-site.sh
@@ -39,7 +39,7 @@ fi
 
 if [ -n "$TRAVIS_TAG" ]; then
 
-    echo "== Travis tag detected. Start uploading the specified update site =="
+    echo "** Travis tag detected. Start uploading the specified update site **"
 
     if [ -z "$UPDATE_SITE_PASSWORD" ]; then
         echo "The variable UPDATE_SITE_PASSWORD is not set. You need to set it in the Travis configuration."
@@ -56,37 +56,45 @@ if [ -n "$TRAVIS_TAG" ]; then
         UPDATE_SITE_USER=$UPDATE_SITE_NAME
     fi
 
-    echo "Setup variables."
+    echo "* Setup variables."
     URL="http://sites.imagej.net/$UPDATE_SITE_NAME/"
     IJ_PATH="$HOME/Fiji.app"
     IJ_LAUNCHER="$IJ_PATH/ImageJ-linux64"
 
-    echo "Installing Fiji."
+    echo "* Installing Fiji."
     mkdir -p $IJ_PATH/
     cd $HOME/
     wget -q "https://downloads.imagej.net/fiji/latest/fiji-linux64.zip"
     unzip -q fiji-linux64.zip
 
-    echo "Updating Fiji."
+    echo "* Updating Fiji."
     $IJ_LAUNCHER --update update-force-pristine
 
-    echo "Install project to Fiji directory."
     cd $TRAVIS_BUILD_DIR/
-    mvn clean install -Dimagej.app.directory=$IJ_PATH -Ddelete.other.versions=true
 
-    echo "Gather some project informations."
+    echo "* Gather some project informations."
     VERSION=`mvn help:evaluate -Dexpression=project.version | grep -e '^[^\[]'`
     NAME=`mvn help:evaluate -Dexpression=project.name | grep -e '^[^\[]'`
 
-    echo "Adding $URL as an Update Site."
+    echo "* Adding $URL as an Update Site."
     $IJ_LAUNCHER --update edit-update-site $UPDATE_SITE_NAME $URL "webdav:$UPDATE_SITE_USER:$UPDATE_SITE_PASSWORD" .
 
     if [ "$UPLOAD_WITH_DEPENDENCIES" = false ]; then
-        echo "Upload only \"jars/$NAME.jar\"."
+
+        echo "* Install project to Fiji directory."
+        mvn clean install -Dimagej.app.directory=$IJ_PATH -Ddelete.other.versions=true -Dscijava.ignoreDependencies=true
+
+        echo "* Upload only \"jars/$NAME.jar\"."
         $IJ_LAUNCHER --update upload --update-site "$UPDATE_SITE_NAME" --force-shadow --forget-missing-dependencies "jars/$NAME.jar"
+
     else
-        echo "Upload $NAME with its dependencies."
+
+        echo "* Install project to Fiji directory."
+        mvn clean install -Dimagej.app.directory=$IJ_PATH -Ddelete.other.versions=true -Dscijava.ignoreDependencies=true
+
+        echo "* Upload $NAME with its dependencies."
         $IJ_LAUNCHER --update upload-complete-site --force-shadow "$UPDATE_SITE_NAME"
+
     fi
 else
     echo "== Travis tag not detected. Don't perform upload to update site =="
