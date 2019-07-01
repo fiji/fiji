94ed2b64edc35ab598b8f3b1e2c502cf7c4978a1
Curtis Rueden <ctrueden@wisc.edu>
2019-07-01 15:39:13 +0200

    Travis: only regenerate wiki content on master

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,26 +4,36 @@ sh travis-build.sh $encrypted_ae5c55655408_key $encrypted_ae5c55655408_iv
 
 # -- Generate component sidebars and summary tables --
 
-# Discern needed component versions.
-imagej_version=$(mvn dependency:list | grep 'net.imagej:imagej:' | sed 's/.*:\([0-9][^:]*\):.*/\1/')
-fiji_version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+if [ "$TRAVIS_SECURE_ENV_VARS" = true \
+  -a "$TRAVIS_PULL_REQUEST" = false \
+  -a "$TRAVIS_BRANCH" = master ]
+then
+  echo
+  echo '== Regenerating wiki content =='
 
-# Write out the wiki upload credentials.
-echo "machine imagej.net" > ~/.netrc
-echo "        login TravisCI" >> ~/.netrc
-echo "        password $WIKI_PASSWORD" >> ~/.netrc
+  # Discern needed component versions.
+  imagej_version=$(mvn dependency:list | grep 'net.imagej:imagej:' | sed 's/.*:\([0-9][^:]*\):.*/\1/')
+  fiji_version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 
-# Clone the wiki content generator.
-git clone git://github.com/scijava/mediawiki-maven-info
-cd mediawiki-maven-info
+  # Write out the wiki upload credentials.
+  echo "machine imagej.net" > ~/.netrc
+  echo "        login TravisCI" >> ~/.netrc
+  echo "        password $WIKI_PASSWORD" >> ~/.netrc
 
-# Generate the content and upload!
-mvn -Dinfo.url=https://imagej.net/ \
-    -Dmwmi.groupId=net.imagej \
-    -Dmwmi.artifactId=imagej \
-    -Dmwmi.version="$imagej_version" \
-    -Dmwmi.includeBase \
-    -Dmwmi.groupId2=sc.fiji \
-    -Dmwmi.artifactId2=fiji \
-    -Dmwmi.version2="$fiji_version" \
-    -Dmwmi.includeBase2
+  # Clone the wiki content generator.
+  git clone git://github.com/scijava/mediawiki-maven-info
+  cd mediawiki-maven-info
+
+  # Generate the content and upload!
+  mvn -Dinfo.url=https://imagej.net/ \
+      -Dmwmi.groupId=net.imagej \
+      -Dmwmi.artifactId=imagej \
+      -Dmwmi.version="$imagej_version" \
+      -Dmwmi.includeBase \
+      -Dmwmi.groupId2=sc.fiji \
+      -Dmwmi.artifactId2=fiji \
+      -Dmwmi.version2="$fiji_version" \
+      -Dmwmi.includeBase2
+else
+  echo 'Skipping wiki content regeneration.'
+fi
