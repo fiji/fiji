a7109893bb264378222cafa3a6b9337c10dc5815
Curtis Rueden <ctrueden@wisc.edu>
2019-07-01 15:15:11 +0200

    Travis: regenerate wiki tables automatically
    This means that the imagej.net wiki will always have component tables
    and sidebars corresponding to what's on fiji/fiji master. This is not
    perfect, because e.g. if a new component version of net.imagej:imagej
    is released, that version will not be reflected on the wiki until the
    fiji/fiji Travis build runs again.
    
    For now, this is not a huge problem from a pragmatic perspective.
    But if a second distribution of ImageJ starts to coexist on imagej.net
    along with Fiji, now there will be fighting if that distribution also
    wants to use this mechanism to update its stats and tables.

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,3 +1,29 @@
 #!/bin/sh
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
 sh travis-build.sh $encrypted_ae5c55655408_key $encrypted_ae5c55655408_iv
+
+# -- Generate component sidebars and summary tables --
+
+# Discern needed component versions.
+imagej_version=$(mvn dependency:list | grep 'net.imagej:imagej:' | sed 's/.*:\([0-9][^:]*\):.*/\1/')
+fiji_version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+
+# Write out the wiki upload credentials.
+echo "machine imagej.net" > ~/.netrc
+echo "        login TravisCI" >> ~/.netrc
+echo "        password $WIKI_PASSWORD" >> ~/.netrc
+
+# Clone the wiki content generator.
+git clone git://github.com/scijava/mediawiki-maven-info
+cd mediawiki-maven-info
+
+# Generate the content and upload!
+mvn -Dinfo.url=https://imagej.net/ \
+    -Dmwmi.groupId=net.imagej \
+    -Dmwmi.artifactId=imagej \
+    -Dmwmi.version="$imagej_version" \
+    -Dmwmi.includeBase \
+    -Dmwmi.groupId2=sc.fiji \
+    -Dmwmi.artifactId2=fiji \
+    -Dmwmi.version2="$fiji_version" \
+    -Dmwmi.includeBase2
