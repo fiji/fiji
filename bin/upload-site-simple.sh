cc215dc7eac9de449d5eacb36b4db4d6d6bc6101
Curtis Rueden <ctrueden@wisc.edu>
2019-08-26 15:23:37 -0500

    upload-site-simple.sh: stop using ImageJ-tiger
    The ImageJ-tiger executable is no longer supported. We need to use
    ImageJ-macosx instead, since ImageJ-tiger may no longer be present.

--- bin/upload-site-simple.sh
+++ bin/upload-site-simple.sh
@@ -23,7 +23,7 @@ url="http://sites.imagej.net/$update_site/"
 case "$(uname -s),$(uname -m)" in
 Linux,x86_64) launcher=ImageJ-linux64;;
 Linux,*) launcher=ImageJ-linux32;;
-Darwin,*) launcher=Contents/MacOS/ImageJ-tiger;;
+Darwin,*) launcher=Contents/MacOS/ImageJ-macosx;;
 MING*,*) launcher=ImageJ-win32.exe;;
 *) echo "Unknown platform" >&2; exit 1;;
 esac
