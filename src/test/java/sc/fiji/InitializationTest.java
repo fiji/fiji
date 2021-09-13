c1c68c318e6f14a88c24b7ae6915fd61a07ed74a
Curtis Rueden <ctrueden@wisc.edu>
2021-09-13 12:28:27 -0500

    Add a sanity test for Fiji startup and shutdown
    This test would have caught the issue with FilamentDetector 1.0.0:
    
      [INFO] Running sc.fiji.InitializationTest
      [ERROR] Tests run: 1, Failures: 0, Errors: 1, Skipped: 0, Time elapsed: 0.986 s <<< FAILURE! - in sc.fiji.InitializationTest
      [ERROR] testInitialization(sc.fiji.InitializationTest)  Time elapsed: 0.89 s  <<< ERROR!
      java.lang.IllegalArgumentException: Invalid service: sc.fiji.filamentdetector.gui.GUIStatusService
              at sc.fiji.InitializationTest.testInitialization(InitializationTest.java:38)
      Caused by: java.lang.NullPointerException
              at sc.fiji.InitializationTest.testInitialization(InitializationTest.java:38)
    
      [INFO]
      [INFO] Results:
      [INFO]
      [ERROR] Errors:
      [ERROR]   InitializationTest.testInitialization:38 » IllegalArgument Invalid service: sc...
      [INFO]
      [ERROR] Tests run: 1, Failures: 0, Errors: 1, Skipped: 0
      [INFO]
    
    With the forthcoming FilamentDetector 1.0.1, the test passes with a
    non-JavaFX-enabled runtime.

---
+++ src/test/java/sc/fiji/InitializationTest.java
@@ -0,0 +1,47 @@
+/*
+ * #%L
+ * Fiji distribution of ImageJ for the life sciences.
+ * %%
+ * Copyright (C) 2007 - 2021 Fiji developers.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+package sc.fiji;
+
+import net.imagej.ImageJ;
+
+import org.junit.Test;
+
+/**
+ * Tests that Fiji can successfully start up and shut down.
+ *
+ * @author Curtis Rueden
+ */
+public final class InitializationTest {
+
+  @Test
+	public void testInitialization() {
+		final ImageJ ij = new ImageJ();
+    ij.ui().setHeadless(true);
+    ij.launch();
+    ij.script().run("script.js",
+			"#@ LogService log\n" +
+			"log.info('Success!');\n",
+			true);
+    ij.dispose();
+	}
+}
