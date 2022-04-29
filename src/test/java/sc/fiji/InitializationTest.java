2ea7270a5a5c11782429bfaa28749c236a1d14a1
Curtis Rueden <ctrueden@wisc.edu>
2022-04-29 01:13:38 -0500

    Happy New Year 2022

--- src/test/java/sc/fiji/InitializationTest.java
+++ src/test/java/sc/fiji/InitializationTest.java
@@ -2,7 +2,7 @@
  * #%L
  * Fiji distribution of ImageJ for the life sciences.
  * %%
- * Copyright (C) 2007 - 2021 Fiji developers.
+ * Copyright (C) 2007 - 2022 Fiji developers.
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
