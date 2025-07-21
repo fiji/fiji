2dcdfb830bfaca4512db4afa66bf50e12dd13f52
Curtis Rueden <ctrueden@wisc.edu>
2025-07-20 19:44:05 -0500

    Update copyright blurbs to 2025

--- src/main/java/sc/fiji/compat/MenuRefresher.java
+++ src/main/java/sc/fiji/compat/MenuRefresher.java
@@ -2,7 +2,7 @@
  * #%L
  * Fiji distribution of ImageJ for the life sciences.
  * %%
- * Copyright (C) 2007 - 2024 Fiji developers.
+ * Copyright (C) 2007 - 2025 Fiji developers.
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
