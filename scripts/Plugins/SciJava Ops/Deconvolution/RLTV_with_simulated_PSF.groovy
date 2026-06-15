13c35a464ac8626d6ffd00d8a6556d949b6a72a2
Edward Evans <elevans2@wisc.edu>
2026-06-15 15:35:47 -0500

    Add boolean options, clean up docstring

--- scripts/Plugins/SciJava Ops/Deconvolution/RLTV_with_simulated_PSF.groovy
+++ scripts/Plugins/SciJava Ops/Deconvolution/RLTV_with_simulated_PSF.groovy
@@ -1,7 +1,7 @@
 #@ OpEnvironment ops
 #@ UIService ui
 #@ Img (label = "Input image:", autofill = false) img
-#@ Integer (label = "Iterations", value = 15) iterations
+#@ String (visibility = MESSAGE, value ="<b>[ Gibson-Lanni PSF settings ]</b>", required = false) psf_msg
 #@ Float (label = "Numerical Aperture", style = "format:0.00", min = 0.00, value = 1.45) numericalAperture
 #@ Integer (label = "Emission Wavelength (nm)", value = 457) wavelength
 #@ Float (label = "Refractive Index (immersion)", style = "format:0.00", min = 0.00, value = 1.5) riImmersion
@@ -9,36 +9,25 @@
 #@ Float (label = "XY spacing (um/pixel)", style = "format:0.0000", min = 0.0000, value = 0.065) xySpacing
 #@ Float (label = "Z spacing (um/pixel)", style = "format:0.0000", min = 0.0000, value = 0.1) zSpacing
 #@ Float (label="Particle/sample Position (um)", style = "format:0.0000", min = 0.0000, value = 0) zPos
+#@ String (visibility = MESSAGE, value ="<b>[ Richardson-Lucy TV settings ]</b>", required = false) rltv_msg
+#@ Integer (label = "Iterations", value = 15) iterations
 #@ Float (label = "Regularization factor", style = "format:0.00000", min = 0.00000, value = 0.002) regFactor
+#@ Boolean (label = "Use non-circulant edge handling", value = false) nonCirc
+#@ Boolean (label = "Use acceleration", value = false) acc
+#@ String (visibility = MESSAGE, value ="<b>[ Output settings ]</b>", required = false) rltv_msg
 #@ Boolean (label = "Show PSF", value = false) showPSF
 #@output Img result
 
-// Richardson-Lucy Total Variation deconvolution with a simulated point spread function.
+// Richardson-Lucy Total Variation deconvolution with a simulated point spread
+// function (PSF).
 //
-// This script utilizes a SciJava Ops implementation of Richardson-Lucy Total Variation (RLTV)
-// deconvolution as described by Dey et al. 2006 and a simulated point spread function (PSF)
-// using the Gibson-Lanni model.
+// This script utilizes a SciJava Ops implementation of Richardson-Lucy Total
+// Variation (RLTV) deconvolution as described by Dey et al. 2006 and a
+// simulated point spread function (PSF) using the Gibson-Lanni model.
 //
-// Arguments
-//    * `img`: A 3-dimensional image with known lateral (x and y) and axial (z) spacing.
-//    * `iterations`: The number of iterations to perform (default = 15).
-//    * `numerical_aperture`: The numerical aperature (NA) of the objective used.
-//    * `wavelength`: The emission wavelength in nanometers (nm) of the image.
-//    * `ri_immersion`: The refractive index of immersion medium (air, oil, etc...).
-//    * `ri_sample`: The refractive index of the sample, a measured value (default = 1.4)
-//    * `lateral_spacing`: The X and Y pixel spacing in μm/pixel of the image.
-//    * `axial_spacing`: The Z spacing in μm/pixel of the image.
-//    * `p_z`: The position of the sample from the coverslip.
-//    * `reg_factor`: The regularization factor.
-//    * `show_psf`: Optionally display the simulated PSF.
-//
-// Returns
-//    * `result`: The deconvolved data.
-//    * `PSF`: The simulated PSF (optional).
-//
-// Reference
-//
-// https://doi.org/10.1002/jemt.20294
+// Reference:
+// <https://doi.org/10.1002/jemt.20294>
+// <https://doi.org/10.1364/josaa.9.000154>
 
 import net.imglib2.type.numeric.complex.ComplexFloatType
 import net.imglib2.type.numeric.real.FloatType
@@ -63,8 +52,8 @@ result = ops.op("deconvolve.richardsonLucyTV").input(img_f32,
                                                      new FloatType(),
                                                      new ComplexFloatType(),
                                                      iterations,
-                                                     false,
-                                                     false,
+                                                     nonCirc,
+                                                     acc,
                                                      regFactor).apply()
 if (showPSF) {
   ui.show("Simulated PSF", psf)
